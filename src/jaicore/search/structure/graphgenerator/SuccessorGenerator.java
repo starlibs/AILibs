9d2abea519a53e184f0e72fa76d95d06dfd63143
Jörn <jkoepe@mail.upb.de>
2017-11-13 20:35:24 +0100

    Anpassung von Successorgenerator als Test ob die Dateien jetzt commitet werden können

--- src/jaicore/search/structure/graphgenerator/SuccessorGenerator.java
+++ src/jaicore/search/structure/graphgenerator/SuccessorGenerator.java
@@ -15,7 +15,7 @@ public interface SuccessorGenerator<T,A> {
 	 * @param node The node we want to expand.
 	 * @return A list of possible next steps.
 	 */
-	public List<NodeExpansionDescription<T,A>> generateSuccessors(Node<T,?> node);
+	public List<NodeExpansionDescription<T,A>> generateSuccessors(T node);
 
 
 }
