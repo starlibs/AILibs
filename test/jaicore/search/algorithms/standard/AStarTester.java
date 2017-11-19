1eb3a47588ce30960e7333028a46304bef4784a2
Jörn <jkoepe@mail.upb.de>
2017-11-19 20:44:32 +0100

    Merging should be complete with this commit

--- test/jaicore/search/algorithms/standard/AStarTester.java
+++ test/jaicore/search/algorithms/standard/AStarTester.java
@@ -7,21 +7,13 @@ import java.util.List;
 
 import org.junit.Test;
 
-<<<<<<< HEAD:test/jaicore/search/algorithms/standard/AStarTester.java
+import jaicore.search.algorithms.standard.AStarTester.TestNode;
 import jaicore.search.algorithms.standard.astar.AStar;
-=======
-import jaicore.search.algorithms.standard.astar.AStarTester.TestNode;
->>>>>>> fd1c7dd64da900832ded0b2d0dc43f9cb47f6214:src/jaicore/search/algorithms/standard/astar/AStarTester.java
 import jaicore.search.structure.core.GraphGenerator;
 import jaicore.search.structure.core.NodeExpansionDescription;
 import jaicore.search.structure.core.NodeType;
 import jaicore.search.structure.graphgenerator.NodeGoalTester;
-<<<<<<< HEAD:test/jaicore/search/algorithms/standard/AStarTester.java
-import jaicore.search.structure.graphgenerator.PathGoalTester;
-import jaicore.search.structure.graphgenerator.RootGenerator;
-=======
 import jaicore.search.structure.graphgenerator.SingleRootGenerator;
->>>>>>> fd1c7dd64da900832ded0b2d0dc43f9cb47f6214:src/jaicore/search/algorithms/standard/astar/AStarTester.java
 import jaicore.search.structure.graphgenerator.SuccessorGenerator;
 
 public class AStarTester {
@@ -55,18 +47,8 @@ public class AStarTester {
 			}
 
 			@Override
-<<<<<<< HEAD:test/jaicore/search/algorithms/standard/AStarTester.java
-			public PathGoalTester<TestNode> getPathGoalTester() {
-				return l -> l.get(l.size()-1).value == 10000;
-			}
-			
-			@Override
-			public NodeGoalTester<TestNode> getNodeGoalTester(){
-				return null;
-=======
 			public NodeGoalTester<TestNode> getGoalTester() {
 				return l -> l.value == 10000;
->>>>>>> fd1c7dd64da900832ded0b2d0dc43f9cb47f6214:src/jaicore/search/algorithms/standard/astar/AStarTester.java
 			}
 		};
 		AStar<TestNode,String> astar = new AStar<>(gen, (n1, n2) -> n2.getPoint().value - n1.getPoint().value, n -> 0);
--- test/jaicore/search/algorithms/standard/BestFirstTester.java
+++ test/jaicore/search/algorithms/standard/BestFirstTester.java
@@ -15,13 +15,7 @@ import jaicore.search.structure.core.GraphGenerator;
 import jaicore.search.structure.core.NodeExpansionDescription;
 import jaicore.search.structure.core.NodeType;
 import jaicore.search.structure.graphgenerator.NodeGoalTester;
-<<<<<<< HEAD:test/jaicore/search/algorithms/standard/BestFirstTester.java
-import jaicore.search.structure.graphgenerator.PathGoalTester;
-import jaicore.search.structure.graphgenerator.RootGenerator;
-import jaicore.search.structure.graphgenerator.SelfContained;
-=======
 import jaicore.search.structure.graphgenerator.SingleRootGenerator;
->>>>>>> fd1c7dd64da900832ded0b2d0dc43f9cb47f6214:src/jaicore/search/algorithms/standard/bestfirst/BestFirstTester.java
 import jaicore.search.structure.graphgenerator.SuccessorGenerator;
 
 public class BestFirstTester {
@@ -54,29 +48,10 @@ public class BestFirstTester {
 				};
 			}
 
-			/*
-			 * only one of the getGoalTester-Methods has to be implemented
-			 */
 			@Override
-<<<<<<< HEAD:test/jaicore/search/algorithms/standard/BestFirstTester.java
-			public PathGoalTester<TestNode> getPathGoalTester() {
-				return l -> l.get(l.size()-1).value == 1000;
-=======
 			public NodeGoalTester<TestNode> getGoalTester() {
 				return l -> l.value == 1000;
->>>>>>> fd1c7dd64da900832ded0b2d0dc43f9cb47f6214:src/jaicore/search/algorithms/standard/bestfirst/BestFirstTester.java
 			}
-
-			@Override
-			public NodeGoalTester<TestNode> getNodeGoalTester() {
-				return null;
-			}
-
-			@Override
-			public SelfContained isSelfContained() {
-				return () ->false;
-			}
-			
 			
 		};
 		
