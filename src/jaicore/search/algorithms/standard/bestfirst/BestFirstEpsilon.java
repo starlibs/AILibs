b736bea7e44bbb64e70ebf18c5b4a59fd96df8f5
Felix Mohr <fmohr@mail.upb.de>
2018-01-18 07:19:22 +0100

    Modifications in BestFirstEpsilon

--- src/jaicore/search/algorithms/standard/bestfirst/BestFirstEpsilon.java
+++ src/jaicore/search/algorithms/standard/bestfirst/BestFirstEpsilon.java
@@ -1,6 +1,7 @@
 package jaicore.search.algorithms.standard.bestfirst;
 
 import jaicore.search.algorithms.standard.core.INodeEvaluator;
+import jaicore.search.algorithms.standard.core.INodeSelector;
 import jaicore.search.algorithms.standard.core.ORGraphSearch;
 import jaicore.search.structure.core.GraphGenerator;
 import jaicore.search.structure.core.Node;
