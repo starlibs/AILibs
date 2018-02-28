81709d747d18b82af2413348d602a18e4a65d312
aminfaez <aminfaez@pc-kbpool-4.cs.uni-paderborn.de>
2018-02-28 12:17:30 +0100

    Added abstract class super type for allowed options classes

--- src/de/upb/crc901/mlplan/evaluablepredicates/mlplan/randomforest/AllowedTreeDepthForRandomForests.java
+++ src/de/upb/crc901/mlplan/evaluablepredicates/mlplan/randomforest/AllowedTreeDepthForRandomForests.java
@@ -1,40 +1,16 @@
 package de.upb.crc901.mlplan.evaluablepredicates.mlplan.randomforest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
-import jaicore.logic.fol.structure.ConstantParam;
-import jaicore.logic.fol.structure.Monom;
-import jaicore.planning.graphgenerators.task.ceociptfd.EvaluablePredicate;
+import de.upb.crc901.mlplan.evaluablepredicates.mlplan.OptionsPredicate;
 
-public class AllowedTreeDepthForRandomForests implements EvaluablePredicate {
+public class AllowedTreeDepthForRandomForests extends OptionsPredicate {
 	
-	private static List<Integer> validValues = Arrays.asList(new Integer[]{1, 2, 3, 4, 5});
+	private static List<Integer> validValues = Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
 
 	@Override
-	public boolean test(Monom state, ConstantParam... params) {
-		return validValues.contains(Integer.valueOf(params[0].getName()));
-	}
-
-	@Override
-	public Collection<List<ConstantParam>> getParamsForPositiveEvaluation(Monom state, ConstantParam... partialGrounding) {
-		List<List<ConstantParam>> validGroundings = new ArrayList<>();
-		for (int value : validValues) {
-			List<ConstantParam> paramList = new ArrayList<>();
-			paramList.add(new ConstantParam("" + value));
-			validGroundings.add(paramList);
-		}
-		return validGroundings;
-	}
-
-	public Collection<List<ConstantParam>> getParamsForNegativeEvaluation(Monom state, ConstantParam... partialGrounding) {
-		return null;
-	}
-
-	@Override
-	public boolean isOracable() {
-		return true;
+	protected List<? extends Object> getValidValues() {
+		return validValues;
 	}
 }
--- src/de/upb/crc901/mlplan/evaluablepredicates/mlplan/randomforest/AllowedTreeEstimatorsForRandomForests.java
+++ src/de/upb/crc901/mlplan/evaluablepredicates/mlplan/randomforest/AllowedTreeEstimatorsForRandomForests.java
@@ -1,38 +1,22 @@
 package de.upb.crc901.mlplan.evaluablepredicates.mlplan.randomforest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import de.upb.crc901.mlplan.evaluablepredicates.mlplan.OptionsPredicate;
 import jaicore.logic.fol.structure.ConstantParam;
 import jaicore.logic.fol.structure.Monom;
 import jaicore.planning.graphgenerators.task.ceociptfd.EvaluablePredicate;
 
-public class AllowedTreeEstimatorsForRandomForests implements EvaluablePredicate {
+public class AllowedTreeEstimatorsForRandomForests extends OptionsPredicate {
+
+	private static List<Integer> validValues = Arrays.asList(new Integer[]{5, 10, 50, 100, 200, 500});
 
 	@Override
-	public boolean test(Monom state, ConstantParam... params) {
-		return false;
+	protected List<? extends Object> getValidValues() {
+		return validValues;
 	}
-
-	@Override
-	public Collection<List<ConstantParam>> getParamsForPositiveEvaluation(Monom state, ConstantParam... partialGrounding) {
-		List<List<ConstantParam>> validGroundings = new ArrayList<>();
-		int[] validValues = new int[]{5, 10, 50, 100, 200, 500};
-		for (int value : validValues) {
-			List<ConstantParam> paramList = new ArrayList<>();
-			paramList.add(new ConstantParam("" + value));
-			validGroundings.add(paramList);
-		}
-		return validGroundings;
-	}
-
-	public Collection<List<ConstantParam>> getParamsForNegativeEvaluation(Monom state, ConstantParam... partialGrounding) {
-		return null;
-	}
-
-	@Override
-	public boolean isOracable() {
-		return true;
-	}
+	
 }
