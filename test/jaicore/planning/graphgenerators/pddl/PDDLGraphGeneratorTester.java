39bed8b4edd4a1b2af02ae845f18524f5f845e87
Jörn <jkoepe@mail.upb.de>
2018-03-01 19:27:48 +0100

    PDDLGraphGenerator is functioning with versioned nodes

--- test/jaicore/planning/graphgenerators/pddl/PDDLGraphGeneratorTester.java
+++ test/jaicore/planning/graphgenerators/pddl/PDDLGraphGeneratorTester.java
@@ -25,26 +25,25 @@ public class PDDLGraphGeneratorTester {
 		File domain = null;
 		File problem = null;
 		
-//		JFileChooser chooser = new JFileChooser();
-//		chooser.setDialogTitle(" Open a Domain-File");
-//		chooser.showOpenDialog(null);
-//		domain = chooser.getSelectedFile();
-		domain = new File("F:/Desktop/pddl4j/pddl/blocksworld/domain.pddl");
-		System.out.println(domain.getAbsolutePath());
+		JFileChooser chooser = new JFileChooser();
+		chooser.setDialogTitle(" Open a Domain-File");
+		chooser.showOpenDialog(null);
+		domain = chooser.getSelectedFile();
+//		domain = new File("F:/Desktop/pddl4j/pddl/blocksworld/domain.pddl");
 		
-//		chooser.setDialogTitle("Open a Problme-File");
-//		chooser.showOpenDialog(null);
-//		problem = chooser.getSelectedFile();
-		problem = new File("F:/Desktop/pddl4j/pddl/blocksworld/p15.pddl");
+		chooser.setDialogTitle("Open a Problme-File");
+		chooser.showOpenDialog(null);
+		problem = chooser.getSelectedFile();
+//		problem = new File("F:/Desktop/pddl4j/pddl/blocksworld/p15.pddl");
 		
 		if(domain.exists() && problem.exists()) {
 			PDDLGraphGenerator gen = new PDDLGraphGenerator(domain, problem);
 		
 			//create a Astar-serch with the heuristic given pddl4j
-			AStar<Node,String> search = new AStar<>(gen, (s1,s2)-> 1.0, state ->(double)gen.getHeuristic().estimate(state.getPoint(),gen.getProblem().getGoal()),ParentDiscarding.ALL);
+			AStar<PDDLNode,String> search = new AStar<>(gen, (s1,s2)-> 1.0, state ->(double)gen.getHeuristic().estimate(state.getPoint().getNode(), gen.getProblem().getGoal()),ParentDiscarding.ALL);
 		
 			
-			List<Node> solution = search.nextSolution();
+			List<PDDLNode> solution = search.nextSolution();
 			assertNotNull(solution);
 		}
 		else
