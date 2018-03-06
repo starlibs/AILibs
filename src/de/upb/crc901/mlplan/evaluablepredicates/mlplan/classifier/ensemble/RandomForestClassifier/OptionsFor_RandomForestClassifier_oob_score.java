a0b4a6d009be3c8c7c85fab03bdef209de30392a
mwever <wever@mail.uni-paderborn.de>
2018-03-06 08:27:53 +0100

    Disabled oob score for RandomForestClassifier

--- src/de/upb/crc901/mlplan/evaluablepredicates/mlplan/classifier/ensemble/RandomForestClassifier/OptionsFor_RandomForestClassifier_oob_score.java
+++ src/de/upb/crc901/mlplan/evaluablepredicates/mlplan/classifier/ensemble/RandomForestClassifier/OptionsFor_RandomForestClassifier_oob_score.java
@@ -1,23 +1,15 @@
 package de.upb.crc901.mlplan.evaluablepredicates.mlplan.classifier.ensemble.RandomForestClassifier;
 
-    import java.util.Arrays;
-    import java.util.List;
+import de.upb.crc901.mlplan.evaluablepredicates.mlplan.DisabledOptionPredicate;
 
-    import de.upb.crc901.mlplan.evaluablepredicates.mlplan.OptionsPredicate;
-    /*
-        oob_score : bool (default=False)
-        Whether to use out-of-bag samples to estimate
-        the generalization accuracy.
+/*
+    oob_score : bool (default=False)
+    Whether to use out-of-bag samples to estimate
+    the generalization accuracy.
 
 
-    */
-    public class OptionsFor_RandomForestClassifier_oob_score extends OptionsPredicate {
-        
-        private static List<Object> validValues = Arrays.asList(new Object[]{"true"}); // default is false
+*/
+// Disabled this option due to dependency on bootstrap
+public class OptionsFor_RandomForestClassifier_oob_score extends DisabledOptionPredicate {
 
-        @Override
-        protected List<? extends Object> getValidValues() {
-            return validValues;
-        }
-    }
-    
+}
