b95bcf819688a3337ebb3ad76959efb05700dbb0
mwever <wever@mail.uni-paderborn.de>
2018-05-02 13:24:37 +0200

    Added toString to CategoricalParameter

--- src/hasco/model/CategoricalParameter.java
+++ src/hasco/model/CategoricalParameter.java
@@ -1,20 +1,28 @@
 package hasco.model;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class CategoricalParameter extends Parameter {
-	private final String[] values;
+  private final String[] values;
 
-	public CategoricalParameter(String name, String[] values, String defaultValue) {
-		super(name, defaultValue);
-		this.values = values;
-	}
-	
-	public CategoricalParameter(String name, Collection<String> values, String defaultValue) {
-		this(name, values.toArray(new String[] {}), defaultValue);
-	}
+  public CategoricalParameter(final String name, final String[] values, final String defaultValue) {
+    super(name, defaultValue);
+    this.values = values;
+  }
 
-	public String[] getValues() {
-		return values;
-	}
+  public CategoricalParameter(final String name, final Collection<String> values, final String defaultValue) {
+    this(name, values.toArray(new String[] {}), defaultValue);
+  }
+
+  public String[] getValues() {
+    return this.values;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(this.getName() + "(" + this.getDefaultValue() + "):" + Arrays.toString(this.values));
+    return sb.toString();
+  }
 }
--- src/hasco/model/ComponentInstance.java
+++ src/hasco/model/ComponentInstance.java
@@ -3,32 +3,43 @@ package hasco.model;
 import java.util.Map;
 
 /**
- * For a given component, a composition defines all parameter values and the required interfaces (recursively)
- * 
+ * For a given component, a composition defines all parameter values and the required interfaces
+ * (recursively)
+ *
  * @author fmohr
  *
  */
 public class ComponentInstance {
-	private final Component component;
-	private final Map<String, String> parameterValues;
-	private final Map<String, ComponentInstance> satisfactionOfRequiredInterfaces;
+  private final Component component;
+  private final Map<String, String> parameterValues;
+  private final Map<String, ComponentInstance> satisfactionOfRequiredInterfaces;
 
-	public ComponentInstance(Component component, Map<String, String> parameterValues, Map<String, ComponentInstance> satisfactionOfRequiredInterfaces) {
-		super();
-		this.component = component;
-		this.parameterValues = parameterValues;
-		this.satisfactionOfRequiredInterfaces = satisfactionOfRequiredInterfaces;
-	}
+  public ComponentInstance(final Component component, final Map<String, String> parameterValues, final Map<String, ComponentInstance> satisfactionOfRequiredInterfaces) {
+    super();
+    this.component = component;
+    this.parameterValues = parameterValues;
+    this.satisfactionOfRequiredInterfaces = satisfactionOfRequiredInterfaces;
+  }
 
-	public Component getComponent() {
-		return component;
-	}
+  public Component getComponent() {
+    return this.component;
+  }
 
-	public Map<String, String> getParameterValues() {
-		return parameterValues;
-	}
+  public Map<String, String> getParameterValues() {
+    return this.parameterValues;
+  }
 
-	public Map<String, ComponentInstance> getSatisfactionOfRequiredInterfaces() {
-		return satisfactionOfRequiredInterfaces;
-	}
+  public Map<String, ComponentInstance> getSatisfactionOfRequiredInterfaces() {
+    return this.satisfactionOfRequiredInterfaces;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(this.component);
+    sb.append(this.parameterValues);
+
+    return sb.toString();
+  }
 }
