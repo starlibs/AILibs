ca8837afbb43d15f83ced6da054b0ff486832862
Julian <Julian@mail.upb.de>
2018-06-13 16:59:15 +0200

    Integrates functionality (basic filters and factory implementation) to generate complex tree-structured feature engineering pipelines

---
+++ softwareconfiguration/autofe/src/autofe/algorithm/hasco/filter/meta/ForwardFilter.java
@@ -0,0 +1,13 @@
+package autofe.algorithm.hasco.filter.meta;
+
+import java.util.Collection;
+
+public class ForwardFilter implements IFilter {
+
+	@Override
+	public Collection<?> applyFilter(Collection<?> inputData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
