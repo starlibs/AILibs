99adc0901d9f51afdb8cafae031292999983739e
Mirko Juergens <mirkoj@mail.upb.de>
2018-06-18 10:02:57 +0200

    Changes the API to query arrays of intervals

--- JAICore/jaicore-ml/src/jaicore/ml/intervaltree/ExtendedRandomTree.java
+++ JAICore/jaicore-ml/src/jaicore/ml/intervaltree/ExtendedRandomTree.java
@@ -18,17 +18,18 @@ public class ExtendedRandomTree extends RandomTree{
 	
 	
 	
-	public void predictInterval(Interval queriedInterval) {
+	public void predictInterval(Interval[] queriedInterval) {
 		// the stack of elements that still have to be processed.
-		Deque<Entry<Interval, Tree>> stack = new ArrayDeque<>();
+		Deque<Entry<Interval[], Tree>> stack = new ArrayDeque<>();
 		// initially, the root and the queried interval
-		stack.push(new AbstractMap.SimpleEntry<Interval, Tree>(queriedInterval, m_Tree));
+		stack.push(new AbstractMap.SimpleEntry<Interval[], Tree>(queriedInterval, m_Tree));
 		//
 		ArrayList<Integer> list = new ArrayList<>();
 		while(stack.peek() != null) {
-			Entry<Interval, Tree> toProcess = stack.pop();
-			
-			InnerTree nextTree = (InnerTree) toProcess.getValue();
+			Entry<Interval[], Tree> toProcess = stack.pop();
+			Tree nextTree = toProcess.getValue();
+			double threshold = nextTree.getSplitPoint();
+			int attribute = nextTree.getAttribute();
 		}
 	}
 	
