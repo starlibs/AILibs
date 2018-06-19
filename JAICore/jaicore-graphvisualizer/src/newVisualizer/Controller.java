fadf800648f101d28c9e6094d5061f267bf879cf
jkoepe <jkoepe@mail.uni-paderborn.de>
2018-06-19 21:52:52 +0200

    fxml file gets loaded now. Somehow, the graph only starts in debug mode

--- JAICore/jaicore-graphvisualizer/src/newVisualizer/Controller.java
+++ JAICore/jaicore-graphvisualizer/src/newVisualizer/Controller.java
@@ -1,7 +1,30 @@
 package newVisualizer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
 	
 	
+    public void play(ActionEvent event){
+	    
+    }
 
+    public void step(ActionEvent actionEvent) {
+    }
+
+    public void back(ActionEvent actionEvent) {
+    }
+
+    public void reset(ActionEvent actionEvent) {
+    }
+
+    public void stop(ActionEvent actionEvent) {
+    }
+
+    public void save(ActionEvent actionEvent) {
+    }
+
+    public void load(ActionEvent actionEvent) {
+    }
 }
