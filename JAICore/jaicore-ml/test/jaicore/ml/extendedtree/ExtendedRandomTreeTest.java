21ca9a47ef6d3b14bff9879368827ffe9c4df838
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2018-07-10 22:22:48 +0200

    Add development branch with all experimental stuff

--- JAICore/jaicore-ml/test/jaicore/ml/extendedtree/ExtendedRandomTreeTest.java
+++ JAICore/jaicore-ml/test/jaicore/ml/extendedtree/ExtendedRandomTreeTest.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 
 import org.junit.Test;
 
@@ -23,12 +24,17 @@ public class ExtendedRandomTreeTest {
 			ArffReader arffReader = new ArffReader(reader);
 			Instances data = arffReader.getData();
 			data.setClassIndex(data.numAttributes() - 1);
+
 			
 			ExtendedRandomTree tree = new ExtendedRandomTree();
 			tree.setFeatureSpace(new FeatureSpace(data));
 			tree.buildClassifier(data);
 			tree.preprocess();
-			System.out.println(tree.computeMarginalForSingleFeature(2));
+			HashSet<Integer> subset = new HashSet<Integer>();
+			subset.add(2);
+			System.out.println("single new: " + 2 + ": " + tree.computeMarginalForSubsetOfFeatures(subset));
+			subset.add(3);
+			System.out.println("single new: " + 2 + ": " + tree.computeMarginalForSubsetOfFeatures(subset));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
