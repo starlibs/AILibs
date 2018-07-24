b16b37f710a742b02f5a79253dfc487511abbf2a
fischor <rudolftobiasfischer@gmail.com>
2018-07-24 21:15:45 +0200

    Fixes bug in compareTo method.

--- JAICore/jaicore-search/src/jaicore/search/algorithms/standard/rstar/RStarK.java
+++ JAICore/jaicore-search/src/jaicore/search/algorithms/standard/rstar/RStarK.java
@@ -1,5 +1,7 @@
 package jaicore.search.algorithms.standard.rstar;
 
+import scala.util.parsing.combinator.testing.Str;
+
 /**
  * k-Values (Priorities used for expansion from open.)
  */
@@ -15,18 +17,25 @@ public class RStarK implements Comparable<RStarK>{
 
     @Override
     /**
-     * Compare. Higher priority means lower RStarK.
-     * @return
+     * Compare to k-values i.e. provide a natural ordering for them.
+     * E.g.: [false, 0.9] < [false, 2.2] < [true, 0.1] < [true, 2.0]
+     *
+     * @return -1 if this < o, 0 iff this == o, +1 iff this > 0
      */
     public int compareTo(RStarK o) {
         // Compare first AVOID flag.
         if (!this.avoid && o.avoid) {
-            return +1;
+            return -1;
         }
         if (this.avoid && !o.avoid) {
-            return -1;
+            return +1;
         }
         // Then compare f-values.
         return Double.compare(this.f, o.f);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%b, %g]", this.avoid, this.f);
     }
 }
