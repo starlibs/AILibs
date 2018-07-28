7f08697792684df7f493950450a71ddf33a8bf0f
Felix Weiland <fweiland@mail.upb.de>
2018-07-28 14:20:45 +0200

    The path is now part of the name of a backward feature

--- softwareconfiguration/autofe/src/autofe/db/model/database/BackwardFeature.java
+++ softwareconfiguration/autofe/src/autofe/db/model/database/BackwardFeature.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import autofe.db.model.relation.AbstractRelationship;
+import autofe.db.model.relation.BackwardRelationship;
+import autofe.db.model.relation.ForwardRelationship;
 import autofe.db.util.Tuple;
 
 public class BackwardFeature extends AbstractFeature {
@@ -26,10 +28,34 @@ public class BackwardFeature extends AbstractFeature {
 
 	public void setPath(List<Tuple<AbstractRelationship, AggregationFunction>> path) {
 		this.path = path;
+		updateName();
 	}
 
 	public void addToPath(AbstractRelationship edge, AggregationFunction aggregationFunction) {
 		path.add(new Tuple<AbstractRelationship, AggregationFunction>(edge, aggregationFunction));
+		updateName();
+	}
+
+	private void updateName() {
+		if (path == null || path.isEmpty()) {
+			this.name = parent.getName();
+			return;
+		}
+
+		String parentTableName = path.get(0).getT().getToTableName();
+		String updatedName = String.format("%s.%s", parentTableName, parent.getName());
+
+		for (Tuple<AbstractRelationship, AggregationFunction> pathElement : path) {
+			if (pathElement.getT() instanceof BackwardRelationship) {
+				updatedName = String.format("%s.%s(%s)", pathElement.getT().getFromTableName(), pathElement.getU(),
+						updatedName);
+			} else if (pathElement.getT() instanceof ForwardRelationship) {
+				updatedName = String.format("%s<-(%s)", pathElement.getT().getFromTableName(), updatedName);
+			}
+		}
+
+		this.name = updatedName;
+
 	}
 
 	@Override
