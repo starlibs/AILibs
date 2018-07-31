6f3f21317477ad15fa3a52796d170a165b6140e9
Felix Weiland <fweiland@mail.upb.de>
2018-07-31 17:35:17 +0200

    OTFML-192: Removes obsolte sql statements + test

--- softwareconfiguration/autofe/src/autofe/db/sql/SqlStatement.java
+++ softwareconfiguration/autofe/src/autofe/db/sql/SqlStatement.java
@@ -2,17 +2,5 @@ package autofe.db.sql;
 
 public class SqlStatement {
 	
-	public static final String CREATE_VIEW_AS_COPY = "CREATE VIEW $1 AS SELECT * FROM $2";
 	
-	public static final String CREATE_TABLE_AS_COPY = "CREATE TABLE $1 AS SELECT * FROM $2";
-	
-	public static final String DELETE_VIEW = "DROP VIEW $1";
-	
-	public static final String DELETE_TABLE = "DROP TABLE $1";
-	
-	public static final String BACKWARD_AGGREGATION = "CREATE VIEW $1 AS SELECT f.*,t.AGG as '$2' FROM $3 f JOIN (SELECT $4,$5($6) AS AGG FROM $7 GROUP BY ($4) t ON (f.$4 = t.$4)";
-
-	public static final String FORWARD_JOIN = "CREATE VIEW $1 AS SELECT * FROM $2 NATURAL JOIN $3";
-	
-	public static final String LOAD_INSTANCES = "SELECT $1 FROM $2";
 }
--- softwareconfiguration/autofe/test/autofe/db/test/SqlUtilsTest.java
+++ softwareconfiguration/autofe/test/autofe/db/test/SqlUtilsTest.java
@@ -17,28 +17,6 @@ public class SqlUtilsTest {
 		assertEquals("SELECT * FROM Customer", finalStatement);
 	}
 
-	@Test
-	public void testBackwardStatement() {
-		String statement = "CREATE VIEW Orders_VIEW AS SELECT f.*,t.AGG as 'MAX(Product.Price)' FROM Orders_VIEW_TEMP f JOIN (SELECT OrderId,MAX(Price) AS AGG FROM Product_VIEW GROUP BY (OrderId) t ON (f.OrderId = t.OrderId)";
-
-		String rawStatement = SqlStatement.BACKWARD_AGGREGATION;
-		String fromViewName = "Orders_VIEW";
-		String aggregatedAttributeName = "MAX(Product.Price)";
-		String fromViewNameTemp = "Orders_VIEW_TEMP";
-		String commonAttribute = "OrderId";
-		String aggregationFunction = "MAX";
-		String toBeAggregated = "Price";
-		String toViewName = "Product_VIEW";
-
-		rawStatement = SqlUtils.replacePlaceholder(rawStatement, 1, fromViewName);
-		rawStatement = SqlUtils.replacePlaceholder(rawStatement, 2, aggregatedAttributeName);
-		rawStatement = SqlUtils.replacePlaceholder(rawStatement, 3, fromViewNameTemp);
-		rawStatement = SqlUtils.replacePlaceholder(rawStatement, 4, commonAttribute);
-		rawStatement = SqlUtils.replacePlaceholder(rawStatement, 5, aggregationFunction);
-		rawStatement = SqlUtils.replacePlaceholder(rawStatement, 6, toBeAggregated);
-		rawStatement = SqlUtils.replacePlaceholder(rawStatement, 7, toViewName);
-		
-		assertEquals(rawStatement, statement);
-	}
+	
 
 }
