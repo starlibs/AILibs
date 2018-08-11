9ca84615f4e4a6c46509ea72cef9998d0d584b69
jkoepe <jkoepe@mail.uni-paderborn.de>
2018-08-11 17:46:45 +0200

    Updated the build file to use graphstream 2.0-alpha
    Also added a test-file for the javafx-viewer of gs

--- JAICore/jaicore-graphvisualizer/src/jaicore/graphvisualizer/SearchVisualizationPanel.java
+++ JAICore/jaicore-graphvisualizer/src/jaicore/graphvisualizer/SearchVisualizationPanel.java
@@ -51,16 +51,16 @@ public class SearchVisualizationPanel<T> extends JPanel {
 	private List<T> roots;
 
 	private final Graph graph;
-	private final Viewer viewer;
-	private final View view;
-	private final ViewerPipe viewerPipe;
+//	private final Viewer viewer;
+//	private final View view;
+//	private final ViewerPipe viewerPipe;
 
 	private final Container graphContainer = new Container();
 	private final JFrame tooltipContainer = new JFrame();
 	private final JLabel tooltipLabel = new JLabel();
 	private final JScrollPane scrollPane = new JScrollPane(tooltipLabel);
 	private Timer tooltipTimer = null;
-	
+
 	private final ConcurrentMap<T, Node> ext2intNodeMap = new ConcurrentHashMap<>();
 	private final ConcurrentMap<Node, T> int2extNodeMap = new ConcurrentHashMap<>();
 
@@ -74,11 +74,11 @@ public class SearchVisualizationPanel<T> extends JPanel {
 		/* setup graphstream stuff */
 		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 		this.graph = new SingleGraph("Search Visualizer");
-		this.viewer = new Viewer(this.graph, ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-		this.viewer.enableAutoLayout();
-		this.view = this.viewer.addDefaultView(false);
-		final Camera cam = this.view.getCamera();
-		cam.setAutoFitView(true);
+//		this.viewer = new Viewer(this.graph, ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+//		this.viewer.enableAutoLayout();
+//		this.view = this.viewer.addDefaultView(false);
+//		final Camera cam = this.view.getCamera();
+//		cam.setAutoFitView(true);
 
 		/* add containers to canvas */
 		tooltipContainer.setLayout(new BorderLayout());
@@ -88,9 +88,9 @@ public class SearchVisualizationPanel<T> extends JPanel {
 		tooltipContainer.setSize(d);
 		tooltipContainer.getContentPane().add(scrollPane, BorderLayout.CENTER);
 		graphContainer.setLayout(new BorderLayout());
-		graphContainer.add((JPanel) this.view, BorderLayout.CENTER);
+//		graphContainer.add((JPanel) this.view, BorderLayout.CENTER);
 		add(graphContainer);
-		
+
 		/* add timer for disappearing tooltips */
 		this.tooltipTimer = new Timer(20000, new ActionListener() {
             @Override
@@ -101,11 +101,11 @@ public class SearchVisualizationPanel<T> extends JPanel {
 		this.tooltipTimer.setRepeats(false);
 
 		/* add listener for mouse events */
-		viewerPipe = this.viewer.newViewerPipe();
-		new PumpThread(viewerPipe, 50).start();
+//		viewerPipe = this.viewer.newViewerPipe();
+//		new PumpThread(viewerPipe, 50).start();
 
 		/* load css file for formatting the graph */
-		graph.addAttribute("ui.stylesheet", "url('conf/searchgraph.css')");
+//		graph.addAttribute("ui.stylesheet", "url('conf/searchgraph.css')");
 
 		/* attach a listener */
 		this.tooltipGenerator = new TooltipGenerator<T>() {
@@ -114,88 +114,88 @@ public class SearchVisualizationPanel<T> extends JPanel {
 				return node.toString();
 			}
 		};
-		this.viewerPipe.addViewerListener(new ViewerListener() {
-
-			@Override
-			public void viewClosed(String arg0) {
-			}
-
-			// @Override
-			// public void mouseOver(String arg0) {
-			// }
-			//
-			// @Override
-			// public void mouseLeft(String arg0) {
-			// }
-
-			@Override
-			public void buttonReleased(String arg0) {
-			}
-
-			@Override
-			public void buttonPushed(String arg0) {
-
-				Runnable doButtonPushedAction = new Runnable() {
-				    public void run() {
-
-				    	/* generate HTML for tooltip and set it */
-						StringBuilder sb = new StringBuilder();
-//						sb.append("<html><div style='padding: 5px; background: #ffffcc; border: 1px solid black;'>");
-						sb.append("<html><div style='padding: 5px;'>");
-						sb.append(SearchVisualizationPanel.this.tooltipGenerator.getTooltip(SearchVisualizationPanel.this.getNodeOfString(arg0)));
-						sb.append("</div></html>");
-						SearchVisualizationPanel.this.tooltipLabel.setText(sb.toString());
-
-						/* determine desired position for tooltip box */
-						final Point mousePosition = MouseInfo.getPointerInfo().getLocation();
-						SwingUtilities.convertPointFromScreen(mousePosition, SearchVisualizationPanel.this);
-						Dimension size;
-						final Dimension preferredSize = SearchVisualizationPanel.this.tooltipLabel.getPreferredSize();
-						if (preferredSize.getWidth() <= getWidth() * 0.5)
-							size = preferredSize;
-						else {
-							javax.swing.text.View view = (javax.swing.text.View) tooltipLabel.getClientProperty(javax.swing.plaf.basic.BasicHTML.propertyKey);
-							view.setSize((int) Math.round(getWidth() * 0.5), 0);
-							float w = view.getPreferredSpan(javax.swing.text.View.X_AXIS);
-							float h = view.getPreferredSpan(javax.swing.text.View.Y_AXIS);
-							size = new java.awt.Dimension((int) Math.ceil(w), (int) Math.ceil(h));
-						}
-						final Point position = new Point(mousePosition.x + 15, mousePosition.y);
-
-						/* check if the tooltip is partly outside the window on the bottom */
-						final int yOffset = getHeight() - size.height - position.y;
-						if (yOffset < 0) {
-							position.y = position.y + yOffset;
-						}
-
-						/* check if the tooltip is partly outside the window on the right side. */
-						final boolean rightOutside = position.x + size.width > getWidth();
-						if (rightOutside) {
-							position.x = mousePosition.x - 15 - size.width;
-						}
-//						SearchVisualizationPanel.this.tooltipLabel.setBounds(position.x, position.y, size.width, size.height);
-
-						SearchVisualizationPanel.this.scrollPane.getViewport().setViewPosition(new Point(0,0));
-						SearchVisualizationPanel.this.scrollPane.revalidate();
-						Dimension d = SearchVisualizationPanel.this.scrollPane.getPreferredSize();
-						d.height += 50;
-						d.width += 50;
-						if ( d.height > (SearchVisualizationPanel.this.tooltipContainer.getMaximumSize().height-50) )
-							d.height = (SearchVisualizationPanel.this.tooltipContainer.getMaximumSize().height-50);
-						if ( d.width > (SearchVisualizationPanel.this.tooltipContainer.getMaximumSize().width-50) )
-							d.width = (SearchVisualizationPanel.this.tooltipContainer.getMaximumSize().width-50);
-						SearchVisualizationPanel.this.tooltipContainer.setSize(d);
-						SearchVisualizationPanel.this.tooltipContainer.repaint();
-		            	SearchVisualizationPanel.this.tooltipContainer.setExtendedState(0);
-
-		            	SearchVisualizationPanel.this.tooltipContainer.setVisible(true);
-		            	SearchVisualizationPanel.this.tooltipTimer.restart();
-					}
-				};
-
-				SwingUtilities.invokeLater(doButtonPushedAction);
-			}
-		});
+//		this.viewerPipe.addViewerListener(new ViewerListener() {
+//
+//			@Override
+//			public void viewClosed(String arg0) {
+//			}
+//
+//			// @Override
+//			// public void mouseOver(String arg0) {
+//			// }
+//			//
+//			// @Override
+//			// public void mouseLeft(String arg0) {
+//			// }
+//
+//			@Override
+//			public void buttonReleased(String arg0) {
+//			}
+//
+//			@Override
+//			public void buttonPushed(String arg0) {
+//
+//				Runnable doButtonPushedAction = new Runnable() {
+//				    public void run() {
+//
+//				    	/* generate HTML for tooltip and set it */
+//						StringBuilder sb = new StringBuilder();
+////						sb.append("<html><div style='padding: 5px; background: #ffffcc; border: 1px solid black;'>");
+//						sb.append("<html><div style='padding: 5px;'>");
+//						sb.append(SearchVisualizationPanel.this.tooltipGenerator.getTooltip(SearchVisualizationPanel.this.getNodeOfString(arg0)));
+//						sb.append("</div></html>");
+//						SearchVisualizationPanel.this.tooltipLabel.setText(sb.toString());
+//
+//						/* determine desired position for tooltip box */
+//						final Point mousePosition = MouseInfo.getPointerInfo().getLocation();
+//						SwingUtilities.convertPointFromScreen(mousePosition, SearchVisualizationPanel.this);
+//						Dimension size;
+//						final Dimension preferredSize = SearchVisualizationPanel.this.tooltipLabel.getPreferredSize();
+//						if (preferredSize.getWidth() <= getWidth() * 0.5)
+//							size = preferredSize;
+//						else {
+//							javax.swing.text.View view = (javax.swing.text.View) tooltipLabel.getClientProperty(javax.swing.plaf.basic.BasicHTML.propertyKey);
+//							view.setSize((int) Math.round(getWidth() * 0.5), 0);
+//							float w = view.getPreferredSpan(javax.swing.text.View.X_AXIS);
+//							float h = view.getPreferredSpan(javax.swing.text.View.Y_AXIS);
+//							size = new java.awt.Dimension((int) Math.ceil(w), (int) Math.ceil(h));
+//						}
+//						final Point position = new Point(mousePosition.x + 15, mousePosition.y);
+//
+//						/* check if the tooltip is partly outside the window on the bottom */
+//						final int yOffset = getHeight() - size.height - position.y;
+//						if (yOffset < 0) {
+//							position.y = position.y + yOffset;
+//						}
+//
+//						/* check if the tooltip is partly outside the window on the right side. */
+//						final boolean rightOutside = position.x + size.width > getWidth();
+//						if (rightOutside) {
+//							position.x = mousePosition.x - 15 - size.width;
+//						}
+////						SearchVisualizationPanel.this.tooltipLabel.setBounds(position.x, position.y, size.width, size.height);
+//
+//						SearchVisualizationPanel.this.scrollPane.getViewport().setViewPosition(new Point(0,0));
+//						SearchVisualizationPanel.this.scrollPane.revalidate();
+//						Dimension d = SearchVisualizationPanel.this.scrollPane.getPreferredSize();
+//						d.height += 50;
+//						d.width += 50;
+//						if ( d.height > (SearchVisualizationPanel.this.tooltipContainer.getMaximumSize().height-50) )
+//							d.height = (SearchVisualizationPanel.this.tooltipContainer.getMaximumSize().height-50);
+//						if ( d.width > (SearchVisualizationPanel.this.tooltipContainer.getMaximumSize().width-50) )
+//							d.width = (SearchVisualizationPanel.this.tooltipContainer.getMaximumSize().width-50);
+//						SearchVisualizationPanel.this.tooltipContainer.setSize(d);
+//						SearchVisualizationPanel.this.tooltipContainer.repaint();
+//		            	SearchVisualizationPanel.this.tooltipContainer.setExtendedState(0);
+//
+//		            	SearchVisualizationPanel.this.tooltipContainer.setVisible(true);
+//		            	SearchVisualizationPanel.this.tooltipTimer.restart();
+//					}
+//				};
+//
+//				SwingUtilities.invokeLater(doButtonPushedAction);
+//			}
+//		});
 	}
 
 	protected synchronized Node newNode(final T newNodeExt) {
@@ -226,14 +226,14 @@ public class SearchVisualizationPanel<T> extends JPanel {
 //TODO		return this.graph.addEdge(edgeId, fromInt, toInt, true);
 		return this.graph.addEdge(edgeId, fromInt, toInt, false);
 	}
-	
+
 	protected synchronized boolean removeEdge(final T from, final T to) {
-		for(Edge e: graph.getEachEdge()) {
-			if (e.getSourceNode().equals(this.ext2intNodeMap.get(from)) && e.getTargetNode().equals(this.ext2intNodeMap.get(to))) {
-				graph.removeEdge(e);
-				return true;
-			}
-		}
+//		for(Edge e: graph.getEachEdge()) {
+//			if (e.getSourceNode().equals(this.ext2intNodeMap.get(from)) && e.getTargetNode().equals(this.ext2intNodeMap.get(to))) {
+//				graph.removeEdge(e);
+//				return true;
+//			}
+//		}
 		return false;
 	}
 
@@ -247,7 +247,7 @@ public class SearchVisualizationPanel<T> extends JPanel {
 			if (roots == null)
 				throw new IllegalArgumentException("Root must not be NULL");
 			newNode(roots.get(roots.size()-1));
-			ext2intNodeMap.get(roots.get(roots.size()-1)).addAttribute("ui.class", "root");
+//			ext2intNodeMap.get(roots.get(roots.size()-1)).addAttribute("ui.class", "root");
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -259,7 +259,7 @@ public class SearchVisualizationPanel<T> extends JPanel {
 			if (!ext2intNodeMap.containsKey(e.getNode()))
 				newNode(e.getNode());
 			newEdge(e.getParent(), e.getNode());
-			ext2intNodeMap.get(e.getNode()).addAttribute("ui.class", e.getType());
+//			ext2intNodeMap.get(e.getNode()).addAttribute("ui.class", e.getType());
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -272,27 +272,27 @@ public class SearchVisualizationPanel<T> extends JPanel {
 				return;
 			if (!ext2intNodeMap.containsKey(e.getNode()))
 				throw new NoSuchElementException("Cannot switch type of node " + e.getNode() + ". This node has not been reached previously.");
-			ext2intNodeMap.get(e.getNode()).addAttribute("ui.class", e.getType());
+//			ext2intNodeMap.get(e.getNode()).addAttribute("ui.class", e.getType());
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	@Subscribe
 	public synchronized void receivedNodeParentSwitchEvent(NodeParentSwitchEvent<T> e) {
 		try {
 			if(!ext2intNodeMap.containsKey(e.getNode()) && !ext2intNodeMap.containsKey(e.getNewParent()))
 				throw new NoSuchElementException("Cannot switch parent of node " + e.getNode()+". Either the node or the new parent node has not been reached previously.");
-			
+
 			removeEdge(e.getOldParent(), e.getNode());
 			newEdge(e.getNewParent(),e.getNode());
-			
+
 		}
 		catch(Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	@Subscribe
 	public synchronized void receiveNodeRemovedEvent(NodeRemovedEvent<T> e) {
 		try {
@@ -309,32 +309,32 @@ public class SearchVisualizationPanel<T> extends JPanel {
 	}
 
 	public void addNodeListener(NodeListener<T> listener) {
-		this.viewerPipe.addViewerListener(new ViewerListener() {
-
-			@Override
-			public void viewClosed(String arg0) {
-			}
-
-			// @Override
-			// public void mouseOver(String arg0) {
-			// listener.mouseOver(getNodeOfString(arg0));
-			// }
-			//
-			// @Override
-			// public void mouseLeft(String arg0) {
-			// listener.mouseLeft(getNodeOfString(arg0));
-			// }
-
-			@Override
-			public void buttonReleased(String arg0) {
-				listener.buttonReleased(getNodeOfString(arg0));
-			}
-
-			@Override
-			public void buttonPushed(String arg0) {
-				listener.buttonPushed(getNodeOfString(arg0));
-			}
-		});
+//		this.viewerPipe.addViewerListener(new ViewerListener() {
+//
+//			@Override
+//			public void viewClosed(String arg0) {
+//			}
+//
+//			// @Override
+//			// public void mouseOver(String arg0) {
+//			// listener.mouseOver(getNodeOfString(arg0));
+//			// }
+//			//
+//			// @Override
+//			// public void mouseLeft(String arg0) {
+//			// listener.mouseLeft(getNodeOfString(arg0));
+//			// }
+//
+//			@Override
+//			public void buttonReleased(String arg0) {
+//				listener.buttonReleased(getNodeOfString(arg0));
+//			}
+//
+//			@Override
+//			public void buttonPushed(String arg0) {
+//				listener.buttonPushed(getNodeOfString(arg0));
+//			}
+//		});
 	}
 
 	public TooltipGenerator<T> getTooltipGenerator() {
@@ -349,7 +349,7 @@ public class SearchVisualizationPanel<T> extends JPanel {
 		ext2intNodeMap.clear();
 		int2extNodeMap.clear();
 		graph.clear();
-		graph.addAttribute("ui.stylesheet", "url('conf/searchgraph.css')");
+//		graph.addAttribute("ui.stylesheet", "url('conf/searchgraph.css')");
 
 
 	}
--- JAICore/jaicore-graphvisualizer/src/jaicore/graphvisualizer/gui/Tester.java
+++ JAICore/jaicore-graphvisualizer/src/jaicore/graphvisualizer/gui/Tester.java
@@ -1,17 +1,51 @@
 package jaicore.graphvisualizer.gui;
 
+
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.stage.Stage;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.graph.Graph;
+import org.graphstream.ui.fx_viewer.FxViewPanel;
+import org.graphstream.ui.fx_viewer.FxViewer;
+import org.graphstream.ui.view.Viewer;
 
 public class Tester extends Application {
+
+    protected static String styleSheet =
+            "graph {"+
+                    "	padding: 60px;"+
+                    "}";
+
+
+    public static void main(String args[]) {
+        Application.launch(Tester.class, args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-        FXCode code = new FXCode();
-        code.open();
-        code.getTabPane().getTabs().add(new Tab("Test",new Button("test")));
+        Graph graph = new MultiGraph("mg");
 
+        FxViewer viewer = new FxViewer(graph, FxViewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+        graph.setAttribute( "ui.antialias" );
+        graph.setAttribute( "ui.quality" );
+        graph.setAttribute( "ui.stylesheet", styleSheet );
 
+        graph.getNode("A").setAttribute("xyz", -1, 0, 0 );
+        graph.getNode("B").setAttribute("xyz",  1, 0, 0 );
+        graph.getNode("C").setAttribute("xyz", 0, 1, 0 );
+
+        FxViewPanel v = (FxViewPanel) viewer.addDefaultView(false);
+        Scene scene = new Scene(v, 800, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 }
