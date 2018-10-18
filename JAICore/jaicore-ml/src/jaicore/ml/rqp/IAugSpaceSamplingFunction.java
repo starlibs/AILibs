404945cbda427dbe2fb70cd899fbafc2f0b7309e
Michael Braun <braunm@mail.uni-paderborn.de>
2018-10-18 15:05:51 +0200

    - Adds all pairs sampling function

---
+++ JAICore/jaicore-ml/src/jaicore/ml/rqp/IAugSpaceSamplingFunction.java
@@ -0,0 +1,9 @@
+package jaicore.ml.rqp;
+
+import com.google.common.base.Function;
+
+import weka.core.Instances;
+
+public interface IAugSpaceSamplingFunction extends Function<Instances, Instances> {
+
+}
