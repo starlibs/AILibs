6af01a5cfd3d3682d862fbbcaec486075eacf834
Helena Graf <Graf.Helena@web.de>
2018-10-18 17:53:06 +0200

    Add MLPipelineEval Cache (first version)

---
+++ softwareconfiguration/mlplan/src/de/upb/crc901/mlpipeline_evaluation/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Package containing classes for the evaluation of MLPipelines and the caching
+ * of those evaluations in a database.
+ * 
+ * @author Helena Graf
+ * @author Joshua
+ * @author Lukas
+ *
+ */
+package de.upb.crc901.mlpipeline_evaluation;
