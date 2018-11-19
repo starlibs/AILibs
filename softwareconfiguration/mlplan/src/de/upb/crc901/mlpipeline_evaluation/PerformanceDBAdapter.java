d5b7a7e2f54d918d4b299027c7247b8e1fda2eb7
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2018-11-19 21:06:59 +0100

    Work on PerformanceDBAdapter

--- softwareconfiguration/mlplan/src/de/upb/crc901/mlpipeline_evaluation/PerformanceDBAdapter.java
+++ softwareconfiguration/mlplan/src/de/upb/crc901/mlpipeline_evaluation/PerformanceDBAdapter.java
@@ -2,15 +2,18 @@ package de.upb.crc901.mlpipeline_evaluation;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.base.Optional;
 
 import hasco.model.ComponentInstance;
-import hasco.serialization.CompositionSerializer;
 import jaicore.basic.SQLAdapter;
 import jaicore.ml.cache.ReproducibleInstances;
 
@@ -42,12 +45,11 @@ public class PerformanceDBAdapter {
 			// if there is no performance table, create it
 			if (!hasPerformanceTable) {
 				System.out.println("Creating table for evaluations");
-				sqlAdapter.update(
-						"CREATE TABLE `" + this.performanceSampleTableName + "` (\r\n" + " `evaluation_id` int(10) NOT NULL AUTO_INCREMENT,\r\n"
-								+ " `composition` json NOT NULL,\r\n" + " `trajectory` json NOT NULL,\r\n"
-								+ " `score` double NOT NULL,\r\n" + " PRIMARY KEY (`evaluation_id`)\r\n"
-								+ ") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin",
-						new ArrayList<>());
+				sqlAdapter.update("CREATE TABLE `" + this.performanceSampleTableName + "` (\r\n"
+						+ " `evaluation_id` int(10) NOT NULL AUTO_INCREMENT,\r\n" + " `composition` json NOT NULL,\r\n"
+						+ " `trajectory` json NOT NULL,\r\n" + " `score` double NOT NULL,\r\n"
+						+ "`evaluation_date` timestamp NULL DEFAULT NULL," + " PRIMARY KEY (`evaluation_id`)\r\n"
+						+ ") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin", new ArrayList<>());
 			}
 
 		} catch (SQLException e) {
@@ -61,30 +63,62 @@ public class PerformanceDBAdapter {
 	 * evaluation specified by the reproducable instances. If so, it returns the
 	 * corresponding performance score.
 	 * 
+	 * 
 	 * @param composition
+	 *            - Solution composition.
 	 * @param reproducableInstances
-	 * @return
+	 *            - Instances object that includes the trajectory, i.e. all
+	 *            operations that have been applied to the instances like loading,
+	 *            splitting etc.
+	 * @return opt - Optional that contains the score corresponding to the
+	 *         composition and the reproducible instances or is empty if no suiting
+	 *         entry is found in the database.
 	 */
 	public Optional<Double> exists(ComponentInstance composition, ReproducibleInstances reproducableInstances) {
-		double result = 0.0;
-		Optional<Double> opt = Optional.of(result);
+		Optional<Double> opt = Optional.empty();
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			String compositionString = mapper.writeValueAsString(composition);
+			String trajectoryString = mapper.writeValueAsString(reproducableInstances);
+			ResultSet rs = sqlAdapter.getResultsOfQuery("SELECT score FROM " + this.performanceSampleTableName
+					+ " WHERE composition = " + compositionString + " AND trajcetory = " + trajectoryString);
+			if (rs.next()) {
+				double score = rs.getDouble("score");
+				opt = Optional.of(score);
+			}
+		} catch (JsonProcessingException | SQLException e) {
+			e.printStackTrace();
+		}
 		return opt;
 	}
 
 	/**
-	 * Stores the composition, the trajectory and the achieved score in the database.
+	 * Stores the composition, the trajectory and the achieved score in the
+	 * database.
 	 * 
 	 * @param composition
+	 *            - Solution composition
 	 * @param reproducableInstances
+	 *            - Instances object that includes the trajectory, i.e. all
+	 *            operations that have been applied to the instances like loading,
+	 *            splitting etc.
 	 * @param score
+	 *            - Score achieved by the composition on the reproducible instances
 	 */
 	public void store(ComponentInstance composition, ReproducibleInstances reproducableInstances, double score) {
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			String compositionString = mapper.writeValueAsString(composition);
 			String trajectoryString = mapper.writeValueAsString(reproducableInstances);
-			
-		} catch (JsonProcessingException e) {
+			Map<String, String> valueMap = new HashMap<>();
+			valueMap.put("composition", compositionString);
+			valueMap.put("trajectory", trajectoryString);
+			valueMap.put("evaluation_date",
+					new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date.from(Instant.now())));
+			// TOOD check if this works
+			valueMap.put("score", Double.toString(score));
+			this.sqlAdapter.insert(this.performanceSampleTableName, valueMap);
+		} catch (JsonProcessingException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
---
+++ softwareconfiguration/mlplan/test/de/upb/crc901/mlpipeline_evaluation/PerformanceDBAdapterTest.java
@@ -0,0 +1,14 @@
+package de.upb.crc901.mlpipeline_evaluation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PerformanceDBAdapterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
