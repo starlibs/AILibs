af68aca9bc9762f47b89365c802165fb4e17e994
Mirko Juergens <mirkoj@mail.upb.de>
2018-11-20 15:14:04 +0100

    First working version

---
+++ softwareconfiguration/mlplan/src/de/upb/crc901/mlpipeline_evaluation/CacheEvaluatorMeasureBridge.java
@@ -0,0 +1,67 @@
+package de.upb.crc901.mlpipeline_evaluation;
+
+import java.util.Optional;
+
+import hasco.model.ComponentInstance;
+import jaicore.ml.cache.ReproducibleInstances;
+import jaicore.ml.evaluation.evaluators.weka.AbstractEvaluatorMeasureBridge;
+import jaicore.ml.evaluation.evaluators.weka.SimpleEvaluatorMeasureBridge;
+import jaicore.ml.evaluation.measures.IMeasure;
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
+public class CacheEvaluatorMeasureBridge extends AbstractEvaluatorMeasureBridge<Double, Double> {
+
+	ComponentInstance evaluatedComponent;
+
+	/* Used for evaluating, when no cache entry could be found. */
+	SimpleEvaluatorMeasureBridge simpleEvaluatorMeasureBridge;
+
+	/* Used for looking up cache entries. */
+	PerformanceDBAdapter performanceDBAdapter;
+
+	public CacheEvaluatorMeasureBridge(IMeasure<Double, Double> basicEvaluator,
+			PerformanceDBAdapter performanceDBAdapter) {
+		super(basicEvaluator);
+		this.performanceDBAdapter = performanceDBAdapter;
+		this.simpleEvaluatorMeasureBridge = new SimpleEvaluatorMeasureBridge(basicEvaluator);
+	}
+
+	@Override
+	public Double evaluateSplit(Classifier pl, Instances trainingData, Instances validationData) throws Exception {
+		if (trainingData instanceof ReproducibleInstances) {
+			// check in the cache if the result exists already
+			Optional<Double> potentialCache = performanceDBAdapter.exists(evaluatedComponent,
+					(ReproducibleInstances) trainingData);
+			if (potentialCache.isPresent()) {
+				System.exit(0);
+				return potentialCache.get();
+			} else {
+				// query the underlying loss function
+				double performance = simpleEvaluatorMeasureBridge.evaluateSplit(pl, trainingData, validationData);
+				// cache it
+				if (trainingData instanceof ReproducibleInstances) {
+					performanceDBAdapter.store(evaluatedComponent, (ReproducibleInstances) trainingData, performance);
+				}
+				return performance;
+			}
+		} else {
+			return simpleEvaluatorMeasureBridge.evaluateSplit(pl, trainingData, validationData);
+		}
+	}
+
+	/**
+	 * Returns a lightweight copy of this object. That is, that the database
+	 * connection stays established and only the component instace is updated.
+	 * 
+	 * @param componentInstance
+	 * @return
+	 */
+	public CacheEvaluatorMeasureBridge getShallowCopy(ComponentInstance componentInstance) {
+		CacheEvaluatorMeasureBridge bridge = new CacheEvaluatorMeasureBridge(this.basicEvaluator,
+				this.performanceDBAdapter);
+		bridge.evaluatedComponent = componentInstance;
+		return bridge;
+	}
+
+}
