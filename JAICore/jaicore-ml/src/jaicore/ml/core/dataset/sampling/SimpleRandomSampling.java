ad3182e593753a5fbf7054e542663e10bb2bb326
Felix Weiland <fweiland@mail.upb.de>
2018-12-05 23:50:41 +0100

    SCALE-2: Added empty classes for the algorithm and the corresponding test class

---
+++ JAICore/jaicore-ml/src/jaicore/ml/core/dataset/sampling/SimpleRandomSampling.java
@@ -0,0 +1,26 @@
+package jaicore.ml.core.dataset.sampling;
+
+import jaicore.basic.algorithm.AlgorithmEvent;
+import jaicore.ml.core.dataset.IDataset;
+
+public class SimpleRandomSampling extends ASamplingAlgorithm {
+
+	@Override
+	public AlgorithmEvent nextWithException() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDataset call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSampleSize(int size) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JAICore/jaicore-ml/test/jaicore/ml/core/dataset/sampling/SimpleRandomSamplingTester.java
@@ -0,0 +1,33 @@
+package jaicore.ml.core.dataset.sampling;
+
+import jaicore.basic.algorithm.AlgorithmProblemTransformer;
+import jaicore.basic.algorithm.IAlgorithmFactory;
+import jaicore.ml.core.dataset.IDataset;
+
+public class SimpleRandomSamplingTester extends GeneralSamplingTester{
+
+	@Override
+	public AlgorithmProblemTransformer<Object, IDataset> getProblemReducer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IAlgorithmFactory<IDataset, IDataset> getFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDataset getSimpleProblemInputForGeneralTestPurposes() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDataset getDifficultProblemInputForGeneralTestPurposes() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
