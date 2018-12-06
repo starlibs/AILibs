65fe56c12822a27b762e569185ba503fff9cf34a
Helena Graf <Helena Graf@131.234.67.176>
2018-12-06 13:25:14 +0100

    Add optimizing interfaces

---
+++ JAICore/jaicore-ml/src/jaicore/ml/dyadranking/optimizing/IGradientFunction.java
@@ -0,0 +1,24 @@
+package jaicore.ml.dyadranking.optimizing;
+
+import de.upb.isys.linearalgebra.Vector;
+
+/**
+ * Represents the gradient of a function that is differentiable.
+ * 
+ * @author Helena Graf, Mirko Jürgens
+ *
+ */
+@FunctionalInterface
+public interface IGradientFunction {
+
+	/**
+	 * Returns the result of applying the gradient to the point represented by the
+	 * given vector.
+	 * 
+	 * @param vector
+	 *            the vector the gradient is applied to
+	 * @return the new vector resulting from applying the gradient to the given
+	 *         vector
+	 */
+	Vector apply(Vector vector);
+}
