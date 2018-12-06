194adb15e8d3799ec762a52ec6231d0953361b26
HELEN-LAPTOP\Helen <helebeen@mail.upb.de>
2018-12-06 19:26:48 +0100

    begin z transformer implemantation

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/zTransfrom.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/zTransfrom.java
@@ -3,6 +3,8 @@
  */
 package jaicore.ml.tsc.filter;
 
+import java.util.ArrayList;
+
 import jaicore.ml.core.dataset.IDataset;
 
 /**
@@ -10,22 +12,32 @@ import jaicore.ml.core.dataset.IDataset;
  *
  */
 public class zTransfrom implements IFilter {
+	
+	private ArrayList<Double> mean = new ArrayList<Double>();
+	private ArrayList<Double> derivation = new ArrayList<Double>();
+	
+	public ArrayList<Double> getMean() {
+		return mean;
+	}
+
+	public ArrayList<Double> getDerivation() {
+		return derivation;
+	}
 
 	/* (non-Javadoc)
 	 * @see jaicore.ml.tsc.filter.IFilter#transform(jaicore.ml.core.dataset.IDataset)
 	 */
 	@Override
 	public IDataset transform(IDataset input) throws IllegalArgumentException{
+		
+		ArrayList<double[]> tsToBeNormalized = new ArrayList<double[]>();
+		
 		if(input.isEmpty()) {
 			throw new IllegalArgumentException("The input dataset was empty");
 		}
 		
-		int length = input.getAttributeTypes().size();
-		for( int i = 0; i< length; i++) {
-			
-		if(true){
-				
-			}
+		for(int i = 0; i < input.size(); i++) {
+			tsToBeNormalized.add(input.get(i).getAsDoubleVector());			
 		}
 		return null;
 	}
