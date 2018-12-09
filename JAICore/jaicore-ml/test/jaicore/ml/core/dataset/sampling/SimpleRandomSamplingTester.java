7a09361248a73164b7396e72514d227cca09e37c
Felix Weiland <fweiland@mail.upb.de>
2018-12-09 22:08:41 +0100

    SCALE-2: Implemented algorithm factory in the SimpleRandomSamplingTester

--- JAICore/jaicore-ml/test/jaicore/ml/core/dataset/sampling/SimpleRandomSamplingTester.java
+++ JAICore/jaicore-ml/test/jaicore/ml/core/dataset/sampling/SimpleRandomSamplingTester.java
@@ -1,10 +1,16 @@
 package jaicore.ml.core.dataset.sampling;
 
+import java.util.Random;
+
+import jaicore.basic.algorithm.AAlgorithm;
 import jaicore.basic.algorithm.AlgorithmProblemTransformer;
+import jaicore.basic.algorithm.IAlgorithm;
 import jaicore.basic.algorithm.IAlgorithmFactory;
 import jaicore.ml.core.dataset.IDataset;
 
-public class SimpleRandomSamplingTester extends GeneralSamplingTester{
+public class SimpleRandomSamplingTester extends GeneralSamplingTester {
+
+	private static final long RANDOM_SEED = 1;
 
 	@Override
 	public AlgorithmProblemTransformer<Object, IDataset> getProblemReducer() {
@@ -14,8 +20,30 @@ public class SimpleRandomSamplingTester extends GeneralSamplingTester{
 
 	@Override
 	public IAlgorithmFactory<IDataset, IDataset> getFactory() {
-		// TODO Auto-generated method stub
-		return null;
+		return new IAlgorithmFactory<IDataset, IDataset>() {
+
+			private IDataset input;
+
+			@Override
+			public void setProblemInput(IDataset problemInput) {
+				this.input = problemInput;
+			}
+
+			@Override
+			public <P> void setProblemInput(P problemInput, AlgorithmProblemTransformer<P, IDataset> reducer) {
+				throw new UnsupportedOperationException("Problem input not applicable for subsampling algorithms!");
+			}
+
+			@Override
+			public IAlgorithm<IDataset, IDataset> getAlgorithm() {
+				Random r = new Random(RANDOM_SEED);
+				AAlgorithm<IDataset, IDataset> algorithm = new SimpleRandomSampling(r);
+				if (this.input != null) {
+					algorithm.setInput(input);
+				}
+				return algorithm;
+			}
+		};
 	}
 
 	@Override
