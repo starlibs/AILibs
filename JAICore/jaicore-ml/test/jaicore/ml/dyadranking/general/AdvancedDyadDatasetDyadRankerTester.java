64c8965d0306ada1c7e3e422be534d42a93a185f
Mirko Juergens <mirkoj@mail.upb.de>
2018-12-11 17:51:46 +0100

    adds seeds

--- JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/AdvancedDyadDatasetDyadRankerTester.java
+++ JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/AdvancedDyadDatasetDyadRankerTester.java
@@ -1,28 +1,20 @@
 package jaicore.ml.dyadranking.general;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import de.upb.isys.linearalgebra.DenseDoubleVector;
-import de.upb.isys.linearalgebra.Vector;
 import jaicore.ml.core.exception.PredictionException;
 import jaicore.ml.core.exception.TrainingException;
 import jaicore.ml.dyadranking.Dyad;
 import jaicore.ml.dyadranking.algorithm.ADyadRanker;
 import jaicore.ml.dyadranking.algorithm.FeatureTransformPLDyadRanker;
-import jaicore.ml.dyadranking.dataset.DyadRankingDataset;
-import jaicore.ml.dyadranking.dataset.DyadRankingInstance;
 import jaicore.ml.dyadranking.dataset.IDyadRankingInstance;
-import jaicore.ml.dyadranking.dataset.SparseDyadRankingInstance;
 
 /**
  * Class that runs a simple functionality check on all dyad rankers.
@@ -34,6 +26,8 @@ import jaicore.ml.dyadranking.dataset.SparseDyadRankingInstance;
 public class AdvancedDyadDatasetDyadRankerTester {
 
 	ADyadRanker ranker;
+
+	int seedTest = 60;
 
 	public AdvancedDyadDatasetDyadRankerTester(ADyadRanker ranker) {
 		this.ranker = ranker;
@@ -48,19 +42,19 @@ public class AdvancedDyadDatasetDyadRankerTester {
 	public void testSwapOrdering1() throws PredictionException {
 		System.out.println("Now testing ordering");
 
-		IDyadRankingInstance test = DyadRankingInstanceSupplier.getDyadRankingInstance(100);
+		IDyadRankingInstance test = DyadRankingInstanceSupplier.getDyadRankingInstance(100, seedTest);
 		IDyadRankingInstance predict = ranker.predict(test);
 
 		Dyad currentMin = predict.getDyadAtPosition(0);
 		int failures = 0;
 		for (Dyad dyad : predict) {
-			if (!(DyadRankingInstanceSupplier.complexDyadRanker().compare(currentMin, dyad) <= 0)) {		
+			if (!(DyadRankingInstanceSupplier.complexDyadRanker().compare(currentMin, dyad) <= 0)) {
 				failures++;
-			} 
+			}
 			currentMin = dyad;
 		}
-		System.out.println("Found failures: "+ failures);
-		
+		System.out.println("Found failures: " + failures);
+
 	}
 
 	@Parameters
--- JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/DyadRankingInstanceSupplier.java
+++ JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/DyadRankingInstanceSupplier.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 import de.upb.isys.linearalgebra.Vector;
 import jaicore.ml.dyadranking.Dyad;
 import jaicore.ml.dyadranking.dataset.DyadRankingDataset;
 import jaicore.ml.dyadranking.dataset.DyadRankingInstance;
+import jaicore.ml.dyadranking.dataset.SparseDyadRankingInstance;
 
 /**
  * Creates simple rankings for testing purposes.
@@ -24,11 +25,12 @@ public class DyadRankingInstanceSupplier {
 	 * @param maxLength the amount of dyads
 	 * @return
 	 */
-	public static DyadRankingInstance getDyadRankingInstance(int maxLength) {
+	public static DyadRankingInstance getDyadRankingInstance(int maxLength, int seed) {
 		List<Dyad> dyads = new ArrayList<Dyad>();
-		int actualLength = ThreadLocalRandom.current().nextInt(2,maxLength+1);
+		
+		int actualLength = new Random(seed).nextInt(maxLength+1);
 		for(int i = 0; i < actualLength; i++) {
-			Dyad dyad = DyadSupplier.getRandomDyad(2, 2);
+			Dyad dyad = DyadSupplier.getRandomDyad(i, 2, 2);
 			dyads.add(dyad);
 		}
 		Comparator<Dyad> comparator = complexDyadRanker();
@@ -55,7 +57,7 @@ public class DyadRankingInstanceSupplier {
 	public static DyadRankingDataset getDyadRankingDataset(int maxLengthDyadRankingInstance, int length) {
 		DyadRankingDataset dataset = new DyadRankingDataset();
 		for(int i = 0; i < length; i++) {
-			dataset.add(DyadRankingInstanceSupplier.getDyadRankingInstance(maxLengthDyadRankingInstance));
+			dataset.add(DyadRankingInstanceSupplier.getDyadRankingInstance(maxLengthDyadRankingInstance, i));
 		}
 		return dataset;
 	}
