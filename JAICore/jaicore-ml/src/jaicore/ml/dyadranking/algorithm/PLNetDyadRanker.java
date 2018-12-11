876f8bd48fffab10cb409ddcb5040d0dd85aba34
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2018-12-11 10:33:30 +0100

    Add prediction methods

--- JAICore/jaicore-ml/src/jaicore/ml/dyadranking/algorithm/PLNetDyadRanker.java
+++ JAICore/jaicore-ml/src/jaicore/ml/dyadranking/algorithm/PLNetDyadRanker.java
@@ -1,5 +1,8 @@
 package jaicore.ml.dyadranking.algorithm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
@@ -20,12 +23,13 @@ import jaicore.ml.core.dataset.IDataset;
 import jaicore.ml.core.dataset.IInstance;
 import jaicore.ml.core.exception.PredictionException;
 import jaicore.ml.core.exception.TrainingException;
+import jaicore.ml.dyadranking.Dyad;
 import jaicore.ml.dyadranking.dataset.DyadRankingDataset;
 import jaicore.ml.dyadranking.dataset.DyadRankingInstance;
 import jaicore.ml.dyadranking.dataset.IDyadRankingInstance;
 
 /**
- * A dyad ranker based on a Placket-Luce network.
+ * A dyad ranker based on a Plackett-Luce network.
  * 
  * @author Helena Graf, Jonas Hanselle
  *
@@ -40,10 +44,10 @@ public class PLNetDyadRanker extends APLDyadRanker {
 	 * 
 	 * @param learningRate
 	 */
-	public PLNetDyadRanker(double learningRate) {
+	public PLNetDyadRanker(double learningRate, int numInputs, int numHiddenNodes, long seed) {
 		this.learningRate = learningRate;
 		this.plNetLoss = new PLNetLoss();
-		this.plNet = this.createNetwork(15, 15, 1, this.plNetLoss);
+		this.plNet = this.createNetwork(numInputs, numHiddenNodes, seed, this.plNetLoss);
 		this.plNetLoss.setPlNet(this.plNet);
 	}
 
@@ -51,10 +55,10 @@ public class PLNetDyadRanker extends APLDyadRanker {
 	public void train(IDataset dataset) throws TrainingException {
 		if (!(dataset instanceof DyadRankingDataset)) {
 			throw new IllegalArgumentException(
-					"Can only train the Placket-Luce net dyad ranker with a dyad ranking dataset!");
+					"Can only train the Plackett-Luce net dyad ranker with a dyad ranking dataset!");
 		}
-		DyadRankingDataset dRDataset = (DyadRankingDataset) dataset;
-		for (IInstance dyadRankingInstance : dRDataset) {
+		DyadRankingDataset drDataset = (DyadRankingDataset) dataset;
+		for (IInstance dyadRankingInstance : drDataset) {
 			this.update(dyadRankingInstance);
 		}
 	}
@@ -62,23 +66,28 @@ public class PLNetDyadRanker extends APLDyadRanker {
 	public void update(IInstance instance) throws TrainingException {
 		if (!(instance instanceof IDyadRankingInstance)) {
 			throw new IllegalArgumentException(
-					"Can only update the Placket-Luce net dyad ranker with a dyad ranking instance!");
+					"Can only update the Plackett-Luce net dyad ranker with a dyad ranking instance!");
 		}
 
 		// TODO
 
-		DyadRankingInstance dRInstance = (DyadRankingInstance) instance;
+		DyadRankingInstance drInstance = (DyadRankingInstance) instance;
+		// init weight update vector
 		INDArray deltaW = Nd4j.zeros(plNet.numParams());
-		plNetLoss.preComputeOutputs(dRInstance);
-		for (int k = 0; k < dRInstance.length(); k++) {
+		plNetLoss.preComputeOutputs(drInstance);
+		for (int k = 0; k < drInstance.length(); k++) {
 			plNetLoss.setK(k);
+			// compute derivative of loss w.r.t. k
 			INDArray currentOutputGradient = plNetLoss.computeGradient(null, null, null, null);
+			// compute backprop gradient for weight updates w.r.t. k
 			Pair<Gradient, INDArray> p = plNet.backpropGradient(currentOutputGradient, null);
 			Gradient gradient = p.getFirst();
 			plNet.getUpdater().update(plNet, gradient, 0, 0, 0, null);
 			INDArray deltaWk = gradient.gradient();
+			// add it
 			deltaW.add(deltaWk);
 		}
+		//
 		INDArray update = deltaW.mul(this.learningRate);
 		plNet.params().subi(update);
 	}
@@ -90,14 +99,39 @@ public class PLNetDyadRanker extends APLDyadRanker {
 
 	@Override
 	public IDyadRankingInstance predict(IInstance instance) throws PredictionException {
-		// TODO Auto-generated method stub
-		return null;
+		if (!(instance instanceof IDyadRankingInstance)) {
+			throw new IllegalArgumentException(
+					"Can only make prediction for dyad ranking instances using the Plackett-Luce net dyad ranker!");
+		}
+		DyadRankingInstance drInstance = (DyadRankingInstance) instance;
+		List<Pair<Dyad, Double>> dyadUtilityPairs = new ArrayList<Pair<Dyad, Double>>(drInstance.length());
+		for (Dyad dyad : drInstance) {
+			INDArray instanceOfDyad = Nd4j.create(dyad.getInstance().asArray());
+			INDArray alternativeOfDyad = Nd4j.create(dyad.getAlternative().asArray());
+			INDArray plNetInput = Nd4j.hstack(instanceOfDyad, alternativeOfDyad);
+			double plNetOutput = plNet.output(plNetInput).getDouble(0);
+			dyadUtilityPairs.add(new Pair<Dyad, Double>(dyad, plNetOutput));
+		}
+		// sort the instance in descending order of utility values
+		Collections.sort(dyadUtilityPairs, Comparator.comparing(p -> p.getRight()));
+		List<Dyad> ranking = new ArrayList<Dyad>();
+		for (Pair<Dyad, Double> pair : dyadUtilityPairs)
+			ranking.add(pair.getLeft());
+		return new DyadRankingInstance(ranking);
 	}
 
 	@Override
 	public List<IDyadRankingInstance> predict(IDataset dataset) throws PredictionException {
-		// TODO Auto-generated method stub
-		return null;
+		if (!(dataset instanceof DyadRankingDataset)) {
+			throw new IllegalArgumentException(
+					"Can only make predictions for dyad ranking datasets using the Plackett-Luce net dyad ranker!");
+		}
+		DyadRankingDataset drDataset = (DyadRankingDataset) dataset;
+		List<IDyadRankingInstance> results = new ArrayList<IDyadRankingInstance>(dataset.size());
+		for (IInstance instance : drDataset) {
+			results.add(this.predict(instance));
+		}
+		return results;
 	}
 
 	/**
