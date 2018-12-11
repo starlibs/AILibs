f445b3d878bdb61ca1b11741fb750252d70c5c80
Helena Graf <Helena Graf@131.234.79.145>
2018-12-11 16:26:47 +0100

    Add tests for dyad ranker & fix dyad ranker so that tests work
    adds jonas advanced test

--- JAICore/jaicore-ml/src/jaicore/ml/dyadranking/algorithm/IDyadFeatureTransform.java
+++ JAICore/jaicore-ml/src/jaicore/ml/dyadranking/algorithm/IDyadFeatureTransform.java
@@ -21,4 +21,15 @@ public interface IDyadFeatureTransform {
 	 */
 	public Vector transform(Dyad dyad);
 
+	/**
+	 * Get the length of the vector returned by the transform method.
+	 * 
+	 * @param alternativeLength
+	 *            the length of the alternative vector of the transformed dyad
+	 * @param instanceLength
+	 *            the length of the instance vector of the transformed dyad
+	 * @return the length of the transformed feature vector
+	 */
+	public int getTransformedVectorLength(int alternativeLength, int instanceLength);
+
 }
---
+++ JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/DyadRankingInstanceSupplier.java
@@ -0,0 +1,63 @@
+package jaicore.ml.dyadranking.general;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import de.upb.isys.linearalgebra.Vector;
+import jaicore.ml.dyadranking.Dyad;
+import jaicore.ml.dyadranking.dataset.DyadRankingDataset;
+import jaicore.ml.dyadranking.dataset.DyadRankingInstance;
+
+/**
+ * Creates simple rankings for testing purposes.
+ * 
+ * @author Jonas Hanselle, Mirko Jürgens, Helena Graf, Michael Braun
+ *
+ */
+public class DyadRankingInstanceSupplier {
+
+	/**
+	 * Creates a random dyad ranking instance (with 2 alternatives and 2 instances)
+	 * @param maxLength the amount of dyads
+	 * @return
+	 */
+	public static DyadRankingInstance getDyadRankingInstance(int maxLength) {
+		List<Dyad> dyads = new ArrayList<Dyad>();
+		int actualLength = ThreadLocalRandom.current().nextInt(2,maxLength+1);
+		for(int i = 0; i < actualLength; i++) {
+			Dyad dyad = DyadSupplier.getRandomDyad(2, 2);
+			dyads.add(dyad);
+		}
+		Comparator<Dyad> comparator = complexDyadRanker();
+		Collections.sort(dyads, comparator);
+		return new DyadRankingInstance(dyads);
+	}
+
+	public static Comparator<Dyad> complexDyadRanker() {
+		Comparator<Dyad> comparator = new Comparator<Dyad>() {
+			@Override
+			public int compare(Dyad d1, Dyad d2) {
+				Vector scoreVecI = d1.getInstance();
+				Vector scoreVecA = d1.getAlternative();
+				Vector scoreVecI2 = d2.getInstance();
+				Vector scoreVecA2 = d2.getAlternative();
+				double score1 = Math.pow(scoreVecI.getValue(0), 2) + Math.pow(scoreVecI.getValue(1), 2) - Math.pow(scoreVecA.getValue(0), 2) - Math.pow(scoreVecA.getValue(1), 2);
+				double score2 = Math.pow(scoreVecI2.getValue(0), 2) + Math.pow(scoreVecI2.getValue(1), 2) - Math.pow(scoreVecA2.getValue(0), 2) - Math.pow(scoreVecA2.getValue(1), 2);
+				return score1 - score2 == 0 ? 0 : (score1 - score2 > 0 ? 1 : -1);
+			}
+		};
+		return comparator;
+	}
+	
+	public static DyadRankingDataset getDyadRankingDataset(int maxLengthDyadRankingInstance, int length) {
+		DyadRankingDataset dataset = new DyadRankingDataset();
+		for(int i = 0; i < length; i++) {
+			dataset.add(DyadRankingInstanceSupplier.getDyadRankingInstance(maxLengthDyadRankingInstance));
+		}
+		return dataset;
+	}
+
+}
