55615d6fffb8cdd583b0d334cf3403556721c6b3
Helen Beierling <helebeen@mail.upb.de>
2018-12-12 18:08:52 +0100

    finish z transform and ppa transform and geather questions for Sax finish

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/zTransfrom.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/zTransfrom.java
@@ -4,8 +4,17 @@
 package jaicore.ml.tsc.filter;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 import jaicore.ml.core.dataset.IDataset;
+import jaicore.ml.core.dataset.attribute.IAttributeType;
+import jaicore.ml.core.dataset.attribute.IAttributeValue;
+import jaicore.ml.core.dataset.attribute.timeseries.TimeSeriesAttributeType;
+import jaicore.ml.core.dataset.attribute.timeseries.TimeSeriesAttributeValue;
+
 
 /**
  * @author Helen
@@ -13,42 +22,99 @@ import jaicore.ml.core.dataset.IDataset;
  */
 public class zTransfrom implements IFilter {
 	
-	private ArrayList<Double> mean = new ArrayList<Double>();
-	private ArrayList<Double> derivation = new ArrayList<Double>();
+	private INDArray means;
+	private INDArray deviation;
+	private ArrayList<Integer> index = new ArrayList<Integer>();
+
 	
-	public ArrayList<Double> getMean() {
-		return mean;
-	}
-
-	public ArrayList<Double> getDerivation() {
-		return derivation;
-	}
-
+	//TODO alle methoden static ? 
+	
 	/* (non-Javadoc)
 	 * @see jaicore.ml.tsc.filter.IFilter#transform(jaicore.ml.core.dataset.IDataset)
 	 */
 	@Override
 	public IDataset transform(IDataset input) throws IllegalArgumentException{
 		
-		ArrayList<double[]> tsToBeNormalized = new ArrayList<double[]>();
-		
 		if(input.isEmpty()) {
 			throw new IllegalArgumentException("The input dataset was empty");
 		}
 		
+//		if(!(input instanceof TimeSeriesDataset)) {
+//			throw new IllegalArgumentException("This method only works with timeseries datasets");
+//		}
+//		
+		fit(input);
+		
 		for(int i = 0; i < input.size(); i++) {
-			tsToBeNormalized.add(input.get(i).getAsDoubleVector());			
+			for(int j = 0 ; j< index.size();j++) {
+				IAttributeValue<TimeSeriesAttributeValue> val = input.get(i).getAttributeValue(index.get(j),TimeSeriesAttributeValue.class);
+				INDArray timeseries = val.getValue().getValue();
+				for(int k = 0; k < timeseries.length(); k++) {
+					double normalizedValue = (timeseries.getDouble(k)-means.getDouble(i, j))/deviation.getDouble(i,j);
+					timeseries.putScalar(k, normalizedValue);
+				}
+				//TODO new dataset or old one
+				val.getValue().setValue(timeseries);
+			}
 		}
-		return null;
+		
+		return input;
 	}
 
 	/* (non-Javadoc)
 	 * @see jaicore.ml.tsc.filter.IFilter#fit(jaicore.ml.core.dataset.IDataset)
 	 */
+	
 	@Override
 	public void fit(IDataset input) {
-		// TODO Auto-generated method stub
+		
+		
+		List<IAttributeType<?>> attributes = input.getAttributeTypes();
+		
+		for(int i = 0; i< attributes.size() ; i++) {
+			if( attributes.get(i) instanceof TimeSeriesAttributeType) {
+				index.add(i);
+			}
+		}
+		
+		means = Nd4j.valueArrayOf(new int[] {index.size(),input.size()}, 0);
+		deviation = Nd4j.valueArrayOf(new int[] {index.size(),input.size()}, 0);
+		
+		// get all timeseries and calculate mean and deviation 
+		for(int i = 0; i < input.size(); i++) {
+			for(int j = 0 ; j< index.size();j++) {
+				IAttributeValue<TimeSeriesAttributeValue> val = input.get(i).getAttributeValue(index.get(j),TimeSeriesAttributeValue.class);
+				INDArray timeseries = val.getValue().getValue();
+				
+				double meanval = mean(timeseries); 
+				double deviationval = deviation(timeseries, meanval);
+				
+				means.putScalar(new int[] {i,j}, meanval);
+				deviation.putScalar(new int[] {i,j}, deviationval);
+			}
+		}
 
+	}
+	//TODO in absolute value? 
+	private double mean(INDArray input) {
+		double mean = 0;
+		for(int i = 0; i<input.length(); i++) {
+			mean += input.getDouble(i);
+		}
+		
+		return mean/input.length();
+	}
+	
+	private double deviation(INDArray input, double mean) {
+		double deviation = 0;
+		//sum of squared difference to mean
+		for(int i = 0; i<input.length(); i++) {
+			deviation += Math.pow(input.getDouble(i)-mean, 2);
+		}
+		//TODO what if the timeseries is only one entry long it would divide by zero
+		deviation = deviation/(input.length()-1);
+		//square route to get deviation 
+		return Math.sqrt(deviation);
 	}
 
 	/* (non-Javadoc)
@@ -56,7 +122,7 @@ public class zTransfrom implements IFilter {
 	 */
 	@Override
 	public IDataset fitTransform(IDataset input) {
-		// TODO Auto-generated method stub
+		//TODO call fit in transform or not ?
 		return null;
 	}
 
