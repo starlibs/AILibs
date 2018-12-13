f5dff466b4a439a1b6a48f0662380d7b4d495b4f
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2018-12-13 08:47:32 +0100

    Adds javadoc to the DyadRankingInstanceSupplier

--- JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/DyadRankingInstanceSupplier.java
+++ JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/DyadRankingInstanceSupplier.java
@@ -21,9 +21,11 @@ import jaicore.ml.dyadranking.dataset.SparseDyadRankingInstance;
 public class DyadRankingInstanceSupplier {
 
 	/**
-	 * Creates a random dyad ranking instance (with 2 alternatives and 2 instances)
+	 * Creates a random {@link jaicore.ml.dyadranking.dataset.DyadRankingInstance}
+	 * consisting of (with 2 alternatives and 2 instances)
+	 * 
 	 * @param maxLength the amount of dyads
-	 * @return
+	 * @return random dyad ranking instance of length at most maxLength
 	 */
 	public static DyadRankingInstance getDyadRankingInstance(int maxLength, int seed) {
 		List<Dyad> dyads = new ArrayList<Dyad>();
@@ -38,6 +40,14 @@ public class DyadRankingInstanceSupplier {
 		return new DyadRankingInstance(dyads);
 	}
 
+	/**
+	 * Creates a comparator for {@link jaicore.ml.dyadranking.Dyad} (with 2
+	 * instances x_1, x_2 and 2 alternatives y_1,y_2). A pair of dyads (d_i, d_j) is
+	 * then ranked by the rule d_i >= d_j iff x_i1^2 + x_i2^2 - y_i1^2 - y_i2^2 >
+	 * x_j1^2 + x_j2^2 - y_j1^2 - y_j2^2
+	 * 
+	 * @return Comparator for dyads with 2 instances and 2 alternatives
+	 */
 	public static Comparator<Dyad> complexDyadRanker() {
 		Comparator<Dyad> comparator = new Comparator<Dyad>() {
 			@Override
@@ -54,6 +64,16 @@ public class DyadRankingInstanceSupplier {
 		return comparator;
 	}
 	
+	/**
+	 * 
+	 * @param maxLengthDyadRankingInstance Maximum length of an individual dyad
+	 *                                     ranking instance in the dataset
+	 * @param size                         Number of dyad ranking instances in the
+	 *                                     dataset
+	 * @return A dyad ranking dataset with random dyads ((with 2 alternatives and 2
+	 *         instances) that are ranked by the ranking function implemented by the
+	 *         {@link Comparator} returned by {@link #complexDyadRanker()}
+	 */
 	public static DyadRankingDataset getDyadRankingDataset(int maxLengthDyadRankingInstance, int length) {
 		DyadRankingDataset dataset = new DyadRankingDataset();
 		for(int i = 0; i < length; i++) {
