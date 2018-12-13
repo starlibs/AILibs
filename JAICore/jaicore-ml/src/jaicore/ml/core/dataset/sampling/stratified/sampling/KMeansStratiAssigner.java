ee778c51435ff5a9e410e03a32c0c5cbf6ab4824
Berberer <lukas.brandt@veds.de>
2018-12-13 13:55:24 +0100

    added stratum assignment with k-means clustering

---
+++ JAICore/jaicore-ml/src/jaicore/ml/core/dataset/sampling/stratified/sampling/KMeansStratiAssigner.java
@@ -0,0 +1,62 @@
+package jaicore.ml.core.dataset.sampling.stratified.sampling;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.math3.ml.clustering.CentroidCluster;
+import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer;
+import org.apache.commons.math3.ml.distance.DistanceMeasure;
+import org.apache.commons.math3.random.JDKRandomGenerator;
+
+import jaicore.ml.core.dataset.IDataset;
+import jaicore.ml.core.dataset.IInstance;
+
+/**
+ * Cluster the data set with k-means into k Clusters, where each cluster stands
+ * for one stratum. The datapoint assignment is performed with a lookup in the
+ * clusters.
+ * 
+ * @author Lukas Brandt
+ */
+public class KMeansStratiAssigner implements IStratiAssigner {
+
+	private int randomSeed;
+	private KMeansPlusPlusClusterer<IInstance> clusterer;
+	private DistanceMeasure distanceMeasure;
+	private List<CentroidCluster<IInstance>> clusters;
+
+	/**
+	 * Constructor for KMeansStratiAssigner.
+	 * 
+	 * @param distanceMeasure Distance measure for datapoints, for example Manhattan
+	 *                        or Euclidian.
+	 * @param randomSeed      Seed for random numbers.
+	 */
+	private KMeansStratiAssigner(DistanceMeasure distanceMeasure, int randomSeed) {
+		this.randomSeed = randomSeed;
+		this.distanceMeasure = distanceMeasure;
+	}
+
+	@Override
+	public void init(IDataset dataset, int stratiAmount) {
+		// Perform initial Clustering of the dataset.
+		this.clusterer = new KMeansPlusPlusClusterer<>(stratiAmount, -1, this.distanceMeasure,
+				new JDKRandomGenerator(this.randomSeed));
+		this.clusters = this.clusterer.cluster(dataset);
+	}
+
+	@Override
+	public int assignToStrati(IInstance datapoint) {
+		// Search for the cluster that contains the datapoint.
+		for (int i = 0; i < this.clusters.size(); i++) {
+			List<IInstance> clusterPoints = this.clusters.get(i).getPoints();
+			for (int n = 0; n < clusterPoints.size(); n++) {
+				if (Arrays.equals(datapoint.getPoint(), clusterPoints.get(n).getPoint())) {
+					return i;
+				}
+			}
+		}
+		throw new Error("Datapoint was not found in any cluster. This should not happen.");
+	}
+
+}
