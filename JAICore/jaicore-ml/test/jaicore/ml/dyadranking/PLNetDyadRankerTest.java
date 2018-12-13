22529e27db1a4ead744dd0cc1805ba6941ec128e
Michael Braun <braunm@mail.uni-paderborn.de>
2018-12-13 15:55:56 +0100

    Work on PLNetDyadRanker early stopping and several cosmetic changes.

--- JAICore/jaicore-ml/test/jaicore/ml/dyadranking/PLNetDyadRankerTest.java
+++ JAICore/jaicore-ml/test/jaicore/ml/dyadranking/PLNetDyadRankerTest.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.Test;
 
 import jaicore.ml.core.exception.PredictionException;
 import jaicore.ml.core.exception.TrainingException;
+import jaicore.ml.dyadranking.algorithm.IPLNetDyadRankerConfiguration;
 import jaicore.ml.dyadranking.algorithm.PLNetDyadRanker;
 import jaicore.ml.dyadranking.dataset.DyadRankingDataset;
 import jaicore.ml.dyadranking.dataset.IDyadRankingInstance;
@@ -22,7 +24,17 @@ public class PLNetDyadRankerTest {
 
 	@Test
 	public void testPLNetCreation() {
-		PLNetDyadRanker pldr = new PLNetDyadRanker(4,7,7);
+		IPLNetDyadRankerConfiguration config = ConfigFactory.create(IPLNetDyadRankerConfiguration.class);
+		config.setProperty(IPLNetDyadRankerConfiguration.K_PLNET_LEARNINGRATE, "0.2");
+		config.setProperty(IPLNetDyadRankerConfiguration.K_PLNET_HIDDEN_NODES, "6,4,3");
+		config.setProperty(IPLNetDyadRankerConfiguration.K_PLNET_SEED, "2");
+		config.setProperty(IPLNetDyadRankerConfiguration.K_ACTIVATION_FUNCTION, "Activation.RELU");
+		config.setProperty(IPLNetDyadRankerConfiguration.K_MAX_EPOCHS, "10");
+		config.setProperty(IPLNetDyadRankerConfiguration.K_EARLY_STOPPING_INTERVAL, "2");
+		config.setProperty(IPLNetDyadRankerConfiguration.K_EARLY_STOPPING_PATIENCE, "5");
+		PLNetDyadRanker pldr = new PLNetDyadRanker();
+		pldr.setConfiguration(config);
+
 		DyadRankingDataset trainingDataset = DyadRankingInstanceSupplier.getDyadRankingDataset(10, 1000);
 		DyadRankingDataset testDataset = DyadRankingInstanceSupplier.getDyadRankingDataset(10, 200);
 		
@@ -37,6 +49,8 @@ public class PLNetDyadRankerTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
 
 }
