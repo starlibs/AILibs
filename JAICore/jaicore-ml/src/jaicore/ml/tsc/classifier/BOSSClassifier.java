429bda248dcce7be1f4cd2ad5dcebc455efee12a
julilien <julianlienen@gmail.com>
2018-12-13 17:59:17 +0100

    Adjusts classifier design to overcome nested model class design

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/classifier/BOSSClassifier.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/classifier/BOSSClassifier.java
@@ -1,8 +1,29 @@
 package jaicore.ml.tsc.classifier;
 
-public class BOSSClassifier extends TSClassifier<Integer> {
-	
+import java.util.List;
+
+import jaicore.ml.core.dataset.IDataset;
+import jaicore.ml.core.dataset.IInstance;
+import jaicore.ml.core.dataset.attribute.categorical.CategoricalAttributeType;
+import jaicore.ml.core.exception.PredictionException;
+
+public class BOSSClassifier extends TSClassifier<CategoricalAttributeType> {
+
+	// TODO: Model parameters
+
 	public BOSSClassifier() {
-		this.algorithm = new BOSSAlgorithm();
+		super(new BOSSAlgorithm());
+	}
+
+	@Override
+	public CategoricalAttributeType predict(IInstance instance) throws PredictionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CategoricalAttributeType> predict(IDataset dataset) throws PredictionException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
