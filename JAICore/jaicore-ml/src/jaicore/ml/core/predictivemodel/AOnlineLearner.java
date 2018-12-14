1d97eecd41e4a17d2748eee077bb3fd877086b62
julilien <julianlienen@gmail.com>
2018-12-14 11:50:34 +0100

    Optimization of the model and algorithm class and interface hierarchy

--- JAICore/jaicore-ml/src/jaicore/ml/core/predictivemodel/AOnlineLearner.java
+++ JAICore/jaicore-ml/src/jaicore/ml/core/predictivemodel/AOnlineLearner.java
@@ -1,5 +1,8 @@
 package jaicore.ml.core.predictivemodel;
 
+import jaicore.ml.core.dataset.IDataset;
+import jaicore.ml.core.dataset.IInstance;
+
 /**
  * Abstract extension of {@link IOnlineLearner} to be able to construct
  * prediction of the given <TARGET> type.
@@ -8,7 +11,13 @@ package jaicore.ml.core.predictivemodel;
  *
  * @param <TARGET>
  *            The type of the target that this {@link AOnlineLearner} predicts.
+ * @param <INSTANCE>
+ *            The type of the instances stored in the data set specified by the
+ *            generic parameter <DATASET>.
+ * @param <DATASET>
+ *            The type of the data set used to learn from and predict batches.
  */
-public abstract class AOnlineLearner<TARGET> extends ABatchLearner<TARGET> implements IOnlineLearner<TARGET> {
+public abstract class AOnlineLearner<TARGET, INSTANCE extends IInstance, DATASET extends IDataset<INSTANCE>>
+		extends ABatchLearner<TARGET, INSTANCE, DATASET> implements IOnlineLearner<TARGET, INSTANCE, DATASET> {
 
 }
--- JAICore/jaicore-ml/src/jaicore/ml/tsc/classifier/BOSSClassifier.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/classifier/BOSSClassifier.java
@@ -2,12 +2,12 @@ package jaicore.ml.tsc.classifier;
 
 import java.util.List;
 
-import jaicore.ml.core.dataset.IDataset;
-import jaicore.ml.core.dataset.IInstance;
+import jaicore.ml.core.dataset.TimeSeriesDataset;
+import jaicore.ml.core.dataset.TimeSeriesInstance;
 import jaicore.ml.core.dataset.attribute.categorical.CategoricalAttributeType;
 import jaicore.ml.core.exception.PredictionException;
 
-public class BOSSClassifier extends TSClassifier<CategoricalAttributeType> {
+public class BOSSClassifier extends TSClassifier<CategoricalAttributeType, TimeSeriesDataset> {
 
 	// TODO: Model parameters
 
@@ -16,13 +16,13 @@ public class BOSSClassifier extends TSClassifier<CategoricalAttributeType> {
 	}
 
 	@Override
-	public CategoricalAttributeType predict(IInstance instance) throws PredictionException {
+	public CategoricalAttributeType predict(TimeSeriesInstance instance) throws PredictionException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<CategoricalAttributeType> predict(IDataset dataset) throws PredictionException {
+	public List<CategoricalAttributeType> predict(TimeSeriesDataset dataset) throws PredictionException {
 		// TODO Auto-generated method stub
 		return null;
 	}
