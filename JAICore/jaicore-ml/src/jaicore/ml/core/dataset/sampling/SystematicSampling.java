aa9518c07288b12092a833ed9210eeec6f177d8b
Berberer <lukas.brandt@veds.de>
2018-12-20 16:20:24 +0100

    Sorting method uses getPoint now

--- JAICore/jaicore-ml/src/jaicore/ml/core/dataset/sampling/SystematicSampling.java
+++ JAICore/jaicore-ml/src/jaicore/ml/core/dataset/sampling/SystematicSampling.java
@@ -7,11 +7,11 @@ import jaicore.basic.algorithm.AlgorithmEvent;
 import jaicore.basic.algorithm.AlgorithmFinishedEvent;
 import jaicore.basic.algorithm.AlgorithmInitializedEvent;
 import jaicore.basic.algorithm.AlgorithmState;
-import jaicore.ml.core.dataset.ContainsNonNumericAttributesException;
 import jaicore.ml.core.dataset.IInstance;
 
 /**
- * Implementation of Systematic Sampling: Sort datapoints and pick every k-th datapoint for the sample.
+ * Implementation of Systematic Sampling: Sort datapoints and pick every k-th
+ * datapoint for the sample.
  * 
  * @author Lukas Brandt
  */
@@ -21,19 +21,13 @@ public class SystematicSampling<I extends IInstance> extends ASamplingAlgorithm<
 	private int k;
 	private int startIndex;
 	private int index;
-	
+
 	// Default Comparator to sort datapoints by their vector representation.
 	private Comparator<IInstance> datapointComparator = new Comparator<IInstance>() {
 		@Override
 		public int compare(IInstance o1, IInstance o2) {
-			double[] v1, v2;
-			try {
-				v1 = o1.getAsDoubleVector();
-				v2 = o2.getAsDoubleVector();
-			} catch (ContainsNonNumericAttributesException e) {
-				e.printStackTrace();
-				return 0;
-			}
+			double[] v1 = o1.getPoint();
+			double[] v2 = o2.getPoint();
 			for (int i = 0; i < Math.min(v1.length, v2.length); i++) {
 				int c = Double.compare(v1[i], v2[i]);
 				if (c != 0) {
@@ -42,26 +36,30 @@ public class SystematicSampling<I extends IInstance> extends ASamplingAlgorithm<
 			}
 			return 0;
 		}
-		
+
 	};
-	
-	/** Simple constructor that uses the default datapoint comparator. 
+
+	/**
+	 * Simple constructor that uses the default datapoint comparator.
+	 * 
 	 * @param random Random Object for determining the sampling start point.
 	 */
 	public SystematicSampling(Random random) {
 		this.random = random;
 	}
-	
+
 	/**
 	 * Constructor for a custom datapoint comparator.
-	 * @param random Random Object for determining the sampling start point.
+	 * 
+	 * @param random              Random Object for determining the sampling start
+	 *                            point.
 	 * @param datapointComparator Comparator to sort the dataset.
 	 */
 	public SystematicSampling(Random random, Comparator<IInstance> datapointComparator) {
 		this.random = random;
 		this.datapointComparator = datapointComparator;
 	}
-	
+
 	@Override
 	public AlgorithmEvent nextWithException() throws Exception {
 		switch (this.getState()) {
@@ -70,12 +68,13 @@ public class SystematicSampling<I extends IInstance> extends ASamplingAlgorithm<
 			this.sample = getInput().createEmpty();
 			this.getInput().sort(this.datapointComparator);
 			this.startIndex = this.random.nextInt(this.getInput().size());
-			this.k = (int)Math.floor(this.getInput().size() / this.sampleSize);
+			this.k = (int) Math.floor(this.getInput().size() / this.sampleSize);
 			this.index = 0;
 			this.setState(AlgorithmState.active);
-			return new AlgorithmInitializedEvent();			
+			return new AlgorithmInitializedEvent();
 		case active:
-			// If the sample size is not reached yet, add the next datapoint from the systematic sampling method.
+			// If the sample size is not reached yet, add the next datapoint from the
+			// systematic sampling method.
 			if (this.sample.size() < this.sampleSize) {
 				int e = (startIndex + (this.index++) * k) % this.getInput().size();
 				this.sample.add(this.getInput().get(e));
@@ -92,7 +91,7 @@ public class SystematicSampling<I extends IInstance> extends ASamplingAlgorithm<
 			}
 		}
 		default:
-			throw new IllegalStateException("Unknown algorithm state "+ this.getState());
+			throw new IllegalStateException("Unknown algorithm state " + this.getState());
 		}
 	}
 
