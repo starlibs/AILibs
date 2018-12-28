a42037f0f497a61015a7b6d438dfa0593663087c
Felix Weiland <fweiland@mail.upb.de>
2018-12-28 16:26:44 +0100

    SCALE-17: Added automatic loading of data sets from OpenML

--- JAICore/jaicore-ml/test/jaicore/ml/core/dataset/sampling/GeneralSamplingTester.java
+++ JAICore/jaicore-ml/test/jaicore/ml/core/dataset/sampling/GeneralSamplingTester.java
@@ -3,15 +3,24 @@ package jaicore.ml.core.dataset.sampling;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
+import org.openml.apiconnector.io.OpenmlConnector;
+import org.openml.apiconnector.xml.DataSetDescription;
 
 import jaicore.basic.algorithm.AlgorithmProblemTransformer;
 import jaicore.basic.algorithm.GeneralAlgorithmTester;
 import jaicore.ml.core.dataset.IDataset;
 import jaicore.ml.core.dataset.IInstance;
+import jaicore.ml.core.dataset.standard.SimpleDataset;
+import jaicore.ml.core.dataset.weka.WekaInstancesUtil;
+import weka.core.Attribute;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 /**
  * This class provides some tests that verify basic properties of a sampling
@@ -25,6 +34,8 @@ public abstract class GeneralSamplingTester<I extends IInstance>
 		extends GeneralAlgorithmTester<Object, IDataset<I>, IDataset<I>> {
 
 	private static final double DEFAULT_SAMPLE_FRACTION = 0.1;
+
+	private static final String OPENML_API_KEY = "4350e421cdc16404033ef1812ea38c01";
 
 	/**
 	 * This test verifies that the produced samples have the desired size.
@@ -139,14 +150,42 @@ public abstract class GeneralSamplingTester<I extends IInstance>
 
 	@Override
 	public IDataset<I> getSimpleProblemInputForGeneralTestPurposes() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		// Load whine quality data set
+		return loadDatasetFromOpenML(287);
 	}
 
 	@Override
 	public IDataset<I> getDifficultProblemInputForGeneralTestPurposes() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		// Load higgs data set
+		return loadDatasetFromOpenML(23512);
+	}
+
+	@SuppressWarnings("unchecked")
+	private IDataset<I> loadDatasetFromOpenML(int id) throws IOException {
+		Instances dataset = null;
+		OpenmlConnector client = new OpenmlConnector();
+		try {
+			DataSetDescription description = client.dataGet(id);
+			File file = description.getDataset(OPENML_API_KEY);
+			DataSource source = new DataSource(file.getCanonicalPath());
+			dataset = source.getDataSet();
+			dataset.setClassIndex(dataset.numAttributes() - 1);
+			Attribute targetAttribute = dataset.attribute(description.getDefault_target_attribute());
+			dataset.setClassIndex(targetAttribute.index());
+		} catch (Exception e) {
+			throw new IOException("Could not load data set from OpenML!", e);
+		}
+
+		SimpleDataset simpleDataset = WekaInstancesUtil.wekaInstancesToDataset(dataset);
+		IDataset<I> toReturn = null;
+		try {
+			toReturn = (IDataset<I>) simpleDataset;
+		} catch (ClassCastException e) {
+			throw new RuntimeException("Cannot cast the loaded simple data set to the desired data set!", e);
+		}
+
+		return toReturn;
+
 	}
 
 }
