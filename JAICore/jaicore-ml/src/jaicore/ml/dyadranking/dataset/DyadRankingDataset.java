6ad5a1f329c7e8e55cccfcf8ca43710f8e69c515
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2019-01-04 11:50:36 +0100

    Work on DyadRankerGATSPTest, add constructors to DyadRankingDataset, change equals method of Dyad such that it compares the instance and the alternative instead of object references

--- JAICore/jaicore-ml/src/jaicore/ml/dyadranking/dataset/DyadRankingDataset.java
+++ JAICore/jaicore-ml/src/jaicore/ml/dyadranking/dataset/DyadRankingDataset.java
@@ -3,6 +3,7 @@ package jaicore.ml.dyadranking.dataset;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import jaicore.ml.core.dataset.IDataset;
@@ -13,12 +14,38 @@ import jaicore.ml.core.dataset.attribute.IAttributeType;
  * A dataset representation for dyad ranking. Contains
  * {@link IDyadRankingInstance}s.
  * 
- * @author Helena Graf
+ * @author Helena Graf, Jonas Hanselle
  *
  */
 public class DyadRankingDataset extends ArrayList<IInstance> implements IDataset {
 
 	private static final long serialVersionUID = -1102494546233523992L;
+
+	/**
+	 * Creates an empty dyad ranking dataset.
+	 */
+	public DyadRankingDataset() {
+		super();
+	}
+
+	/**
+	 * Creates a dyad ranking dataset containing all elements in the given
+	 * {@link Collection} in the order specified by the collections iterator.
+	 * 
+	 * @param c {@link Collection} containing {@link IInstance} objects
+	 */
+	public DyadRankingDataset(Collection<IInstance> c) {
+		super(c);
+	}
+
+	/**
+	 * Creates an empty dyad ranking dataset with the given initial capacity.
+	 * 
+	 * @param initialCapacity initial capacity of the dyad ranking dataset
+	 */
+	public DyadRankingDataset(int initialCapacity) {
+		super(initialCapacity);
+	}
 
 	@Override
 	public <T> IAttributeType<T> getTargetType(Class<? extends T> clazz) {
