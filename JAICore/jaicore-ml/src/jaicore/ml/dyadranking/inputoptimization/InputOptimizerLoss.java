e29a2c2c9644e84f3b7673f7c53f2e3a6aada63e
Michael Braun <braunm@mail.uni-paderborn.de>
2019-01-10 17:31:14 +0100

    Start on implementation of input optimizer.

---
+++ JAICore/jaicore-ml/src/jaicore/ml/dyadranking/inputoptimization/InputOptimizerLoss.java
@@ -0,0 +1,9 @@
+package jaicore.ml.dyadranking.inputoptimization;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+public interface InputOptimizerLoss {
+	
+	public double loss(INDArray plNetOutput);
+	
+}
