e793f712d232b7ff31d1d2c6586a52bcbbfec9bb
Nino <nschnitker@foxfire.consulting>
2019-01-10 06:46:15 +0100

    Tests added

---
+++ JAICore/jaicore-ml/test/jaicore/ml/core/dataset/sampling/CaseControlSamplingTester.java
@@ -0,0 +1,15 @@
+package jaicore.ml.core.dataset.sampling;
+
+import jaicore.basic.algorithm.IAlgorithmFactory;
+import jaicore.ml.core.dataset.IDataset;
+import jaicore.ml.core.dataset.IInstance;
+
+public class CaseControlSamplingTester<I extends IInstance> extends GeneralSamplingTester<I> {
+
+	@Override
+	public IAlgorithmFactory<IDataset<I>, IDataset<I>> getFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
