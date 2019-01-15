022c829fb8e782f07f5585dad838daba9b33a805
Michael Braun <braunm@mail.uni-paderborn.de>
2019-01-15 15:14:10 +0100

    Input optimizer made static

--- JAICore/jaicore-ml/test/jaicore/ml/dyadranking/inputopt/InputOptimizerTest.java
+++ JAICore/jaicore-ml/test/jaicore/ml/dyadranking/inputopt/InputOptimizerTest.java
@@ -30,6 +30,7 @@ public class InputOptimizerTest {
 		int nTestInstances = 100;
 		double avgKendallTau = 0;
 		
+		//TODO: exchange for general kendall's tau implementation once it is merged to dev
 		for (int testInst = 0; testInst < nTestInstances; testInst++) {
 			IDyadRankingInstance test = DyadRankingInstanceSupplier.getDyadRankingInstance(maxDyadRankingLength, 2, 2, DyadRankingInstanceSupplier.inputOptimizerTestRanker());
 			IDyadRankingInstance predict = testnet.predict(test);
@@ -55,14 +56,13 @@ public class InputOptimizerTest {
 		
 		System.out.println("Kendall's tau: " + avgKendallTau); 
 		
-		PLNetInputOptimizer inpopt = new PLNetInputOptimizer(testnet);	
 		Random rng = new Random(1);
 		double[] randDoubles = new double[4];
 		for (int i = 0; i < 4; i++) {
 			randDoubles[i] = rng.nextGaussian();
 		}
 		INDArray testinp = Nd4j.create(randDoubles); 
-		INDArray optimized = inpopt.optimizeInput(testinp, new NegIdentityInpOptLoss(), 0.01, 50, new Pair<Integer, Integer>(2,4));
+		INDArray optimized = PLNetInputOptimizer.optimizeInput(testnet, testinp, new NegIdentityInpOptLoss(), 0.01, 50, new Pair<Integer, Integer>(2,4));
 		System.out.println("Optimized input: " + optimized);
 	}
 }
