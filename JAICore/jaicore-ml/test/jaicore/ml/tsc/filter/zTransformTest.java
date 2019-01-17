efe0f6edd4af4e8796b523782d9d8b415e0438b9
HELEN-LAPTOP\Helen <helebeen@mail.upb.de>
2019-01-17 11:41:14 +0100

    add test for ztransform

---
+++ JAICore/jaicore-ml/test/jaicore/ml/tsc/filter/zTransformTest.java
@@ -0,0 +1,20 @@
+package jaicore.ml.tsc.filter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class zTransformTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
