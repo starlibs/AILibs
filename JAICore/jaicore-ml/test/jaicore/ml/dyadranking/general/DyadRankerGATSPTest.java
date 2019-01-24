341e06993118a8ba45ef07963a3d2c70403a32e5
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2019-01-24 11:14:22 +0100

    Trim dyad rankings before standardizing them in DyadRankerGATSPTest to avoid data snooping

--- JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/DyadRankerGATSPTest.java
+++ JAICore/jaicore-ml/test/jaicore/ml/dyadranking/general/DyadRankerGATSPTest.java
@@ -84,6 +84,9 @@ public class DyadRankerGATSPTest {
 		// split data
 		DyadRankingDataset trainData = new DyadRankingDataset(dataset.subList(0, N));
 		DyadRankingDataset testData = new DyadRankingDataset(dataset.subList(N, dataset.size()));
+		
+		// trim dyad ranking instances for train data
+		trainData = randomlyTrimSparseDyadRankingInstances(trainData, M);
 
 		// standardize data
 		DyadStandardScaler scaler = new DyadStandardScaler();
@@ -91,9 +94,6 @@ public class DyadRankerGATSPTest {
 		scaler.transformInstances(trainData);
 		scaler.transformInstances(testData);
 		
-		// trim dyad ranking instances for train data
-		trainData = randomlyTrimSparseDyadRankingInstances(trainData, M);
-
 		try {
 
 			// train the ranker
