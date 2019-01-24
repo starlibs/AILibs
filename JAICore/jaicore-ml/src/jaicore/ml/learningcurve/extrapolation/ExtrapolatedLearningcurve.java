e8d3e0987e9445c6452620deae29559314debdcd
Berberer <lukas.brandt@veds.de>
2019-01-24 12:43:03 +0100

    changed saturation point getter to return a interval around the point

--- JAICore/jaicore-ml/src/jaicore/ml/learningcurve/extrapolation/ExtrapolatedLearningcurve.java
+++ JAICore/jaicore-ml/src/jaicore/ml/learningcurve/extrapolation/ExtrapolatedLearningcurve.java
@@ -1,5 +1,7 @@
 package jaicore.ml.learningcurve.extrapolation;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 /**
  * Interface for the result of an learning curve extrapolation. Has to save the
  * data necessary for representing the curve and give information about the
@@ -10,12 +12,14 @@ package jaicore.ml.learningcurve.extrapolation;
 public interface ExtrapolatedLearningcurve {
 
 	/**
-	 * Calculated or search the saturation point, i. e. the point where the curve
+	 * Calculated or search an interval around saturation point, i. e. the point where the curve
 	 * starts to flatten.
 	 * 
-	 * @return The x value of the point.
+	 * @param epsilon Epsilon value(must be > 0) that the prediction of the
+	 *                saturation point is allowed to deviate.
+	 * @return Left and right border of the interval, where the saturation point will be inside of.
 	 */
-	public double getSaturationPoint();
+	public Pair<Double, Double> getSaturationPoint(double epsilon);
 
 	/**
 	 * Calculates or looks-up the curves value at a given point.
