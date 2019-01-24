1e1e634ba8492cf80b8912297087fae01f696ac9
Berberer <lukas.brandt@veds.de>
2019-01-24 10:54:09 +0100

    Changed splitting into a stratified splitting

--- JAICore/jaicore-ml/src/jaicore/ml/learningcurve/extrapolation/LearningCurveExtrapolator.java
+++ JAICore/jaicore-ml/src/jaicore/ml/learningcurve/extrapolation/LearningCurveExtrapolator.java
@@ -1,5 +1,10 @@
 package jaicore.ml.learningcurve.extrapolation;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import jaicore.ml.core.dataset.IDataset;
 import jaicore.ml.core.dataset.IInstance;
 import jaicore.ml.core.dataset.sampling.ASamplingAlgorithm;
@@ -21,7 +26,7 @@ import weka.core.Instances;
 public abstract class LearningCurveExtrapolator {
 
 	protected Classifier learner;
-	protected IDataset<IInstance> dataset;
+	protected IDataset<IInstance> dataset, train, test;
 	protected ASamplingAlgorithm<IInstance> subsamplingAlgorithm;
 
 	/**
@@ -30,15 +35,18 @@ public abstract class LearningCurveExtrapolator {
 	 * 
 	 * @param learner           Learning model to predict the learning curve of.
 	 * @param dataset           Dataset to measure evaluate the learner on.
+	 * @param trainsplit        Portion of the dataset, which shall be used to
+	 *                          sample from for training.
 	 * @param subsamplingMethod Subsampling method to retrieve a default configured
 	 *                          subsampler for.
-	 * @param seed              Random seed for the subsampler.
+	 * @param seed              Random seed.
 	 */
-	public LearningCurveExtrapolator(Classifier learner, IDataset<IInstance> dataset,
+	public LearningCurveExtrapolator(Classifier learner, IDataset<IInstance> dataset, double trainsplit,
 			SubsamplingMethod subsamplingMethod, long seed) {
 		this.learner = learner;
 		this.dataset = dataset;
 		this.subsamplingAlgorithm = subsamplingMethod.getSubsampler(seed);
+		this.createSplit(trainsplit, seed);
 	}
 
 	/**
@@ -46,14 +54,18 @@ public abstract class LearningCurveExtrapolator {
 	 * 
 	 * @param learner              Learning model to predict the learning curve of.
 	 * @param dataset              Dataset to measure evaluate the learner on.
+	 * @param trainsplit           Portion of the dataset, which shall be used to
+	 *                             sample from for training.
 	 * @param subsamplingAlgorithm Subsampler to create the samples at the
 	 *                             anchorpoints.
+	 * @param seed                 Random seed.
 	 */
-	public LearningCurveExtrapolator(Classifier learner, IDataset<IInstance> dataset,
-			ASamplingAlgorithm<IInstance> subsamplingAlgorithm) {
+	public LearningCurveExtrapolator(Classifier learner, IDataset<IInstance> dataset, double trainsplit,
+			ASamplingAlgorithm<IInstance> subsamplingAlgorithm, long seed) {
 		this.learner = learner;
 		this.dataset = dataset;
 		this.subsamplingAlgorithm = subsamplingAlgorithm;
+		this.createSplit(trainsplit, seed);
 	}
 
 	/**
@@ -67,40 +79,89 @@ public abstract class LearningCurveExtrapolator {
 	public ExtrapolatedLearningcurve extapolateLearningCurve(int[] anchorPoints) throws Exception {
 		double[] yValues = new double[anchorPoints.length];
 
-		// Create test and train splits.
-		IDataset<IInstance> train = this.dataset.createEmpty();
-		IDataset<IInstance> test = this.dataset.createEmpty();
-		int testSplitStartIndex = (int) (this.dataset.size() * 0.8d);
-		for (int i = 0; i < dataset.size(); i++) {
-			if (i < testSplitStartIndex) {
-				train.add(this.dataset.get(i));
-			} else {
-				test.add(this.dataset.get(i));
-			}
-		}
-		Instances testInstances = WekaInstancesUtil.datasetToWekaInstances(test);
+		Instances testInstances = WekaInstancesUtil.datasetToWekaInstances(this.test);
 
-		// Create subsamples at the anchorpoints and measure the accuracy there.
+		// Create subsamples at the anchorpoints and measure the accuracy there
 		for (int i = 0; i < anchorPoints.length; i++) {
 
 			this.subsamplingAlgorithm.setSampleSize(anchorPoints[i]);
-			this.subsamplingAlgorithm.setInput(train);
+			this.subsamplingAlgorithm.setInput(this.train);
 			IDataset<IInstance> subsampledDataset = this.subsamplingAlgorithm.call();
 
-			// Train classifier on subsampler.
+			// Train classifier on subsample
 			this.learner.buildClassifier(WekaInstancesUtil.datasetToWekaInstances(subsampledDataset));
 
-			// Measure accuracy of learner on test split.
+			// Measure accuracy of the trained learner on test split
 			double correctCounter = 0d;
 			for (Instance instance : testInstances) {
 				if (this.learner.classifyInstance(instance) == instance.classValue()) {
 					correctCounter++;
 				}
 			}
-			yValues[i] = correctCounter / (double) test.size();
+			yValues[i] = correctCounter / (double) testInstances.size();
 		}
 
 		return extrapolateLearningCurveFromAnchorPoints(anchorPoints, yValues);
+	}
+
+	private void createSplit(double trainsplit, long seed) {
+		this.train = this.dataset.createEmpty();
+		this.test = this.dataset.createEmpty();
+		IDataset<IInstance> data = this.dataset.createEmpty();
+		data.addAll(this.dataset);
+
+		// Shuffle the data
+		Random random = new Random(seed);
+		Collections.shuffle(data, random);
+
+		// Stratify the data by class
+		Map<Object, IDataset<IInstance>> classStrati = new HashMap<Object, IDataset<IInstance>>();
+		this.dataset.forEach(d -> {
+			Object c = d.getTargetValue(new Object().getClass());
+			if (!classStrati.containsKey(c)) {
+				classStrati.put(c, this.dataset.createEmpty());
+			}
+			classStrati.get(c).add(d);
+		});
+
+		// Retrieve strati sizes
+		Map<Object, Integer> classStratiSizes = new HashMap<>(classStrati.size());
+		for (Object c : classStrati.keySet()) {
+			classStratiSizes.put(c, classStrati.get(c).size());
+		}
+
+		// First assign one item of each class to train and test
+		for (Object c : classStrati.keySet()) {
+			IDataset<IInstance> availableInstances = classStrati.get(c);
+			if (!availableInstances.isEmpty()) {
+				train.add(availableInstances.get(0));
+				availableInstances.remove(0);
+			}
+			if (!availableInstances.isEmpty()) {
+				test.add(availableInstances.get(0));
+				availableInstances.remove(0);
+			}
+		}
+
+		// Distribute remaining instances over train test
+		for (Object c : classStrati.keySet()) {
+			IDataset<IInstance> availableInstances = classStrati.get(c);
+			int trainItems = (int) Math.min(availableInstances.size(), Math.ceil(trainsplit * classStratiSizes.get(c)));
+			for (int j = 0; j < trainItems; j++) {
+				this.train.add(availableInstances.get(0));
+				availableInstances.remove(0);
+			}
+			int testItems = (int) Math.min(availableInstances.size(),
+					Math.ceil((1 - trainsplit) * classStratiSizes.get(c)));
+			for (int j = 0; j < testItems; j++) {
+				this.test.add(availableInstances.get(0));
+				availableInstances.remove(0);
+			}
+		}
+
+		// Shuffle train and test
+		Collections.shuffle(this.train, random);
+		Collections.shuffle(this.test, random);
 	}
 
 	protected abstract ExtrapolatedLearningcurve extrapolateLearningCurveFromAnchorPoints(int[] xValues,
