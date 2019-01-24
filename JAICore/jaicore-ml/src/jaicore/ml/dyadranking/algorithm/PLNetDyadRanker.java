abbda03eab7466c4b6fbb105319da4be37818de3
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2019-01-24 08:27:52 +0100

    Add javadoc to the DyadStandardScaler, clean up unit tests, add config to unit tests to make them run faster

--- JAICore/jaicore-ml/src/jaicore/ml/dyadranking/algorithm/PLNetDyadRanker.java
+++ JAICore/jaicore-ml/src/jaicore/ml/dyadranking/algorithm/PLNetDyadRanker.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 
 import org.aeonbits.owner.ConfigFactory;
