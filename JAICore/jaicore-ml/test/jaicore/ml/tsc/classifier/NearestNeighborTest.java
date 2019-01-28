b1b9c3328ba2c6658e9d4fea5543a20aae6bd694
fischor <rudolftobiasfischer@gmail.com>
2019-01-28 20:11:53 +0100

    Simplifications.

--- JAICore/jaicore-ml/test/jaicore/ml/tsc/classifier/NearestNeighborTest.java
+++ JAICore/jaicore-ml/test/jaicore/ml/tsc/classifier/NearestNeighborTest.java
@@ -1,16 +1,13 @@
 package jaicore.ml.tsc.classifier;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
-import java.util.concurrent.TimeoutException;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import jaicore.basic.algorithm.AlgorithmExecutionCanceledException;
-import jaicore.basic.algorithm.exceptions.AlgorithmException;
 import jaicore.ml.core.exception.PredictionException;
+import jaicore.ml.core.exception.TrainingException;
 import jaicore.ml.tsc.dataset.TimeSeriesDataset;
 import jaicore.ml.tsc.distances.EuclideanDistance;
 
@@ -31,114 +28,40 @@ public class NearestNeighborTest {
     }
 
     @Test
-    public void testPredictionWithK1() {
+    public void testPredictionWithK1() throws TrainingException, PredictionException {
         int k = 1;
-        NearestNeighborAlgorithm algorithm = new NearestNeighborAlgorithm();
         NearestNeighborClassifier classifier = new NearestNeighborClassifier(k, new EuclideanDistance());
-
-        algorithm.setModel(classifier);
-        algorithm.setInput(dataset);
-
-        try {
-            algorithm.call();
-        } catch (InterruptedException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (AlgorithmExecutionCanceledException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (TimeoutException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (AlgorithmException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
+        classifier.train(dataset);
+        // Predict.
         double[] instance = { 0.5, 0.5 };
-        int prediction;
-        try {
-            prediction = classifier.predict(instance);
-            int expectation = 1;
-            assertEquals(expectation, prediction);
-        } catch (PredictionException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        int prediction = classifier.predict(instance);
+        int expectation = 1;
+        assertEquals(expectation, prediction);
     }
 
     @Test
-    public void testPredictionWithK2() {
+    public void testPredictionWithK2() throws TrainingException, PredictionException {
         int k = 3;
-        NearestNeighborAlgorithm algorithm = new NearestNeighborAlgorithm();
         NearestNeighborClassifier classifier = new NearestNeighborClassifier(k, new EuclideanDistance());
-
-        algorithm.setModel(classifier);
-        algorithm.setInput(dataset);
-
-        try {
-            algorithm.call();
-        } catch (InterruptedException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (AlgorithmExecutionCanceledException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (TimeoutException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (AlgorithmException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
+        classifier.train(dataset);
+        // Predict.
         double[] instance = { 0.5, 0.5 };
-        int prediction;
-        try {
-            prediction = classifier.predict(instance);
-            int expectation = 2;
-            assertEquals(expectation, prediction);
-        } catch (PredictionException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        int prediction = classifier.predict(instance);
+        int expectation = 2;
+        assertEquals(expectation, prediction);
+
     }
 
     @Test
-    public void testPredictionWithK5() {
+    public void testPredictionWithK5() throws TrainingException, PredictionException {
         int k = 5;
-        NearestNeighborAlgorithm algorithm = new NearestNeighborAlgorithm();
         NearestNeighborClassifier classifier = new NearestNeighborClassifier(k, new EuclideanDistance());
-
-        algorithm.setModel(classifier);
-        algorithm.setInput(dataset);
-
-        try {
-            algorithm.call();
-        } catch (InterruptedException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (AlgorithmExecutionCanceledException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (TimeoutException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (AlgorithmException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
+        classifier.train(dataset);
+        // Predict.
         double[] instance = { 0.5, 0.5 };
-        int prediction;
-        try {
-            prediction = classifier.predict(instance);
-            int expectation = 1;
-            assertEquals(expectation, prediction);
-        } catch (PredictionException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        int prediction = classifier.predict(instance);
+        int expectation = 1;
+        assertEquals(expectation, prediction);
     }
 
 }
