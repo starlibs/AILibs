bac201c42d5278577c512481561cf171feaeb288
fischor <rudolftobiasfischer@gmail.com>
2019-01-28 20:11:11 +0100

    Fixes Dynamic Time Warping.

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/distances/DynamicTimeWarping.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/distances/DynamicTimeWarping.java
@@ -1,10 +1,51 @@
 package jaicore.ml.tsc.distances;
 
-import static jaicore.ml.tsc.util.TimeSeriesUtil.*;
 import jaicore.ml.tsc.exceptions.TimeSeriesLengthException;
+import jaicore.ml.tsc.util.TimeSeriesUtil;
 
 /**
  * Class for the Time Warp Distance Calculation.
+ * 
+ * <p>
+ * Let <code>A = {1, 1, 2, 2, 3, 5}</code> and
+ * <code>B = {1, 1, 2, 2, 3, 5}</code> be two time series. Let <code>n=6</code>
+ * and <code>m=7</code> be the length of <code>A</code> and <code>B</code>
+ * respec. Define the distance between to points <code>x</code> and
+ * <code>y</code> as <code>d(x, y) = |x - y|</code> as their absolute
+ * difference.
+ * </p>
+ * 
+ * <p>
+ * The distance matrix <code>M</code> will look like this:
+ * </p>
+ * <p>
+ * <code>
+ * +------+------+------+------+------+------+------+------+
+ * |      |   0  |   1  |   1  |   2  |   2  |   3  |   5  |
+ * +------+------+------+------+------+------+------+------+
+ * |   0  |   0  |  inf |  inf |  inf |  inf |  inf |  inf |
+ * +------+------+------+------+------+------+------+------+
+ * |   1  |  inf |   0  |   0  |   1  |   2  |   4  |   8  |
+ * +------+------+------+------+------+------+------+------+
+ * |   2  |  inf |   1  |   1  |   0  |   0  |   1  |   4  |
+ * +------+------+------+------+------+------+------+------+
+ * |   3  |  inf |   3  |   3  |   1  |   1  |   0  |   2  |
+ * +------+------+------+------+------+------+------+------+
+ * |   5  |  inf |   7  |   7  |   4  |   4  |   2  |   0  |
+ * +------+------+------+------+------+------+------+------+        
+ * |   5  |  inf |  11  |  11  |   7  |   7  |   4  |   0  |        
+ * +------+------+------+------+------+------+------+------+            
+ * |   5  |  inf |  15  |  15  |  10  |  10  |   6  |   0  |            
+ * +------+------+------+------+------+------+------+------+            
+ * |   6  |  inf |  20  |  20  |  14  |  14  |   9  |   1  |            
+ * +------+------+------+------+------+------+------+------+     
+ * </code>
+ * </p>
+ * 
+ * References:
+ * <ul>
+ * <li>https://riptutorial.com/algorithm/example/24981/introduction-to-dynamic-time-warping</li>
+ * </ul>
  */
 public class DynamicTimeWarping implements ITimeSeriesDistance {
 
@@ -29,29 +70,56 @@ public class DynamicTimeWarping implements ITimeSeriesDistance {
 
     @Override
     public double distance(double[] A, double[] B) throws TimeSeriesLengthException {
-        // Parameter checks.
-        isSameLengthOrException(A, B);
+        // Care in the most algorithm descriptions, the time series are 1-indexed.
 
         int n = A.length;
-        double[][] M = new double[n + 1][n + 1];
+        int m = B.length;
+        double[][] M = new double[n + 1][m + 1]; // from 0 to n+1 incl. and 0 to m+1 incl.
 
-        // Dynamic Programming initialization.
+        // Initialize first row and column to infinity (except [0][0]).
         for (int i = 1; i <= n; i++)
             M[i][0] = Double.MAX_VALUE;
-        for (int i = 1; i <= n; i++)
-            M[0][i] = Double.MAX_VALUE;
+        for (int j = 1; j <= m; j++)
+            M[0][j] = Double.MAX_VALUE;
+        // Initialize [0][0] with 0.
         M[0][0] = 0d;
 
         // Dynamic programming.
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= m; j++) {
+                double cost = d.distance(A[i - 1], B[j - 1]); // 1 indexed in algo.
+                double mini = Math.min(M[i - 1][j], Math.min(M[i][j - 1], M[i - 1][j - 1]));
+                M[i][j] = cost + mini;
+            }
+        }
+
+        return M[n][m];
+    }
+
+    public double distanceWithWindow(double[] A, double[] B, int w) {
+        int n = A.length;
+        int m = B.length;
+        double[][] M = new double[n + 1][m + 1];
+
+        w = Math.max(w, Math.abs(n - m));
+
+        // Initialize first row and column to infinity (except [0][0]).
+        for (int i = 1; i <= n; i++)
+            M[i][0] = Double.MAX_VALUE;
+        for (int j = 1; j <= m; j++)
+            M[0][j] = Double.MAX_VALUE;
+        // Initialize [0][0] with 0.
+        M[0][0] = 0d;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = Math.max(1, i - w); j <= Math.min(m, i + w); j++) {
                 double cost = d.distance(A[i - 1], B[j - 1]);
                 double mini = Math.min(M[i - 1][j], Math.min(M[i][j - 1], M[i - 1][j - 1]));
                 M[i][j] = cost + mini;
             }
         }
 
-        return M[n][n];
+        return M[n][m];
     }
 
 }
--- JAICore/jaicore-ml/test/jaicore/ml/tsc/distances/DynamicTimeWarpingTest.java
+++ JAICore/jaicore-ml/test/jaicore/ml/tsc/distances/DynamicTimeWarpingTest.java
@@ -1,7 +1,6 @@
 package jaicore.ml.tsc.distances;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.Test;
 
 import jaicore.ml.tsc.exceptions.TimeSeriesLengthException;
@@ -13,8 +12,12 @@ public class DynamicTimeWarpingTest {
 
     double[] timeSeries1 = { 1, 1, 1, 1, 1, 1 };
     double[] timeSeries2 = { 1, 1, 1, 1, 1, 1 };
-    double[] timeSeries3 = { 0.50, 0.87, 0.90, 0.82, 0.70 };
+
+    double[] timeSeries3 = { 0.50, 0.87, 0.90, 0.82, 0.70 }; // distance with
     double[] timeSeries4 = { 0.10, 0.10, 0.10, 0.10, 0.10 };
+
+    double[] timeSeries5 = { 1, 1, 2, 2, 3, 5 }; // distance with d(x,y) = |x-y| is 1
+    double[] timeSeries6 = { 1, 2, 3, 5, 5, 6 };
 
     @Test
     public void testDistanceCalculation() throws TimeSeriesLengthException {
@@ -33,10 +36,19 @@ public class DynamicTimeWarpingTest {
     }
 
     @Test
-    public void testThrowsErrorWhenTimeSeriesHaveDifferentLength() {
+    public void testDistanceCalculationWithWindow() throws TimeSeriesLengthException {
         DynamicTimeWarping dtw = new DynamicTimeWarping();
-        assertThrows(IllegalArgumentException.class, () -> {
-            dtw.distance(timeSeries1, timeSeries4);
-        });
+        double distance = dtw.distanceWithWindow(timeSeries3, timeSeries4, 10000);
+        double expectation = 3.29;
+        assertEquals(expectation, distance, 1.0E-5);
     }
+
+    @Test
+    public void testDistanceCalculation3() throws TimeSeriesLengthException {
+        DynamicTimeWarping dtw = new DynamicTimeWarping();
+        double distance = dtw.distance(timeSeries5, timeSeries6);
+        double expectation = 1;
+        assertEquals(expectation, distance, 1.0E-5);
+    }
+
 }
