6560003641e4517c7f6b5509b7dbb9190b677938
Helen Beierling <helebeen@mail.upb.de>
2019-01-29 15:28:16 +0100

    finish Numerosity Reduction implement more needed functionality for the Boss Classifer

---
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/HistogramBuilder.java
@@ -0,0 +1,28 @@
+package jaicore.ml.tsc.filter;
+
+import jaicore.ml.tsc.dataset.TimeSeriesDataset;
+import jaicore.ml.tsc.exceptions.NoneFittedFilterExeception;
+
+public class HistogramBuilder implements IFilter {
+
+	@Override
+	public TimeSeriesDataset transform(TimeSeriesDataset input)
+			throws IllegalArgumentException, NoneFittedFilterExeception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void fit(TimeSeriesDataset input) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public TimeSeriesDataset fitTransform(TimeSeriesDataset input)
+			throws IllegalArgumentException, NoneFittedFilterExeception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/NumerosityReduction.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/NumerosityReduction.java
@@ -1,19 +1,28 @@
 package jaicore.ml.tsc.filter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import jaicore.ml.tsc.dataset.TimeSeriesDataset;
 import jaicore.ml.tsc.exceptions.NoneFittedFilterExeception;
 
 public class NumerosityReduction implements IFilter {
-
+	
+	private boolean fitted = false;
 	private ArrayList<double[][]> reducedDataset = new ArrayList<double[][]>();
 	
 	@Override
 	public TimeSeriesDataset transform(TimeSeriesDataset input)
 			throws IllegalArgumentException, NoneFittedFilterExeception {
-		// TODO Auto-generated method stub
-		return null;
+		if(input.isEmpty()) {
+			throw new IllegalArgumentException("The to transform dataset can not be empty.");
+		}
+		if(!fitted) {
+			throw new NoneFittedFilterExeception("The fit method must be called before transforming");
+		}
+		
+		
+		return new TimeSeriesDataset(reducedDataset,null,null);
 	}
 
 	@Override
@@ -22,8 +31,28 @@ public class NumerosityReduction implements IFilter {
 		if(input.isEmpty()) {
 			throw new IllegalArgumentException("The input dataset can not be empty.");
 		}
-		
-		
+		// find all duplicates in the 
+		for(int matrix = 0; matrix < input.getNumberOfVariables(); matrix++) {
+			
+			ArrayList<double[]> baseForNewMatrix = new ArrayList<double[]>();
+			double[] word = input.getValues(matrix)[0];
+			baseForNewMatrix.add(word);
+			
+			for(int instance = 0; instance < input.getNumberOfInstances(); instance++) {
+				if(!(Arrays.equals(word, input.getValues(matrix)[instance]))) {
+					word = input.getValues(matrix)[instance];
+					baseForNewMatrix.add(word);
+				}
+			}
+			
+			double[][] reducedMatrix = new double[baseForNewMatrix.size()][input.getValues(matrix)[0].length];
+			int index = 0;
+			for(double[] d : reducedMatrix) {
+				d=baseForNewMatrix.get(index);
+				index++;
+			}
+			reducedDataset.add(reducedMatrix);
+		}
 	}
 
 	@Override
