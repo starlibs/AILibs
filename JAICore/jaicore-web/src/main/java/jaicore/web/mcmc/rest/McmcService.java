8f280deb3383f278a35e507639b2a7922df69cca
Felix Weiland <fweiland@mail.upb.de>
2019-01-31 15:41:14 +0100

    SCALE-60: Adds dummy response to the MCMC service

--- JAICore/jaicore-web/src/main/java/jaicore/web/mcmc/rest/McmcService.java
+++ JAICore/jaicore-web/src/main/java/jaicore/web/mcmc/rest/McmcService.java
@@ -1,5 +1,8 @@
 package jaicore.web.mcmc.rest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,7 +20,31 @@ public class McmcService {
 
 	@PostMapping("/modelparams")
 	public ResponseEntity<McmcResponse> computeModelParams(McmcRequest request) {
-		throw new UnsupportedOperationException("Not impelmented yet!");
+		McmcResponse dummyResponse = getDummyResponse();
+		return ResponseEntity.ok().body(dummyResponse);
+	}
+
+	private McmcResponse getDummyResponse() {
+		McmcResponse response = new McmcResponse();
+		Map<String, Double> weights = new HashMap<>();
+		weights.put("fun1", 0.1);
+		weights.put("fun2", 0.9);
+		response.setWeights(weights);
+
+		Map<String, Double> fun1Params = new HashMap<>();
+		fun1Params.put("a", 2.0);
+		fun1Params.put("b", 1.2);
+
+		Map<String, Double> fun2Params = new HashMap<>();
+		fun2Params.put("alpha", 0.1);
+
+		Map<String, Map<String, Double>> params = new HashMap<>();
+		params.put("fun1", fun1Params);
+		params.put("fun2", fun2Params);
+
+		response.setParameters(params);
+
+		return response;
 	}
 
 	@ExceptionHandler({ Exception.class })
--- JAICore/jaicore-web/src/main/java/jaicore/web/mcmc/rest/message/McmcResponse.java
+++ JAICore/jaicore-web/src/main/java/jaicore/web/mcmc/rest/message/McmcResponse.java
@@ -1,5 +1,27 @@
 package jaicore.web.mcmc.rest.message;
 
+import java.util.Map;
+
 public class McmcResponse {
+
+	private Map<String, Double> weights;
+
+	private Map<String, Map<String, Double>> parameters;
+
+	public Map<String, Double> getWeights() {
+		return weights;
+	}
+
+	public void setWeights(Map<String, Double> weights) {
+		this.weights = weights;
+	}
+
+	public Map<String, Map<String, Double>> getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(Map<String, Map<String, Double>> parameters) {
+		this.parameters = parameters;
+	}
 
 }
