0544c58f7311ff24c20efff090e5f1b067b26f27
Felix Weiland <fweiland@mail.upb.de>
2019-02-04 22:43:32 +0100

    SCALE-60: Adds missing annotation + minor change in the dummy response

--- JAICore/jaicore-web/src/main/java/jaicore/web/mcmc/rest/McmcService.java
+++ JAICore/jaicore-web/src/main/java/jaicore/web/mcmc/rest/McmcService.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ import jaicore.web.mcmc.rest.message.McmcResponse;
 public class McmcService {
 
 	@PostMapping("/modelparams")
-	public ResponseEntity<McmcResponse> computeModelParams(McmcRequest request) {
+	public ResponseEntity<McmcResponse> computeModelParams(@RequestBody McmcRequest request) {
 		McmcResponse dummyResponse = getDummyResponse();
 		return ResponseEntity.ok().body(dummyResponse);
 	}
@@ -27,20 +28,16 @@ public class McmcService {
 	private McmcResponse getDummyResponse() {
 		McmcResponse response = new McmcResponse();
 		Map<String, Double> weights = new HashMap<>();
-		weights.put("fun1", 0.1);
-		weights.put("fun2", 0.9);
+		weights.put("pow3", 1.0);
 		response.setWeights(weights);
 
 		Map<String, Double> fun1Params = new HashMap<>();
-		fun1Params.put("a", 2.0);
-		fun1Params.put("b", 1.2);
-
-		Map<String, Double> fun2Params = new HashMap<>();
-		fun2Params.put("alpha", 0.1);
+		fun1Params.put("a", 0.5);
+		fun1Params.put("alpha", 0.7);
+		fun1Params.put("c", 10.0);
 
 		Map<String, Map<String, Double>> params = new HashMap<>();
-		params.put("fun1", fun1Params);
-		params.put("fun2", fun2Params);
+		params.put("pow3", fun1Params);
 
 		response.setParameters(params);
 
