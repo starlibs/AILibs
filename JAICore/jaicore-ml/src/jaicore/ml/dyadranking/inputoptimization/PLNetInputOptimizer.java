fe888b0ef1d8b75b8ed8e0d31e5019b0b2feb595
Michael Braun <braunm@mail.uni-paderborn.de>
2019-02-07 16:13:47 +0100

    Added KKT conditions to input optimizer.

--- JAICore/jaicore-ml/src/jaicore/ml/dyadranking/inputoptimization/PLNetInputOptimizer.java
+++ JAICore/jaicore-ml/src/jaicore/ml/dyadranking/inputoptimization/PLNetInputOptimizer.java
@@ -4,7 +4,10 @@ import org.deeplearning4j.nn.gradient.Gradient;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.BooleanIndexing;
 import org.nd4j.linalg.indexing.NDArrayIndex;
+import org.nd4j.linalg.indexing.conditions.Condition;
+import org.nd4j.linalg.indexing.conditions.Conditions;
 import org.nd4j.linalg.primitives.Pair;
 
 import de.upb.isys.linearalgebra.DenseDoubleVector;
@@ -29,23 +32,49 @@ public class PLNetInputOptimizer {
 	
 	public static INDArray optimizeInput(PLNetDyadRanker plNet, INDArray input, InputOptimizerLoss loss, double learningRate, int numSteps, INDArray inputMask) {
 		INDArray inp = input.dup();
-		System.out.print(inp);
-		Dyad testinpDyad = ndArrayToDyad(inp, 2, 2);
-		System.out.print("PLNet output: " + plNet.getPlNet().output(inp) + " ");
-		System.out.println(" input score: " + DyadRankingInstanceSupplier.inputOptimizerTestScore(testinpDyad));
+		INDArray alphas = Nd4j.zeros(inp.shape());
+		INDArray betas = Nd4j.zeros(inp.shape());
+		INDArray ones = Nd4j.ones(inp.shape());
+		double lambda = 0.0;
+		System.out.println(inp);
+		//Dyad testinpDyad = ndArrayToDyad(inp, 2, 2);
+		double output = plNet.getPlNet().output(inp).getDouble(0);
+		double incumbentOutput = output;
+		INDArray incumbent = inp.dup();
+		System.out.println("PLNet output: " + output + " ");
+		//System.out.println(" input score: " + DyadRankingInstanceSupplier.inputOptimizerTestScore(testinpDyad));
 		for(int i = 0; i < numSteps; i++) {
+			// Gradient of PLNet
 			INDArray grad = computeInputDerivative(plNet, inp, loss);
+			// Gradient of L2 norm
+			INDArray l2grad = inp.dup().muli(2);
+			l2grad.muli(lambda);
+			grad.addi(l2grad);
+			// Gradient of KKT term
+			grad.subi(alphas);
+			grad.addi(betas);
+			// Apply gradient to alphas and betas
+			alphas.subi(inp);
+			betas.addi(inp.sub(ones));
+			BooleanIndexing.replaceWhere(alphas, 0.0d, Conditions.lessThan(0.0d));
+			BooleanIndexing.replaceWhere(betas, 0.0d, Conditions.lessThan(0.0d));
 			grad.muli(inputMask);
-			System.out.println("Gradient: " + grad);
 			grad.muli(learningRate);
 			inp.subi(grad);
-			System.out.print(inp);
-			System.out.print("PLNet output: " + plNet.getPlNet().output(inp) + " ");
-			testinpDyad = ndArrayToDyad(inp, 2, 2);
-			System.out.println(" input score: " + DyadRankingInstanceSupplier.inputOptimizerTestScore(testinpDyad));
+			
+			output = plNet.getPlNet().output(inp).getDouble(0);
+			System.out.print("inps: " + inp.getDouble(input.length() - 2) + ", " + inp.getDouble(input.length() - 1));
+			System.out.print("  alphas: " + alphas.getDouble(input.length() - 2) + ", " + alphas.getDouble(input.length() - 1));
+			System.out.println("  betas: " + betas.getDouble(input.length() - 2) + ", " + betas.getDouble(input.length() - 1));
+			System.out.println("PLNet output: " + output + " ");
+			if (output > incumbentOutput) {
+				incumbent = inp.dup();
+				incumbentOutput = output;
+				System.out.println("Found new incumbent.");
+			}
 		}
 		
-		return inp;
+		return incumbent;
 	}
 
 	private static INDArray computeInputDerivative(PLNetDyadRanker plNet, INDArray input, InputOptimizerLoss loss) {
