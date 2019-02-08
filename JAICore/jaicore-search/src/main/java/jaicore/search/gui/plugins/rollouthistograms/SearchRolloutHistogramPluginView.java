dc51d28f50a4da0f814c96e94a26647022e1c1db
Alexander Hetzer <alexander.hetzer@upb.de>
2019-02-08 15:34:09 +0100

    Fix import in SearchRolloutHistogramPluginView

--- JAICore/jaicore-search/src/main/java/jaicore/search/gui/plugins/rollouthistograms/SearchRolloutHistogramPluginView.java
+++ JAICore/jaicore-search/src/main/java/jaicore/search/gui/plugins/rollouthistograms/SearchRolloutHistogramPluginView.java
@@ -2,7 +2,7 @@ package jaicore.search.gui.plugins.rollouthistograms;
 
 import java.util.List;
 
-import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 import jaicore.graphvisualizer.events.gui.Histogram;
 import jaicore.graphvisualizer.plugin.ASimpleMVCPluginView;
@@ -21,15 +21,15 @@ public class SearchRolloutHistogramPluginView extends ASimpleMVCPluginView<Searc
 
 	private final Histogram histogram;
 	private FlowPane root = new FlowPane();
-    private final int n = 100;
+	private final int n = 100;
 
-    public SearchRolloutHistogramPluginView(SearchRolloutHistogramPluginModel model) {
+	public SearchRolloutHistogramPluginView(SearchRolloutHistogramPluginModel model) {
 		super(model);
 		histogram = new Histogram(n);
 		Platform.runLater(() -> {
 			root.getChildren().add(histogram);
 		});
-	}	
+	}
 
 	@Override
 	public Node getNode() {
@@ -38,29 +38,29 @@ public class SearchRolloutHistogramPluginView extends ASimpleMVCPluginView<Searc
 
 	@Override
 	public void update() {
-        Platform.runLater(() -> {
-        	histogram.update(getHistogram(getModel().getObservedPerformances(), n));
+		Platform.runLater(() -> {
+			histogram.update(getHistogram(getModel().getObservedPerformances(), n));
 		});
-		
+
 	}
-     
-    //count data population in groups
-    private int[] getHistogram(List<Double> values, int numBars){
-    	int[] histogram = new int[numBars];
-    	DescriptiveStatistics stats = new DescriptiveStatistics();
-    	values.forEach(v -> stats.addValue(v));
-    	double min = stats.getMin();
-    	double stepSize = (stats.getMax() - min) / numBars;
-        for(int i = 0; i < values.size(); i++){
-        	for (int j = 0; j < numBars; j++) {
-        		if (values.get(i) <= min + (j * stepSize)) {
-        			histogram[j] ++;
-        			break;
-        		}
-        	}
-        }
-        return histogram;
-    }
+
+	// count data population in groups
+	private int[] getHistogram(List<Double> values, int numBars) {
+		int[] histogram = new int[numBars];
+		DescriptiveStatistics stats = new DescriptiveStatistics();
+		values.forEach(v -> stats.addValue(v));
+		double min = stats.getMin();
+		double stepSize = (stats.getMax() - min) / numBars;
+		for (int i = 0; i < values.size(); i++) {
+			for (int j = 0; j < numBars; j++) {
+				if (values.get(i) <= min + (j * stepSize)) {
+					histogram[j]++;
+					break;
+				}
+			}
+		}
+		return histogram;
+	}
 
 	@Override
 	public String getTitle() {
