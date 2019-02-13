73d87619b8730b4ad985c82ff781d21156d42629
Felix Weiland <fweiland@mail.upb.de>
2019-02-13 16:02:47 +0100

    SCALE-62: Fills McmcRequest

--- JAICore/jaicore-web/src/main/java/jaicore/web/mcmc/rest/message/McmcRequest.java
+++ JAICore/jaicore-web/src/main/java/jaicore/web/mcmc/rest/message/McmcRequest.java
@@ -1,5 +1,32 @@
 package jaicore.web.mcmc.rest.message;
 
+import java.util.List;
+
 public class McmcRequest {
+
+	private List<Integer> xValues;
+
+	private List<Double> yValues;
+
+	public List<Integer> getxValues() {
+		return xValues;
+	}
+
+	public void setxValues(List<Integer> xValues) {
+		this.xValues = xValues;
+	}
+
+	public List<Double> getyValues() {
+		return yValues;
+	}
+
+	public void setyValues(List<Double> yValues) {
+		this.yValues = yValues;
+	}
+
+	@Override
+	public String toString() {
+		return "McmcRequest [xValues=" + xValues + ", yValues=" + yValues + "]";
+	}
 
 }
