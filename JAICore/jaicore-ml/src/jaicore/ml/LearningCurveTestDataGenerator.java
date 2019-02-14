065a905443b930b156dd0e08001689429347004e
Felix Weiland <fweiland@mail.upb.de>
2019-02-14 12:15:14 +0100

    Extended test data generation and changed generation method (the data are now provided by an actual learning curve object)

--- JAICore/jaicore-ml/src/jaicore/ml/LearningCurveTestDataGenerator.java
+++ JAICore/jaicore-ml/src/jaicore/ml/LearningCurveTestDataGenerator.java
@@ -1,73 +1,106 @@
 package jaicore.ml;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.TimeoutException;
 
-import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-import org.openml.apiconnector.io.OpenmlConnector;
-import org.openml.apiconnector.xml.DataSetDescription;
-
 import jaicore.basic.algorithm.AlgorithmExecutionCanceledException;
 import jaicore.basic.algorithm.exceptions.AlgorithmException;
-import jaicore.ml.core.dataset.IDataset;
-import jaicore.ml.core.dataset.IInstance;
-import jaicore.ml.core.dataset.sampling.SimpleRandomSampling;
-import jaicore.ml.core.dataset.sampling.WekaInstancesUtil;
-import jaicore.ml.core.dataset.standard.SimpleDataset;
-import jaicore.ml.core.dataset.standard.SimpleInstance;
-import jaicore.ml.evaluation.evaluators.weka.MonteCarloCrossValidationEvaluator;
-import jaicore.ml.openml.OpenMLHelper;
-import weka.classifiers.Classifier;
-import weka.classifiers.evaluation.Evaluation;
-import weka.classifiers.trees.RandomForest;
-import weka.core.Attribute;
-import weka.core.Instance;
-import weka.core.Instances;
+import jaicore.ml.learningcurve.extrapolation.lc.LinearCombinationConfiguration;
+import jaicore.ml.learningcurve.extrapolation.lc.LinearCombinationLearningCurve;
 import weka.core.UnsupportedAttributeTypeException;
-import weka.core.converters.ConverterUtils.DataSource;
 
 /**
  * 
  * @author jnowack
+ * @author Felix Weiland
  *
  */
 public class LearningCurveTestDataGenerator {
 
-	public static int[] anchors = new int[] {1,2,4,8,16, 32, 64, 128};
-	
-	
-	public static void main(String[] args) throws UnsupportedAttributeTypeException, InterruptedException, AlgorithmExecutionCanceledException, TimeoutException, AlgorithmException, Exception {
-		
-//		anchors = new int[10];
-//		for (int i = 0; i < anchors.length; i++) {
-//			anchors[i] = (i+1)*2;
-//		}
-//		
+	public static int[] anchors = new int[] { 2, 4, 8, 16, 32, 64, 128, 256 };
+
+	public static void main(String[] args) throws UnsupportedAttributeTypeException, InterruptedException,
+			AlgorithmExecutionCanceledException, TimeoutException, AlgorithmException, Exception {
+
+		LinearCombinationLearningCurve lc = generateFunction();
+
 		Random rand = new Random(42);
-		
+
 		StringBuilder xString = new StringBuilder("x <- c(");
 		StringBuilder yString = new StringBuilder("y <- c(");
-		int i = 0;		
+		int i = 0;
 		for (int anchor : anchors) {
-			if(i != 0) {
+			if (i != 0) {
 				xString.append(",");
 				yString.append(",");
 			}
 			xString.append(anchor);
-				
-			yString.append(""+(Math.log(2* Math.log(anchor) + 1) + rand.nextGaussian()*0.01));
+
+			yString.append("" + (lc.getCurveValue(anchor) + rand.nextGaussian() * 0.01));
 			i++;
 		}
 		xString.append(")");
 		yString.append(")");
-		
-		
-		System.out.println("N <- " + anchors.length);
+
+		System.out.println("N <- " + i);
 		System.out.println(xString.toString());
 		System.out.println(yString.toString());
-	}	
+	}
+
+	private static LinearCombinationLearningCurve generateFunction() {
+		LinearCombinationConfiguration configuration = new LinearCombinationConfiguration();
+		Map<String, Double> weights = new HashMap<>();
+		weights.put("pow_3", 1.0 / 5.0);
+		weights.put("log_log_linear", 1.0 / 5.0);
+		weights.put("log_power", 1.0 / 5.0);
+		weights.put("pow_4", 1.0 / 5.0);
+		weights.put("mmf", 1.0 / 5.0);
+
+		Map<String, Map<String, Double>> modelParams = new HashMap<>();
+
+		// pow_3
+		Map<String, Double> pow3Params = new HashMap<>();
+		pow3Params.put("a", 4.5);
+		pow3Params.put("c", 1.0);
+		pow3Params.put("alpha", 0.5);
+		modelParams.put("pow_3", pow3Params);
+
+		// log log linear
+		Map<String, Double> logLogLinearParams = new HashMap<>();
+		logLogLinearParams.put("a", 0.35);
+		logLogLinearParams.put("b", -0.05);
+		modelParams.put("log_log_linear", logLogLinearParams);
+
+		// log power
+		Map<String, Double> logPowerParams = new HashMap<>();
+		logPowerParams.put("a", 1.0);
+		logPowerParams.put("b", 4.0);
+		logPowerParams.put("c", -1.15);
+		modelParams.put("log_power", logPowerParams);
+
+		// pow_4
+		Map<String, Double> pow4Params = new HashMap<>();
+		pow4Params.put("a", 0.04);
+		pow4Params.put("b", 0.0);
+		pow4Params.put("c", 1.2);
+		pow4Params.put("alpha", 0.37);
+		modelParams.put("pow_4", pow4Params);
+
+		// mmf
+		Map<String, Double> mmfParams = new HashMap<>();
+		mmfParams.put("alpha", 1.1);
+		mmfParams.put("beta", -1.0);
+		mmfParams.put("delta", 0.6);
+		mmfParams.put("kappa", 0.05);
+		modelParams.put("mmf", mmfParams);
+
+		configuration.setParameters(modelParams);
+		configuration.setWeights(weights);
+
+		LinearCombinationLearningCurve lc = new LinearCombinationLearningCurve(configuration);
+		return lc;
+
+	}
 }
