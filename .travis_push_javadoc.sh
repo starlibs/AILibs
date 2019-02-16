4a7d824f9d1711fcce3e3b62e39ead837a2cbfc0
Felix Mohr <mail@felixmohr.de>
2019-02-16 07:00:15 +0100

    modified gradle and travis configuration

---
+++ .travis_push_javadoc.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {  
+  git remote add origin-pages https://${GH_TOKEN}@github.com/fmohr/AILibs.git > /dev/null 2>&1
+  git checkout -b javadoc
+  git add ./\*.html
+  git add ./\*.css
+  git add ./\*.js
+  git add ./\*package-list
+  git commit --message "Travis built Javadoc"
+}
+
+upload_files() {
+  git push --quiet --set-upstream origin-pages javadoc 
+}
+
+setup_git
+commit_website_files
+upload_files
