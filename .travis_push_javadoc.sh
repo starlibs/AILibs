c89f1161653b3b39e4f686a91d75096d089eccd9
Felix Mohr <fmohr@mail.upb.de>
2019-02-16 19:17:16 +0100

    inserted case distinction for commit types

--- .travis_push_javadoc.sh
+++ .travis_push_javadoc.sh
@@ -6,11 +6,16 @@ setup_git() {
 }
 
 commit_website_files() {  
-  git remote add origin-pages https://${GH_TOKEN}@github.com/fmohr/AILibs.git > /dev/null 2>&1
-  git remote update
+  git remote add origin-pages https://${GH_TOKEN}@github.com/fmohr/AILibs.git > /dev/null 2>&1  
+  if [ -z "${TRAVIS_PULL_REQUEST_BRANCH}" ]; then
+    BRANCHTOPUSH=${TRAVIS_BRANCH}
+  else
+    BRANCHTOPUSH=${TRAVIS_PULL_REQUEST_BRANCH}
+  fi
   echo "Travis branch is \"${TRAVIS_BRANCH}\""
-  echo "Checking out branch \"${TRAVIS_PULL_REQUEST_BRANCH}\" that is the source of this pull request"
-  git checkout -b ${TRAVIS_PULL_REQUEST_BRANCH}
+  echo "Travis pull request branch is \"${TRAVIS_PULL_REQUEST_BRANCH}\""
+  echo "Checking out ${BRANCHTOPUSH} that is the source here"
+  git checkout -b ${BRANCHTOPUSH}
   git add ./\*.html
   git add ./\*.css
   git add ./\*.js
@@ -19,7 +24,6 @@ commit_website_files() {
 }
 
 upload_files() {
-  echo "Pushing back to ${TRAVIS_PULL_REQUEST_BRANCH}"
   git push --set-upstream origin-pages
 }
 
