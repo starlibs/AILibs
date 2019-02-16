2c963306895f86a2ad610febbd348236445f5df7
mwever <wever@mail.uni-paderborn.de>
2019-02-16 11:04:17 +0100

    Fixed problem of vanishing preferred node evaluator.

--- JAICore/jaicore-search/src/main/java/jaicore/search/algorithms/standard/bestfirst/StandardBestFirstFactory.java
+++ JAICore/jaicore-search/src/main/java/jaicore/search/algorithms/standard/bestfirst/StandardBestFirstFactory.java
@@ -1,46 +1,60 @@
 package jaicore.search.algorithms.standard.bestfirst;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import jaicore.logging.ToJSONStringUtil;
 import jaicore.search.algorithms.standard.bestfirst.nodeevaluation.AlternativeNodeEvaluator;
 import jaicore.search.algorithms.standard.bestfirst.nodeevaluation.INodeEvaluator;
 import jaicore.search.core.interfaces.GraphGenerator;
 import jaicore.search.probleminputs.GraphSearchWithSubpathEvaluationsInput;
 
 public class StandardBestFirstFactory<N, A, V extends Comparable<V>> extends BestFirstFactory<GraphSearchWithSubpathEvaluationsInput<N, A, V>, N, A, V> {
-	
+
 	private INodeEvaluator<N, V> preferredNodeEvaluator;
-	
-	public void setNodeEvaluator(INodeEvaluator<N, V> nodeEvaluator) {
-		setProblemInput(new GraphSearchWithSubpathEvaluationsInput<>(getInput() != null ? getInput().getGraphGenerator() : null, nodeEvaluator));
+
+	public void setNodeEvaluator(final INodeEvaluator<N, V> nodeEvaluator) {
+		this.setProblemInput(new GraphSearchWithSubpathEvaluationsInput<>(this.getInput() != null ? this.getInput().getGraphGenerator() : null, nodeEvaluator));
 	}
 
-	public void setGraphGenerator(GraphGenerator<N, A> graphGenerator) {
-		setProblemInput(new GraphSearchWithSubpathEvaluationsInput<>(graphGenerator, getInput() != null ? getInput().getNodeEvaluator() : null));
+	public void setGraphGenerator(final GraphGenerator<N, A> graphGenerator) {
+		this.setProblemInput(new GraphSearchWithSubpathEvaluationsInput<>(graphGenerator, this.getInput() != null ? this.getInput().getNodeEvaluator() : null));
 	}
 
 	public INodeEvaluator<N, V> getPreferredNodeEvaluator() {
-		return preferredNodeEvaluator;
+		return this.preferredNodeEvaluator;
 	}
 
-	public void setPreferredNodeEvaluator(INodeEvaluator<N, V> preferredNodeEvaluator) {
+	public void setPreferredNodeEvaluator(final INodeEvaluator<N, V> preferredNodeEvaluator) {
 		this.preferredNodeEvaluator = preferredNodeEvaluator;
 	}
-	
+
 	@Override
 	public BestFirst<GraphSearchWithSubpathEvaluationsInput<N, A, V>, N, A, V> getAlgorithm() {
-		if (getInput().getGraphGenerator() == null)
+		if (this.getInput().getGraphGenerator() == null) {
 			throw new IllegalStateException("Cannot produce BestFirst searches before the graph generator is set in the problem.");
-		if (getInput().getNodeEvaluator() == null)
+		}
+		if (this.getInput().getNodeEvaluator() == null) {
 			throw new IllegalStateException("Cannot produce BestFirst searches before the node evaluator is set.");
-		
+		}
+
 		/* determine search problem */
-		GraphSearchWithSubpathEvaluationsInput<N, A, V> problem = getInput();
-		if (preferredNodeEvaluator != null) {
-			problem = new GraphSearchWithSubpathEvaluationsInput<N, A, V>(problem.getGraphGenerator(), new AlternativeNodeEvaluator<>(preferredNodeEvaluator, problem.getNodeEvaluator()));
+		GraphSearchWithSubpathEvaluationsInput<N, A, V> problem = this.getInput();
+		if (this.preferredNodeEvaluator != null) {
+			problem = new GraphSearchWithSubpathEvaluationsInput<N, A, V>(problem.getGraphGenerator(), new AlternativeNodeEvaluator<>(this.preferredNodeEvaluator, problem.getNodeEvaluator()));
 		}
 		BestFirst<GraphSearchWithSubpathEvaluationsInput<N, A, V>, N, A, V> search = new BestFirst<>(problem);
-		search.setTimeoutForComputationOfF(getTimeoutForFInMS(), getTimeoutEvaluator());
-		if (getLoggerName() != null && getLoggerName().length() > 0)
-			search.setLoggerName(getLoggerName());
+		search.setTimeoutForComputationOfF(this.getTimeoutForFInMS(), this.getTimeoutEvaluator());
+		if (this.getLoggerName() != null && this.getLoggerName().length() > 0) {
+			search.setLoggerName(this.getLoggerName());
+		}
 		return search;
+	}
+
+	@Override
+	public String toString() {
+		Map<String, Object> fields = new HashMap<>();
+		fields.put("preferredNodeEvaluator", this.preferredNodeEvaluator);
+		return ToJSONStringUtil.toJSONString(this.getClass().getSimpleName(), fields);
 	}
 }
