6f2707f19898dc44d4e12550a063af1787f43c2a
Felix Mohr <fmohr@mail.upb.de>
2019-02-16 13:01:07 +0100

    yet another tiny change

--- .travis_push_javadoc.sh
+++ .travis_push_javadoc.sh
@@ -8,7 +8,7 @@ setup_git() {
 commit_website_files() {  
   git remote add origin-pages https://${GH_TOKEN}@github.com/fmohr/AILibs.git > /dev/null 2>&1
   git remote update
-#  git checkout -b javadoc
+  git checkout -b ${TRAVIS_PULL_REQUEST_BRANCH}
   git add ./\*.html
   git add ./\*.css
   git add ./\*.js
@@ -18,7 +18,7 @@ commit_website_files() {
 
 upload_files() {
   echo "Pushing back to ${TRAVIS_PULL_REQUEST_BRANCH}"
-  git push --set-upstream origin-pages ${TRAVIS_PULL_REQUEST_BRANCH}
+  git push --set-upstream origin-pages
 }
 
 setup_git
