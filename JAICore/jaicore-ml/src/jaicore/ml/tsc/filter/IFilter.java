45e0eb05f9459a6c8296783fb265ab47aa551485
fischor <rudolftobiasfischer@gmail.com>
2019-02-20 21:28:16 +0100

    Use new dataset.

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/IFilter.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/filter/IFilter.java
@@ -1,28 +1,38 @@
 package jaicore.ml.tsc.filter;
 
-import jaicore.ml.core.dataset.IDataset;
+import jaicore.ml.tsc.dataset.TimeSeriesDataset;
 import jaicore.ml.tsc.exceptions.NoneFittedFilterExeception;
 
 public interface IFilter {
-	
-	/** represents a function working on a dataset by transforming the dataset itself.
-	 * @param input the data set that is to transform 
-	 * @return the transformt dataset 
-	 * @throws NoneFittedFilterExeception  used if transform is called without fit
-	 * @throws IllegalArgumentException 	used if dataset to transform is empty
+
+	/**
+	 * represents a function working on a dataset by transforming the dataset
+	 * itself.
+	 * 
+	 * @param input the data set that is to transform
+	 * @return the transformt dataset
+	 * @throws NoneFittedFilterExeception used if transform is called without fit
+	 * @throws IllegalArgumentException   used if dataset to transform is empty
 	 */
-	public IDataset transform(IDataset input) throws IllegalArgumentException, NoneFittedFilterExeception;
-	
-	/** the function computes the needed information for the transform function.
-	 * @param input the dataset that is to transform 
+	public TimeSeriesDataset transform(TimeSeriesDataset input)
+			throws IllegalArgumentException, NoneFittedFilterExeception;
+
+	/**
+	 * the function computes the needed information for the transform function.
+	 * 
+	 * @param input the dataset that is to transform
 	 */
-	public void fit(IDataset input);
-	
-	/**	a utility function to avoid the added effort of calling the fit and transform function separate
-	 * @param input the dataset that is to be transfromed 
+	public void fit(TimeSeriesDataset input);
+
+	/**
+	 * a utility function to avoid the added effort of calling the fit and transform
+	 * function separate
+	 * 
+	 * @param input the dataset that is to be transfromed
 	 * @return the transformed dataset
-	 * @throws NoneFittedFilterExeception  	used if transform is called without fit
-	 * @throws IllegalArgumentException 	used if dataset to transform is empty
+	 * @throws NoneFittedFilterExeception used if transform is called without fit
+	 * @throws IllegalArgumentException   used if dataset to transform is empty
 	 */
-	public IDataset fitTransform(IDataset input) throws IllegalArgumentException, NoneFittedFilterExeception;
+	public TimeSeriesDataset fitTransform(TimeSeriesDataset input)
+			throws IllegalArgumentException, NoneFittedFilterExeception;
 }
