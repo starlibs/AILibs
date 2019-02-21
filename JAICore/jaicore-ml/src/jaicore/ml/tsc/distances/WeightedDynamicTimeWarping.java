4ca1bbca954a7c9404aae9994d85b344c8326688
rtf <rudolftobiasfischer@gmail.com>
2019-02-21 15:53:35 +0100

    Adds memoization to weight calculation.

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/distances/WeightedDynamicTimeWarping.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/distances/WeightedDynamicTimeWarping.java
@@ -1,34 +1,41 @@
 package jaicore.ml.tsc.distances;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class for the Weighted Dynamic Time Warp Distance Calculation.
  */
 public class WeightedDynamicTimeWarping implements ITimeSeriesDistance {
 
     int p;
+
     /**
      * Controls the level of penalization for the points with larger phase
-     * difference
+     * difference.
      */
     double g;
-    double Wmax;
 
-    private IScalarDistance d;
+    double maximumWeight;
+
+    private Map<Integer, double[]> weightMemoization = new HashMap<>();
 
     private double[] weights;
+
+    private IScalarDistance d;
 
     /**
      * Constructor.
      * 
-     * @param p    Defines the p-norm.
-     * @param g    Controls the penelization in weights for points with larger phase
-     *             difference.
-     * @param Wmax The desired upper bound for the weight parameter.
+     * @param p             Defines the p-norm.
+     * @param g             Controls the penelization in weights for points with
+     *                      larger phase difference.
+     * @param maximumWeight The desired upper bound for the weight parameter.
      */
-    public WeightedDynamicTimeWarping(int p, double g, double Wmax, IScalarDistance d) {
+    public WeightedDynamicTimeWarping(int p, double g, double maximumWeight, IScalarDistance d) {
         this.p = p;
         this.g = g;
-        this.Wmax = Wmax;
+        this.maximumWeight = maximumWeight;
         this.d = d;
     }
 
@@ -38,13 +45,13 @@ public class WeightedDynamicTimeWarping implements ITimeSeriesDistance {
         int m = B.length;
         double[][] M = new double[n + 1][m + 1];
 
-        initWeights(n);
+        weights = calculateWeights(Math.max(n, m));
 
         // Dynamic Programming initialization.
         for (int i = 1; i <= n; i++)
             M[i][0] = Double.MAX_VALUE;
-        for (int i = 1; i <= m; i++)
-            M[0][i] = Double.MAX_VALUE;
+        for (int j = 1; j <= m; j++)
+            M[0][j] = Double.MAX_VALUE;
         M[0][0] = 0d;
 
         // Dynamic programming.
@@ -54,26 +61,36 @@ public class WeightedDynamicTimeWarping implements ITimeSeriesDistance {
                 // double cost = Math.pow(Math.abs(weights[Math.abs(i - j)] * (A[i - 1] - B[j -
                 // 1])), p);
                 double cost = weights[Math.abs(i - j)] * d.distance(A[i - 1], B[j - 1]);
-                double mini = Math.min(M[i - 1][j], Math.min(M[i][j - 1], M[i - 1][j - 1]));
-                M[i][j] = cost + mini;
+                double minimum = Math.min(M[i - 1][j], Math.min(M[i][j - 1], M[i - 1][j - 1]));
+                M[i][j] = cost + minimum;
             }
         }
         return M[n][m];
-        // return Math.pow(M[n][n], 1 / (double) p);
     }
 
     /**
      * Initialize weights, as explained in 4.2 Modified logistic weight function.
      * 
-     * @param m Length of the time series, i.e. length of the weight vector.
+     * @param length Length of the time series, i.e. length of the weight vector.
      */
-    private void initWeights(int m) {
-        weights = new double[m];
-        double mc = (double) m / 2; // center of time series.
+    private double[] calculateWeights(int length) {
+        // Use memoization.
+        double[] memoized = weightMemoization.get(length);
+        if (memoized != null)
+            return memoized;
 
-        for (int i = 0; i < m; i++) {
-            weights[i] = Wmax / (1 + Math.exp(-g * (i - mc)));
+        // Calculate weights when not memoized.
+        double[] weights = new double[length];
+        double halfLength = (double) length / 2; // center of time series.
+
+        for (int i = 0; i < length; i++) {
+            weights[i] = maximumWeight / (1 + Math.exp(-g * (i - halfLength)));
         }
+
+        // Add to memoization-
+        weightMemoization.put(length, weights);
+
+        return weights;
     }
 
 }
