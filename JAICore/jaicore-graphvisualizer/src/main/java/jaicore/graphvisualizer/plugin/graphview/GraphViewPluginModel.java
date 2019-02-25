a91f4802c6999076df2f25cdd0f67c47cf230eb1
mwever <wever@mail.uni-paderborn.de>
2019-02-25 07:26:45 +0100

    Mlplan/validity check fixes (#74)
    * Some fixes for validity checking of pipelines to avoid unnecessary
    samples.
    
    * Some fixes for validity checking of pipelines to avoid unnecessary
    samples.
    
    * Added sources jar as an artifact to AILibs build script.
    
    * Fix for HASCO to update best seen solution during node expansion of the
    search.
    
    Added documentation to AOptimizer.
    
    * Removed duplicate event post in HASCO.
    
    * Fixed issue with caching and multiple threads.
    
    * Removed command line outputs without logger.
    
    * Travis built JavaDoc. ]ci skip[
    
    * Removed code smells
    
    * Travis built JavaDoc. ]ci skip[

--- JAICore/jaicore-graphvisualizer/src/main/java/jaicore/graphvisualizer/plugin/graphview/GraphViewPluginModel.java
+++ JAICore/jaicore-graphvisualizer/src/main/java/jaicore/graphvisualizer/plugin/graphview/GraphViewPluginModel.java
@@ -26,122 +26,126 @@ public class GraphViewPluginModel implements IGUIPluginModel {
 
 	private ConcurrentMap<Node, Set<Edge>> nodeToConnectedEdgesMap;
 
-	public GraphViewPluginModel(GraphViewPluginView view) {
+	public GraphViewPluginModel(final GraphViewPluginView view) {
+		this(view, "conf/searchgraph.css");
+	}
+
+	public GraphViewPluginModel(final GraphViewPluginView view, final String searchGraphCSSPath) {
 		this.view = view;
 		this.searchGraphNodesToViewGraphNodesMap = new ConcurrentHashMap<>();
 		this.viewGraphNodesToSearchGraphNodesMap = new ConcurrentHashMap<>();
 		this.nodeToConnectedEdgesMap = new ConcurrentHashMap<>();
 		this.nodeIdCounter = 0;
 
-		initializeGraph();
+		this.initializeGraph(searchGraphCSSPath);
 	}
 
-	private void initializeGraph() {
+	private void initializeGraph(final String searchGraphCSSPath) {
 		this.graph = new SingleGraph("Search Graph");
-		this.graph.setAttribute("ui.stylesheet", "url('conf/searchgraph.css')");
+		this.graph.setAttribute("ui.stylesheet", "url('" + searchGraphCSSPath + "')");
 	}
 
-	public void addNode(Object node, List<Object> predecessorNodes, String typeOfNode) throws ViewGraphManipulationException {
+	public void addNode(final Object node, final List<Object> predecessorNodes, final String typeOfNode) throws ViewGraphManipulationException {
 		try {
-			Node viewNode = graph.addNode(String.valueOf(nodeIdCounter));
-			registerNodeMapping(node, viewNode);
+			Node viewNode = this.graph.addNode(String.valueOf(this.nodeIdCounter));
+			this.registerNodeMapping(node, viewNode);
 
 			for (Object predecessorNode : predecessorNodes) {
-				createEdge(node, predecessorNode);
+				this.createEdge(node, predecessorNode);
 			}
 
-			switchNodeType(viewNode, typeOfNode);
-			view.update();
-			nodeIdCounter++;
+			this.switchNodeType(viewNode, typeOfNode);
+			this.view.update();
+			this.nodeIdCounter++;
 		} catch (IdAlreadyInUseException exception) {
-			throw new ViewGraphManipulationException("Cannot add node " + node + " as the id " + nodeIdCounter + " is already in use.");
+			throw new ViewGraphManipulationException("Cannot add node " + node + " as the id " + this.nodeIdCounter + " is already in use.");
 		}
 	}
 
-	private void createEdge(Object node, Object predecessorNode) throws ViewGraphManipulationException {
-		Node viewNode = searchGraphNodesToViewGraphNodesMap.get(node);
-		Node viewPredecessorNode = searchGraphNodesToViewGraphNodesMap.get(predecessorNode);
+	private void createEdge(final Object node, final Object predecessorNode) throws ViewGraphManipulationException {
+		Node viewNode = this.searchGraphNodesToViewGraphNodesMap.get(node);
+		Node viewPredecessorNode = this.searchGraphNodesToViewGraphNodesMap.get(predecessorNode);
 		if (viewPredecessorNode == null) {
 			throw new ViewGraphManipulationException("Cannot add edge from node " + predecessorNode + " to node " + viewNode + " due to missing view node of predecessor.");
 		}
 		String edgeId = viewPredecessorNode.getId() + "-" + viewNode.getId();
-		Edge edge = graph.addEdge(edgeId, viewPredecessorNode, viewNode, true);
-		registerEdgeConnectedToNodesInMap(edge);
+		Edge edge = this.graph.addEdge(edgeId, viewPredecessorNode, viewNode, true);
+		this.registerEdgeConnectedToNodesInMap(edge);
 	}
 
-	private void registerNodeMapping(Object node, Node viewNode) {
-		searchGraphNodesToViewGraphNodesMap.put(node, viewNode);
-		viewGraphNodesToSearchGraphNodesMap.put(viewNode, node);
+	private void registerNodeMapping(final Object node, final Node viewNode) {
+		this.searchGraphNodesToViewGraphNodesMap.put(node, viewNode);
+		this.viewGraphNodesToSearchGraphNodesMap.put(viewNode, node);
 	}
 
-	private void registerEdgeConnectedToNodesInMap(Edge edge) {
-		if (!nodeToConnectedEdgesMap.containsKey(edge.getNode0())) {
-			nodeToConnectedEdgesMap.put(edge.getNode0(), new HashSet<>());
+	private void registerEdgeConnectedToNodesInMap(final Edge edge) {
+		if (!this.nodeToConnectedEdgesMap.containsKey(edge.getNode0())) {
+			this.nodeToConnectedEdgesMap.put(edge.getNode0(), new HashSet<>());
 		}
-		nodeToConnectedEdgesMap.get(edge.getNode0()).add(edge);
+		this.nodeToConnectedEdgesMap.get(edge.getNode0()).add(edge);
 
-		if (!nodeToConnectedEdgesMap.containsKey(edge.getNode1())) {
-			nodeToConnectedEdgesMap.put(edge.getNode1(), new HashSet<>());
+		if (!this.nodeToConnectedEdgesMap.containsKey(edge.getNode1())) {
+			this.nodeToConnectedEdgesMap.put(edge.getNode1(), new HashSet<>());
 		}
-		nodeToConnectedEdgesMap.get(edge.getNode1()).add(edge);
+		this.nodeToConnectedEdgesMap.get(edge.getNode1()).add(edge);
 	}
 
-	public void switchNodeType(Object node, String newType) throws ViewGraphManipulationException {
+	public void switchNodeType(final Object node, final String newType) throws ViewGraphManipulationException {
 		if (node == null) {
 			throw new ViewGraphManipulationException("Cannot switch type of null node.");
 		}
-		Node viewNode = searchGraphNodesToViewGraphNodesMap.get(node);
+		Node viewNode = this.searchGraphNodesToViewGraphNodesMap.get(node);
 		if (viewNode == null) {
 			throw new ViewGraphManipulationException("Cannot switch type of node " + node + " without corresponding view node.");
 		}
-		switchNodeType(viewNode, newType);
-		view.update();
+		this.switchNodeType(viewNode, newType);
+		this.view.update();
 	}
 
-	private void switchNodeType(Node node, String newType) {
-		if (!isLabeledAsRootNode(node)) {
+	private void switchNodeType(final Node node, final String newType) {
+		if (!this.isLabeledAsRootNode(node)) {
 			node.setAttribute("ui.class", newType);
 		}
 	}
 
-	private boolean isLabeledAsRootNode(Node node) {
+	private boolean isLabeledAsRootNode(final Node node) {
 		return node.getAttribute("ui.class") != null && node.getAttribute("ui.class").equals("root");
 	}
 
-	public void removeNode(Object node) throws ViewGraphManipulationException {
+	public void removeNode(final Object node) throws ViewGraphManipulationException {
 		if (node == null) {
 			throw new ViewGraphManipulationException("Cannot remove null node.");
 		}
-		Node viewNode = searchGraphNodesToViewGraphNodesMap.remove(node);
+		Node viewNode = this.searchGraphNodesToViewGraphNodesMap.remove(node);
 		if (viewNode == null) {
 			throw new ViewGraphManipulationException("Cannot remove node " + node + " without corresponding view node.");
 		}
-		viewGraphNodesToSearchGraphNodesMap.remove(viewNode);
+		this.viewGraphNodesToSearchGraphNodesMap.remove(viewNode);
 
-		Set<Edge> connectedEdges = nodeToConnectedEdgesMap.remove(viewNode);
-		graph.removeNode(viewNode);
+		Set<Edge> connectedEdges = this.nodeToConnectedEdgesMap.remove(viewNode);
+		this.graph.removeNode(viewNode);
 		for (Edge edge : connectedEdges) {
 			Node otherNode = edge.getNode0().equals(viewNode) ? edge.getNode1() : edge.getNode0();
-			Set<Edge> connectedEdgesOfOtherNode = nodeToConnectedEdgesMap.get(otherNode);
+			Set<Edge> connectedEdgesOfOtherNode = this.nodeToConnectedEdgesMap.get(otherNode);
 			connectedEdgesOfOtherNode.remove(edge);
-			graph.removeEdge(edge);
+			this.graph.removeEdge(edge);
 		}
 	}
 
 	public void reset() {
-		graph.clear();
-		graph.setAttribute("ui.stylesheet", "url('conf/searchgraph.css')");
-		searchGraphNodesToViewGraphNodesMap.clear();
-		viewGraphNodesToSearchGraphNodesMap.clear();
-		nodeToConnectedEdgesMap.clear();
-		view.update();
+		this.graph.clear();
+		this.graph.setAttribute("ui.stylesheet", "url('conf/searchgraph.css')");
+		this.searchGraphNodesToViewGraphNodesMap.clear();
+		this.viewGraphNodesToSearchGraphNodesMap.clear();
+		this.nodeToConnectedEdgesMap.clear();
+		this.view.update();
 	}
 
 	public Graph getGraph() {
-		return graph;
+		return this.graph;
 	}
 
-	public Object getSearchGraphNodeMappedToViewGraphNode(Object searchGraphNode) {
-		return viewGraphNodesToSearchGraphNodesMap.get(searchGraphNode);
+	public Object getSearchGraphNodeMappedToViewGraphNode(final Object searchGraphNode) {
+		return this.viewGraphNodesToSearchGraphNodesMap.get(searchGraphNode);
 	}
 }
