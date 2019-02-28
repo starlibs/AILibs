b2be06bb85e8ddcef84c54a9bee83e6e4fddb060
rtf <rudolftobiasfischer@gmail.com>
2019-02-28 16:11:43 +0100

    Tests and fixes ShotgunDistance.

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/distances/ShotgunDistance.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/distances/ShotgunDistance.java
@@ -9,7 +9,14 @@ import jaicore.ml.tsc.util.TimeSeriesUtil;;
  */
 public class ShotgunDistance implements ITimeSeriesDistance {
 
+    /**
+     * Windw length.
+     */
     private int windowLength;
+
+    /**
+     * Mean normalization. If false, no mean substraction at vertical alignment.
+     */
     private boolean meanNormalization;
 
     private EuclideanDistance euclideanDistance = new EuclideanDistance();
@@ -21,31 +28,46 @@ public class ShotgunDistance implements ITimeSeriesDistance {
 
     @Override
     public double distance(double[] A, double[] B) {
-        int totalDistance = 0;
+        // Assure that max(A.length, B.length) <= windowLength, otherwise
+        // IndexOutOfBoundsException will be thrown.
 
-        // For each disjoint query window with lenth windowLength.
-        int numberOfDisjointWindows = A.length / windowLength;
+        double totalDistance = 0;
+
+        // For each disjoint query window with lenth this.windowLength.
+        int numberOfDisjointWindows = A.length / this.windowLength;
         for (int i = 0; i < numberOfDisjointWindows; i++) {
-            int startOfDisjointWindow = i * windowLength;
+            int startOfDisjointWindow = i * this.windowLength;
             double[] disjointWindow = Arrays.copyOfRange(A, startOfDisjointWindow,
-                    startOfDisjointWindow + windowLength);
-            disjointWindow = TimeSeriesUtil.zTransform(disjointWindow); // TODO: Optional mean substraction.
+                    startOfDisjointWindow + this.windowLength);
+
+            // Vertical alignment.
+            if (this.meanNormalization) {
+                disjointWindow = TimeSeriesUtil.zTransform(disjointWindow);
+            } else {
+                disjointWindow = TimeSeriesUtil.normalizeByStandardDeviation(disjointWindow);
+            }
 
             // Holds the minumum distance between the current disjoint window and all
             // sliding windows.
             double windowDistance = Double.MAX_VALUE;
 
             // Slide window length windowLength and stride 1 over the time series B.
-            int numberOfSlidingWindows = B.length - windowLength + 1;
+            int numberOfSlidingWindows = B.length - this.windowLength + 1;
             for (int j = 0; j < numberOfSlidingWindows; j++) {
                 int startOfSlidingWindow = j;
                 double[] slidingWindow = Arrays.copyOfRange(B, startOfSlidingWindow,
-                        startOfSlidingWindow + windowLength);
-                slidingWindow = TimeSeriesUtil.zTransform(slidingWindow);
+                        startOfSlidingWindow + this.windowLength);
+
+                // Vertical alignment.
+                if (this.meanNormalization) {
+                    slidingWindow = TimeSeriesUtil.zTransform(slidingWindow);
+                } else {
+                    slidingWindow = TimeSeriesUtil.normalizeByStandardDeviation(slidingWindow);
+                }
 
                 // Calculate distance between disjoint and sliding window. For each disjoint
                 // window, keep the minumum distance to all sliding windows.
-                double distanceDisjointSliding = euclideanDistance.distance(disjointWindow, slidingWindow); 
+                double distanceDisjointSliding = euclideanDistance.distance(disjointWindow, slidingWindow);
                 if (distanceDisjointSliding < windowDistance) {
                     windowDistance = distanceDisjointSliding;
                 }
