9a5c1867a1c5f514c4693496a4c1559527e10d1c
julilien <julianlienen@gmail.com>
2019-03-02 13:08:08 +0100

    Adds feature generation and prepares random forest training

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/classifier/trees/TimeSeriesBagOfFeaturesAlgorithm.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/classifier/trees/TimeSeriesBagOfFeaturesAlgorithm.java
@@ -1,5 +1,6 @@
 package jaicore.ml.tsc.classifier.trees;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -14,8 +15,12 @@ import jaicore.basic.algorithm.IAlgorithm;
 import jaicore.basic.algorithm.IAlgorithmConfig;
 import jaicore.basic.algorithm.events.AlgorithmEvent;
 import jaicore.basic.algorithm.exceptions.AlgorithmException;
+import jaicore.ml.core.exception.TrainingException;
 import jaicore.ml.tsc.classifier.ASimplifiedTSCAlgorithm;
 import jaicore.ml.tsc.dataset.TimeSeriesDataset;
+import jaicore.ml.tsc.features.TimeSeriesFeature;
+import jaicore.ml.tsc.util.WekaUtil;
+import weka.classifiers.trees.RandomForest;
 
 public class TimeSeriesBagOfFeaturesAlgorithm
 		extends ASimplifiedTSCAlgorithm<Integer, TimeSeriesBagOfFeaturesClassifier> {
@@ -54,7 +59,7 @@ public class TimeSeriesBagOfFeaturesAlgorithm
 		if (dataset == null || dataset.isEmpty())
 			throw new IllegalArgumentException("Dataset used for training must not be null or empty!");
 
-		if(dataset.isMultivariate())
+		if (dataset.isMultivariate())
 			LOGGER.info(
 					"Only univariate data is used for training (matrix index 0), although multivariate data is available.");
 
@@ -69,23 +74,60 @@ public class TimeSeriesBagOfFeaturesAlgorithm
 		int lMin = (int) (zProp * T);
 
 		int wMin = 3; // Minimum interval length used for meaningful intervals
-		
+
 		int d = (int) Math.floor(lMin / wMin); // Number of intervals for each subsequence
 
 		int r = (int) Math.floor(T / wMin); // Number of possible intervals in a time series
 
-		// TODO Generate r-d subsequences with each d intervalsand calculate features
+		int numBins = 10; // Number of bins used for the CPEs
+
+		// TODO Generate r-d subsequences with each d intervals and calculate features
+		int[][][] subsequences = new int[r - d][d][2];
 		Random random = new Random(seed);
 		for (int i = 0; i < r - d; i++) {
 
 			for (int j = 0; j < d; j++) {
-
+				int startIndex = random.nextInt(T - lMin);
+				int subSeqLength = random.nextInt(T - lMin - startIndex);
+				subsequences[i][j][0] = startIndex;
+				subsequences[i][j][1] = startIndex + subSeqLength; // exclusive
 			}
+		}
 
+		// Generate features
+		double[][][][] generatedFeatures = new double[data.length][r - d][d][3];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < r - d; j++) {
+				for (int k = 0; k < d; k++) {
+					generatedFeatures[i][j][k] = TimeSeriesFeature.getFeatures(data[i], subsequences[j][k][0],
+							subsequences[j][k][1] - 1, true);
+				}
+			}
 		}
 
 		// TODO Generate class probability estimate (CPE) for each instance using a
 		// classifier
+		RandomForest[] forests = new RandomForest[r - d];
+		for (int i = 0; i < r - d; i++) {
+
+			double[][] subSeqValueMatrix = new double[data.length][d * 3];
+			int[] targetMatrix = new int[data.length];
+			for (int j = 0; j < r - d; j++) {
+
+			}
+			ArrayList<double[][]> valueMatrices = new ArrayList<>();
+			valueMatrices.add(subSeqValueMatrix);
+			TimeSeriesDataset subSeqDataset = new TimeSeriesDataset(valueMatrices, targetMatrix);
+
+			forests[i] = new RandomForest();
+			try {
+				WekaUtil.buildWekaClassifierFromSimplifiedTS(forests[i], subSeqDataset);
+			} catch (TrainingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
 
 		return null;
 	}
