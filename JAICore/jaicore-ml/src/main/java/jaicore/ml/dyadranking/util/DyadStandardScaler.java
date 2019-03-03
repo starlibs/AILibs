4e201a24a3cfa2a42caef6343421ac3a3695abd4
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2019-03-03 21:09:43 +0100

    Adds functionality for ignoring specified attributes when transformations are performed to the dyad scalers

--- JAICore/jaicore-ml/src/main/java/jaicore/ml/dyadranking/util/DyadStandardScaler.java
+++ JAICore/jaicore-ml/src/main/java/jaicore/ml/dyadranking/util/DyadStandardScaler.java
@@ -1,5 +1,9 @@
 package jaicore.ml.dyadranking.util;
 
+import java.util.List;
+
+import org.nd4j.nativeblas.Nd4jCpu.IGenerator;
+
 import jaicore.ml.core.dataset.IInstance;
 import jaicore.ml.dyadranking.Dyad;
 import jaicore.ml.dyadranking.dataset.DyadRankingDataset;
@@ -24,8 +28,7 @@ public class DyadStandardScaler extends AbstractDyadScaler {
 	 * Transforms only the instances of each dyad according to the mean and standard
 	 * of the data the scaler has been fit to.
 	 * 
-	 * @param dataset
-	 *            The dataset of which the instances are to be standardized.
+	 * @param dataset The dataset of which the instances are to be standardized.
 	 */
 	public void transformInstances(DyadRankingDataset dataset) {
 		int lengthX = dataset.get(0).getDyadAtPosition(0).getInstance().length();
@@ -46,8 +49,7 @@ public class DyadStandardScaler extends AbstractDyadScaler {
 	 * Transforms only the alternatives of each dyad according to the mean and
 	 * standard deviation of the data the scaler has been fit to.
 	 * 
-	 * @param dataset
-	 *            The dataset of which the alternatives are to be standardized.
+	 * @param dataset The dataset of which the alternatives are to be standardized.
 	 */
 	public void transformAlternatives(DyadRankingDataset dataset) {
 		int lengthY = dataset.get(0).getDyadAtPosition(0).getAlternative().length();
@@ -61,6 +63,42 @@ public class DyadStandardScaler extends AbstractDyadScaler {
 					dyad.getAlternative().setValue(i, value);
 				}
 
+			}
+		}
+	}
+
+	@Override
+	public void transformInstances(DyadRankingDataset dataset, List<Integer> ignoredIndices) {
+		int lengthX = dataset.get(0).getDyadAtPosition(0).getInstance().length();
+		for (IInstance instance : dataset) {
+			IDyadRankingInstance drInstance = (IDyadRankingInstance) instance;
+			for (Dyad dyad : drInstance) {
+				for (int i = 0; i < lengthX; i++) {
+					if (!ignoredIndices.contains(i)) {
+						double value = dyad.getInstance().getValue(i);
+						value -= statsX[i].getMean();
+						value /= statsX[i].getStandardDeviation();
+						dyad.getInstance().setValue(i, value);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void transformAlternatives(DyadRankingDataset dataset, List<Integer> ignoredIndices) {
+		int lengthY = dataset.get(0).getDyadAtPosition(0).getAlternative().length();
+		for (IInstance instance : dataset) {
+			IDyadRankingInstance drInstance = (IDyadRankingInstance) instance;
+			for (Dyad dyad : drInstance) {
+				for (int i = 0; i < lengthY; i++) {
+					if (!ignoredIndices.contains(i)) {
+						double value = dyad.getAlternative().getValue(i);
+						value -= statsY[i].getMean();
+						value /= statsY[i].getStandardDeviation();
+						dyad.getAlternative().setValue(i, value);
+					}
+				}
 			}
 		}
 	}
