daf9b4eefc59e616049b619eea6556d5b1cbcf29
Felix Mohr <fmohr@mail.upb.de>
2019-03-05 12:07:14 +0100

    Resolved all compile issues in jaicore-logic

--- JAICore/jaicore-logic/src/test/java/jaicore/logic/ForwardChainingTest.java
+++ JAICore/jaicore-logic/src/test/java/jaicore/logic/ForwardChainingTest.java
@@ -2,30 +2,53 @@ package jaicore.logic;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
 
 import jaicore.basic.algorithm.GeneralAlgorithmTester;
 import jaicore.basic.algorithm.IAlgorithm;
 import jaicore.logic.fol.structure.Monom;
 import jaicore.logic.fol.util.ForwardChainer;
 import jaicore.logic.fol.util.ForwardChainingProblem;
+import jaicore.logic.problems.ForwardChainingProblemSet;
 
 public class ForwardChainingTest extends GeneralAlgorithmTester {
+
+	@Parameters(name = "problemset = {0}")
+	public static Collection<Object[]> data() {
+		List<Object> problemSets = new ArrayList<>();
+
+		/* add N-Queens (as a graph search problem set) */
+		problemSets.add(new ForwardChainingProblemSet());
+		List<Collection<Object>> input = new ArrayList<>();
+		input.add(problemSets);
+
+		Object[][] data = new Object[problemSets.size()][1];
+		for (int i = 0; i < data.length; i++) {
+			data[i][0] = problemSets.get(i);
+		}
+		return Arrays.asList(data);
+	}
 
 	@Test
 	public void testSingle() throws Exception {
 		Monom factbase = new Monom("P('a', 'b') & P('b', 'c') & Q('b', 'c')");
 		Monom conclusion = new Monom("P(x, y) & Q(y, z)");
-		
+
 		ForwardChainer fc = new ForwardChainer(new ForwardChainingProblem(factbase, conclusion, false));
 		assertEquals(1, fc.call().size());
 	}
-	
+
 	@Test
 	public void testDouble() throws Exception {
 		Monom factbase = new Monom("P('a', 'b') & P('b', 'c') & Q('b', 'c') & Q('c', 'a')");
 		Monom conclusion = new Monom("P(x, y) & Q(y, z)");
-		
+
 		ForwardChainer fc = new ForwardChainer(new ForwardChainingProblem(factbase, conclusion, false));
 		assertEquals(2, fc.call().size());
 	}
@@ -34,37 +57,37 @@ public class ForwardChainingTest extends GeneralAlgorithmTester {
 	public void testTriple() throws Exception {
 		Monom factbase = new Monom("P('a', 'b') & P('b', 'c') & Q('b', 'c') & Q('c', 'a')");
 		Monom conclusion = new Monom("P(x, y) & Q(y, z) & P(y, z) & Q(z, x)");
-		
+
 		ForwardChainer fc = new ForwardChainer(new ForwardChainingProblem(factbase, conclusion, false));
 		assertEquals(1, fc.call().size());
 	}
-	
+
 	@Test
 	public void testCWA() throws Exception {
 		Monom factbase = new Monom("P('a', 'b') & P('b', 'c') & Q('b', 'c')");
 		Monom conclusion = new Monom("P(x, y) & !Q(y, z)");
-		
+
 		ForwardChainer fc = new ForwardChainer(new ForwardChainingProblem(factbase, conclusion, true));
 		assertEquals(2, fc.call().size()); // <x,y,z> = <a,b,c>/<b,c,a>
 	}
-	
+
 	@Override
 	public void testInterrupt() throws Exception {
 		// can't produce difficult enough problems
 	}
-	
+
 	@Override
 	public void testCancel() throws Exception {
 		// can't produce difficult enough problems
 	}
-	
+
 	@Override
 	public void testQuickTimeout() throws Exception {
 		// can't produce difficult enough problems
 	}
 
 	@Override
-	public IAlgorithm<?, ?> getAlgorithm(Object problem) {
+	public IAlgorithm<?, ?> getAlgorithm(final Object problem) {
 		return new ForwardChainer((ForwardChainingProblem)problem);
 	}
 }
