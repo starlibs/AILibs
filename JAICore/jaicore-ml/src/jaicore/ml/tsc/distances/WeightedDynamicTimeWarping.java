0d369f8d2828eb2789447e6c455511eb27da24bb
rtf <rudolftobiasfischer@gmail.com>
2019-03-07 10:29:49 +0100

    Removes unused p parameter for the p-norm.

--- JAICore/jaicore-ml/src/jaicore/ml/tsc/distances/WeightedDynamicTimeWarping.java
+++ JAICore/jaicore-ml/src/jaicore/ml/tsc/distances/WeightedDynamicTimeWarping.java
@@ -4,36 +4,48 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Class for the Weighted Dynamic Time Warp Distance Calculation.
+ * Implementation of the Dynamic Time Warping (DTW) measure as published in
+ * "Weighted dynamic time warping for time series classification" by Young-Seon
+ * Jeong, Myong K. Jeong and Olufemi A. Omitaomu.
+ * 
+ * DTW does not account for the relative importance regarding the phase
+ * difference between a reference point and a testing point. This may lead to
+ * misclassification especially in applications where the shape similarity
+ * between two sequences is a major consideration for an accurate recognition.
+ * Therefore, [the authors] propose a novel distance measure, called a weighted
+ * DTW (WDTW), which is a penalty-based DTW. [Their] approach penalizes points
+ * with higher phase difference between a reference point and a testing point in
+ * order to prevent minimum distance distortion caused by outliers.
  */
 public class WeightedDynamicTimeWarping implements ITimeSeriesDistance {
-
-    int p;
 
     /**
      * Controls the level of penalization for the points with larger phase
      * difference.
      */
-    double g;
+    private double g;
 
-    double maximumWeight;
+    /**
+     * The desired upper bound for the weight parameter that is used to penalize
+     * points with higher phase difference.
+     */
+    private double maximumWeight;
 
-    private Map<Integer, double[]> weightMemoization = new HashMap<>();
-
-    private double[] weights;
-
+    /** Distance measure for scalar points. */
     private IScalarDistance d;
+
+    /** Memorizes the calculated weight vectors for a specific length. */
+    private Map<Integer, double[]> weightMemoization = new HashMap<>();
 
     /**
      * Constructor.
      * 
-     * @param p             Defines the p-norm.
      * @param g             Controls the penelization in weights for points with
      *                      larger phase difference.
-     * @param maximumWeight The desired upper bound for the weight parameter.
+     * @param maximumWeight The desired upper bound for the weight parameter that is
+     *                      used to penalize points with higher phase difference.
      */
-    public WeightedDynamicTimeWarping(int p, double g, double maximumWeight, IScalarDistance d) {
-        this.p = p;
+    public WeightedDynamicTimeWarping(double g, double maximumWeight, IScalarDistance d) {
         this.g = g;
         this.maximumWeight = maximumWeight;
         this.d = d;
@@ -45,7 +57,7 @@ public class WeightedDynamicTimeWarping implements ITimeSeriesDistance {
         int m = B.length;
         double[][] M = new double[n + 1][m + 1];
 
-        weights = calculateWeights(Math.max(n, m));
+        double[] weights = calculateWeights(Math.max(n, m));
 
         // Dynamic Programming initialization.
         for (int i = 1; i <= n; i++)
@@ -60,7 +72,7 @@ public class WeightedDynamicTimeWarping implements ITimeSeriesDistance {
                 // Paper: | w[i-j] (a_i - b_j) |^p
                 // double cost = Math.pow(Math.abs(weights[Math.abs(i - j)] * (A[i - 1] - B[j -
                 // 1])), p);
-                double cost = weights[Math.abs(i - j)] * d.distance(A[i - 1], B[j - 1]);
+                double cost = weights[Math.abs(i - j)] * this.d.distance(A[i - 1], B[j - 1]);
                 double minimum = Math.min(M[i - 1][j], Math.min(M[i][j - 1], M[i - 1][j - 1]));
                 M[i][j] = cost + minimum;
             }
@@ -69,13 +81,17 @@ public class WeightedDynamicTimeWarping implements ITimeSeriesDistance {
     }
 
     /**
-     * Initialize weights, as explained in 4.2 Modified logistic weight function.
+     * Calculates the weight vector via the Modified logistic weight function (see
+     * paper 4.2). Uses memoization to avoid multiple calculations for the same
+     * length.
      * 
-     * @param length Length of the time series, i.e. length of the weight vector.
+     * @param length Length of the time series, i.e. length of the weight vector. Is
+     *               guaranteed to be greater 0 within this class.
+     * @return Resulting weight vector.
      */
     protected double[] calculateWeights(int length) {
         // Use memoization.
-        double[] memoized = weightMemoization.get(length);
+        double[] memoized = this.weightMemoization.get(length);
         if (memoized != null)
             return memoized;
 
@@ -84,11 +100,11 @@ public class WeightedDynamicTimeWarping implements ITimeSeriesDistance {
         double halfLength = (double) length / 2; // center of time series.
 
         for (int i = 0; i < length; i++) {
-            weights[i] = maximumWeight / (1 + Math.exp(-g * (i - halfLength)));
+            weights[i] = maximumWeight / (1 + Math.exp(-this.g * (i - halfLength)));
         }
 
         // Add to memoization-
-        weightMemoization.put(length, weights);
+        this.weightMemoization.put(length, weights);
 
         return weights;
     }
--- JAICore/jaicore-ml/test/jaicore/ml/tsc/distances/WeightedDynamicTimeWarpingTest.java
+++ JAICore/jaicore-ml/test/jaicore/ml/tsc/distances/WeightedDynamicTimeWarpingTest.java
@@ -6,24 +6,29 @@ import org.junit.Test;
 import jaicore.ml.tsc.util.ScalarDistanceUtil;
 
 /**
- * WeightedDynamicTimeWarpingTest
+ * Test suite for the
+ * {@link jaicore.ml.tsc.distances.WeightedDynamicTimeWarping} implementation.
  */
 public class WeightedDynamicTimeWarpingTest {
 
-    double[] timeSeries1 = { 1, 1, 1, 1, 1, 1 };
-    double[] timeSeries2 = { 1, 1, 1, 1, 1, 1 };
-    double[] timeSeries3 = { 0.50, 0.87, 0.90, 0.82, 0.70 };
-    double[] timeSeries4 = { 0.10, 0.10, 0.10, 0.10, 0.10 };
-
+    /**
+     * Correctness test. Tests the distance calculation based on an defined input
+     * and expected output.
+     */
     @Test
-    public void testDistanceCalculation() {
-        int p = 1;
-        double g = 1;
-        double Wmax = 1;
-        IScalarDistance d = ScalarDistanceUtil.getSquaredDistance();
-        WeightedDynamicTimeWarping wdtw = new WeightedDynamicTimeWarping(p, g, Wmax, d);
-        double distance = wdtw.distance(timeSeries1, timeSeries2);
+    public void testCorrectnessForDistanceCalculation() {
+        // Input.
+        double[] timeSeries1 = { 1, 1, 1, 1, 1, 1 };
+        double[] timeSeries2 = { 1, 1, 1, 1, 1, 1 };
+        // Expectation.
         double expectation = 0;
+
+        double g = 1;
+        double maximumWeight = 1;
+        IScalarDistance d = ScalarDistanceUtil.getSquaredDistance();
+        WeightedDynamicTimeWarping wdtw = new WeightedDynamicTimeWarping(g, maximumWeight, d);
+        double distance = wdtw.distance(timeSeries1, timeSeries2);
+
         assertEquals(expectation, distance, 1.0E-5);
     }
 
