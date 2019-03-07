18797b196e6d8bdcdd6618c432c8bdcd855a009a
mwever <wever@mail.uni-paderborn.de>
2019-03-07 10:48:42 +0100

    Added package documentation.

--- JAICore/jaicore-ml/src/main/java/jaicore/ml/core/dataset/weka/package-info.java
+++ JAICore/jaicore-ml/src/main/java/jaicore/ml/core/dataset/weka/package-info.java
@@ -1,8 +1,7 @@
 /**
- * 
- */
-/**
- * @author wever
+ * This package contains classes for weka-specific logics regarding the dataset.
+ * For instance, it contains a util for transforming weka's Instances to IDataset.
  *
+ * @author wever
  */
 package jaicore.ml.core.dataset.weka;
