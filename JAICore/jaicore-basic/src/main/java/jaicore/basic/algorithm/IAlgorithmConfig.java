8b76e2b62a95153091dc09828937fc3403f3a8b0
mwever <wever@mail.uni-paderborn.de>
2019-03-11 15:50:37 +0100

    Added default methods to IAlgorithmConfig to load properties files.

--- JAICore/jaicore-basic/src/main/java/jaicore/basic/algorithm/IAlgorithmConfig.java
+++ JAICore/jaicore-basic/src/main/java/jaicore/basic/algorithm/IAlgorithmConfig.java
@@ -1,6 +1,16 @@
 package jaicore.basic.algorithm;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
 import org.aeonbits.owner.Mutable;
+
+import jaicore.basic.FileUtil;
 
 public interface IAlgorithmConfig extends Mutable {
 
@@ -15,14 +25,14 @@ public interface IAlgorithmConfig extends Mutable {
 	@Key(K_CPUS)
 	@DefaultValue("8")
 	public int cpus();
-	
+
 	/**
 	 * @return Number of threads that may be spawned by the algorithm.
 	 */
 	@Key(K_THREADS)
 	@DefaultValue("-1")
 	public int threads();
-	
+
 	/**
 	 * @return The main memory that is available to be used. This is merely a documentation variable since the true memory must be set over the JVM initialization anyway and cannot be restricted inside of it.
 	 */
@@ -36,4 +46,55 @@ public interface IAlgorithmConfig extends Mutable {
 	@Key(K_TIMEOUT)
 	@DefaultValue("-1")
 	public long timeout();
+
+	/**
+	 * Reads properties of a config from a config file.
+	 * @param file The file to read in as properties.
+	 * @throws IOException Throws an IOException if an issue occurs while reading in the properties from the given file.
+	 */
+	default void loadPropertiesFromFile(final File file) throws IOException {
+		if (!file.exists() || !file.isFile()) {
+			throw new IllegalArgumentException("File to load properties from does not exist or is not a file.");
+		}
+		loadPropertiesFromList(FileUtil.readFileAsList(file));
+	}
+
+	/**
+	 * Loads properties from a resource (instead of a file).
+	 * @param resourcePath The path to the resource.
+	 * @throws IOException Throws an IOException if an issue occurs while reading in the properties from the given resource.
+	 */
+	default void loadPropertiesFromResource(final String resourcePath) throws IOException {
+		// Get file from resources folder
+		ClassLoader classLoader = FileUtil.class.getClassLoader();
+		String content = null;
+		try (InputStream inputStream = classLoader.getResourceAsStream(resourcePath)) {
+			ByteArrayOutputStream result = new ByteArrayOutputStream(1024);
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = inputStream.read(buffer)) != -1) {
+				result.write(buffer, 0, length);
+			}
+			content = result.toString(StandardCharsets.UTF_8.name());
+		}
+
+		if (content != null) {
+			loadPropertiesFromList(Arrays.asList(content.split("\n")));
+		}
+	}
+
+	/**
+	 * Loads a properties config from a list of property assignments.
+	 *
+	 * @param propertiesList The list of property assignments.
+	 */
+	default void loadPropertiesFromList(final List<String> propertiesList) {
+		for (String line : propertiesList) {
+			if (!line.contains("=") || line.startsWith("#")) {
+				continue;
+			}
+			String[] split = line.split("=");
+			this.setProperty(split[0].trim(), split[1].trim());
+		}
+	}
 }
