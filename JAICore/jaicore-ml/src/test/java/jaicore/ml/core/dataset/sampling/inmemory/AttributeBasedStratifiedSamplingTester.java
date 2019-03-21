ef808ca378a5b545d1926cbaf2afda25964a4259
Berberer <lukas.brandt@veds.de>
2019-03-21 15:41:58 +0100

    added factory for stratified sampling

--- JAICore/jaicore-ml/src/test/java/jaicore/ml/core/dataset/sampling/inmemory/AttributeBasedStratifiedSamplingTester.java
+++ JAICore/jaicore-ml/src/test/java/jaicore/ml/core/dataset/sampling/inmemory/AttributeBasedStratifiedSamplingTester.java
@@ -9,10 +9,9 @@ import jaicore.basic.algorithm.IAlgorithm;
 import jaicore.basic.algorithm.IAlgorithmFactory;
 import jaicore.ml.core.dataset.IDataset;
 import jaicore.ml.core.dataset.IInstance;
-import jaicore.ml.core.dataset.sampling.inmemory.ASamplingAlgorithm;
+import jaicore.ml.core.dataset.sampling.inmemory.factories.StratifiedSamplingFactory;
 import jaicore.ml.core.dataset.sampling.inmemory.stratified.sampling.AttributeBasedStratiAmountSelectorAndAssigner;
 import jaicore.ml.core.dataset.sampling.inmemory.stratified.sampling.DiscretizationHelper.DiscretizationStrategy;
-import jaicore.ml.core.dataset.sampling.inmemory.stratified.sampling.StratifiedSampling;
 
 public class AttributeBasedStratifiedSamplingTester<I extends IInstance> extends GeneralSamplingTester<I> {
 
@@ -38,7 +37,6 @@ public class AttributeBasedStratifiedSamplingTester<I extends IInstance> extends
 
 			@Override
 			public IAlgorithm<IDataset<I>, IDataset<I>> getAlgorithm() {
-				Random r = new Random(RANDOM_SEED);
 				List<Integer> attributeIndices = new ArrayList<>();
 				// We assume that the target is the last attribute
 				attributeIndices.add(input.getNumberOfAttributes());
@@ -46,14 +44,13 @@ public class AttributeBasedStratifiedSamplingTester<I extends IInstance> extends
 				AttributeBasedStratiAmountSelectorAndAssigner<I> selectorAndAssigner = new AttributeBasedStratiAmountSelectorAndAssigner<>(
 						attributeIndices, DiscretizationStrategy.EQUAL_SIZE, 10);
 
-				ASamplingAlgorithm<I> algorithm = new StratifiedSampling<>(selectorAndAssigner, selectorAndAssigner, r);
-
+				StratifiedSamplingFactory<I> factory = new StratifiedSamplingFactory<>(selectorAndAssigner,
+						selectorAndAssigner);
 				if (this.input != null) {
-					algorithm.setInput(input);
 					int sampleSize = (int) (DEFAULT_SAMPLE_FRACTION * (double) input.size());
-					algorithm.setSampleSize(sampleSize);
+					return factory.getAlgorithm(sampleSize, input, new Random(RANDOM_SEED));
 				}
-				return algorithm;
+				return null;
 			}
 
 		};
