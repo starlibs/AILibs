33c23dc9bb81e9cbc0356deb5641ed748f57c255
Jonas Hanselle <jmhansel@mail.uni-paderborn.de>
2019-03-26 22:50:14 +0100

    Work on new active learning metamining test

--- JAICore/jaicore-ml/src/test/java/jaicore/ml/dyadranking/activelearning/ActiveDyadRankingMetaminingTest.java
+++ JAICore/jaicore-ml/src/test/java/jaicore/ml/dyadranking/activelearning/ActiveDyadRankingMetaminingTest.java
@@ -3,7 +3,6 @@ package jaicore.ml.dyadranking.activelearning;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -31,6 +30,8 @@ import jaicore.ml.dyadranking.dataset.DyadRankingDataset;
 import jaicore.ml.dyadranking.dataset.IDyadRankingInstance;
 import jaicore.ml.dyadranking.loss.DyadRankingLossUtil;
 import jaicore.ml.dyadranking.loss.KendallsTauDyadRankingLoss;
+import jaicore.ml.dyadranking.util.AbstractDyadScaler;
+import jaicore.ml.dyadranking.util.DyadMinMaxScaler;
 
 /**
  * @author Jonas Hanselle
@@ -39,12 +40,12 @@ import jaicore.ml.dyadranking.loss.KendallsTauDyadRankingLoss;
 @RunWith(Parameterized.class)
 public class ActiveDyadRankingMetaminingTest {
 
-	private static final String DYADS_SCORES_FILE = "testsrc/ml/dyadranking/dyad_pool_newest.txt";
+	private static final String META_DATA_DATASET_FILE = "testsrc/ml/dyadranking/ga-tsp/MLPlan-Data.txt";
 
 	private static final double TRAIN_RATIO = 0.7d;
 
 	private static boolean REMOVE_DYADS_WHEN_QUERIED = false;
-	
+
 	private static final int MAX_BATCH_SIZE = 5;
 	private static final int TOP_RANKING_LENGTH = 5;
 	private static final double RATIO_OF_OLD_SAMPLES_IN_MINIBATCH = 0.0d;
@@ -53,24 +54,35 @@ public class ActiveDyadRankingMetaminingTest {
 	DyadRankingDataset dataset;
 
 	public ActiveDyadRankingMetaminingTest(PLNetDyadRanker ranker) {
-//		this.ranker = ranker;
+
 	}
 
 	@Before
 	public void init() {
+		// load dataset
+//		dataset = loadDatasetFromXXLAndCSV();
+		dataset = new DyadRankingDataset();
+		try {
+			dataset.deserialize(new FileInputStream(new File(META_DATA_DATASET_FILE)));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void test() {
-//			dataset = randomlyTrimSparseDyadRankingInstances(dataset, 5);
 
 		// split data
+		DyadRankingDataset trainData = new DyadRankingDataset(dataset.subList(0, (int) (dataset.size() * TRAIN_RATIO)));
+		DyadRankingDataset testData = new DyadRankingDataset(
+				dataset.subList((int) (dataset.size() * TRAIN_RATIO), dataset.size()));
 
 		// standardize data
-//		DyadStandardScaler scaler = new DyadStandardScaler();
-//		scaler.fit(trainData);
-//		scaler.transformInstances(trainData);
-//		scaler.transformInstances(testData);
+		AbstractDyadScaler scaler = new DyadMinMaxScaler();
+		scaler.fit(trainData);
+		scaler.transformInstances(trainData);
+		scaler.transformInstances(testData);
 
 //			try {
 //				ranker.train(new DyadRankingDataset( trainData.subList(0, 5)));
@@ -79,9 +91,9 @@ public class ActiveDyadRankingMetaminingTest {
 //				e1.printStackTrace();
 //			}
 
-//		DyadDatasetPoolProvider poolProvider = new DyadDatasetPoolProvider(trainData);
+		DyadDatasetPoolProvider poolProvider = new DyadDatasetPoolProvider(trainData);
 
-		List<Pair<Dyad, Double>> dyadScorePairs = loadDyadsAndScore(DYADS_SCORES_FILE);
+//		List<Pair<Dyad, Double>> dyadScorePairs = loadDyadsAndScore(DYADS_SCORES_FILE);
 		int seed = 443;
 		SummaryStatistics[] results = new SummaryStatistics[100];
 		for (int i = 0; i < results.length; i++) {
@@ -89,66 +101,13 @@ public class ActiveDyadRankingMetaminingTest {
 		}
 
 		ranker = new PLNetDyadRanker();
-		DyadScorePoolProvider poolProvider = new DyadScorePoolProvider(dyadScorePairs);
-		poolProvider.printCounts();
 		poolProvider.setRemoveDyadsWhenQueried(REMOVE_DYADS_WHEN_QUERIED);
-		DyadRankingDataset dataset = new DyadRankingDataset();
-		for (Vector vector : poolProvider.getInstanceFeatures()) {
-			dataset.add(poolProvider.getDyadRankingInstanceForInstanceFeatures(vector));
-		}
-		
-		try {
-			dataset.serialize(new FileOutputStream(new File("testsrc/ml/dyadranking/dyad_pool_newest.txt")));
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 
-		Collections.shuffle(dataset, new Random(seed));
-
-		DyadRankingDataset trainData = new DyadRankingDataset(dataset.subList(0, (int) (TRAIN_RATIO * dataset.size())));
-		DyadRankingDataset testData = new DyadRankingDataset(
-				dataset.subList((int) (TRAIN_RATIO * dataset.size()), dataset.size()));
-
-		System.out.println("size before: " + poolProvider.getInstanceFeatures().size());
-
-		System.out.println("train data: ");
-		for (IInstance instance : trainData) {
-			IDyadRankingInstance drInstance = (IDyadRankingInstance) instance;
-			System.out.println(drInstance.getDyadAtPosition(0).getInstance());
-		}
-
-		System.out.println("test data: ");
-		for (IInstance instance : testData) {
-			IDyadRankingInstance drInstance = (IDyadRankingInstance) instance;
-			System.out.println(drInstance.getDyadAtPosition(0).getInstance());
-		}
-
-		DyadRankingDataset queryAnswers = new DyadRankingDataset();
-		for (IInstance instance : testData) {
-			queryAnswers.add((IDyadRankingInstance) poolProvider.query(instance));
-		}
-
-		for (IInstance instance : testData) {
-			IDyadRankingInstance drInstance = (IDyadRankingInstance) instance;
-
-			poolProvider.removeDyadsFromPoolByInstances(drInstance.getDyadAtPosition(0).getInstance());
-		}
-
-		System.out.println("size after: " + poolProvider.getInstanceFeatures().size());
-
-//		ActiveDyadRanker activeDyadRanker = new PrototypicalPoolBasedActiveDyadRanker(ranker, poolProvider, MAX_BATCH_SIZE, TOP_RANKING_LENGTH, RATIO_OF_OLD_SAMPLES_IN_MINIBATCH, 5, seed);
 		ActiveDyadRanker activeDyadRanker = new RandomPoolBasedActiveDyadRanker(ranker, poolProvider, seed);
-		
-		System.out.println("tau of query: "
-				+ DyadRankingLossUtil.computeAverageLoss(new KendallsTauDyadRankingLoss(), testData, queryAnswers));
 
 		try {
 
-			// train the ranker
-//			for (IInstance inst : trainData)
-//				System.out.println(((IDyadRankingInstance) inst).getDyadAtPosition(0).getInstance());
-			for (int i = 0; i < 100; i++) {
+			for (int i = 0; i < 10; i++) {
 				activeDyadRanker.activelyTrain(1);
 				double avgKendallTau = DyadRankingLossUtil.computeAverageLoss(new KendallsTauDyadRankingLoss(),
 						testData, ranker);
@@ -158,53 +117,24 @@ public class ActiveDyadRankingMetaminingTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-//		System.out.println("final averaged results: ");
-//		for (int i = 0; i < results.length; i++) {
-//			System.out.print(results[i].getMean() + ",");
-//		}
 	}
 
 	@Parameters
 	public static List<APLDyadRanker> supplyDyadRankers() {
 		PLNetDyadRanker plNetRanker = new PLNetDyadRanker();
+		// Use a simple config such that the test finishes quickly
+		/*plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_ACTIVATION_FUNCTION, "SIGMOID");
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_PLNET_HIDDEN_NODES, "5");
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_MAX_EPOCHS, "100");
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_EARLY_STOPPING_INTERVAL, "1");
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_EARLY_STOPPING_PATIENCE, "5");
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_EARLY_STOPPING_RETRAIN, "false");
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_PLNET_LEARNINGRATE, "0.1");
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_MINI_BATCH_SIZE, "1");
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_PLNET_SEED, Long.toString(seed));
+		plNetRanker.getConfiguration().setProperty(IPLNetDyadRankerConfiguration.K_EARLY_STOPPING_TRAIN_RATIO, "0.8");
+		*/
 		return Arrays.asList(plNetRanker);
 	}
-
-	private List<Pair<Dyad, Double>> loadDyadsAndScore(String filePath) {
-		List<Pair<Dyad, Double>> dyadScorePairs = new LinkedList<Pair<Dyad, Double>>();
-		try {
-			FileInputStream in = new FileInputStream(new File(filePath));
-			String input = IOUtils.toString(in, StandardCharsets.UTF_8);
-			String[] rows = input.split("\n");
-			for (String row : rows) {
-				if (row.isEmpty())
-					break;
-				List<Dyad> dyads = new LinkedList<Dyad>();
-				String[] dyadTokens = row.split("\\|");
-				String dyadString = dyadTokens[0];
-				String[] values = dyadString.split(";");
-				if (values[0].length() > 1 && values[1].length() > 1) {
-					String[] instanceValues = values[0].substring(1, values[0].length() - 1).split(",");
-					String[] alternativeValues = values[1].substring(1, values[1].length() - 1).split(",");
-					Vector instance = new DenseDoubleVector(instanceValues.length);
-					for (int i = 0; i < instanceValues.length; i++) {
-						instance.setValue(i, Double.parseDouble(instanceValues[i]));
-					}
-
-					Vector alternative = new DenseDoubleVector(alternativeValues.length);
-					for (int i = 0; i < alternativeValues.length; i++) {
-						alternative.setValue(i, Double.parseDouble(alternativeValues[i]));
-					}
-					Dyad dyad = new Dyad(instance, alternative);
-
-					Double score = Double.parseDouble(dyadTokens[1]);
-
-					dyadScorePairs.add(new Pair<Dyad, Double>(dyad, score));
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return dyadScorePairs;
-	}
+	
 }
