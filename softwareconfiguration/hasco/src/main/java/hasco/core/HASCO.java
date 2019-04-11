1a753878418fca410dc1aa3ce6d35dd70ff4aaa8
mwever <wever@mail.uni-paderborn.de>
2019-04-11 10:32:22 +0200

    Moved component related utils from Util to ComponentUtil.

--- softwareconfiguration/hasco/src/main/java/hasco/core/HASCO.java
+++ softwareconfiguration/hasco/src/main/java/hasco/core/HASCO.java
@@ -19,6 +19,7 @@ import com.google.common.eventbus.Subscribe;
 import hasco.events.HASCOSolutionEvent;
 import hasco.model.Component;
 import hasco.model.ComponentInstance;
+import hasco.model.ComponentUtil;
 import hasco.model.Parameter;
 import hasco.model.ParameterRefinementConfiguration;
 import hasco.model.UnparametrizedComponentInstance;
@@ -105,13 +106,13 @@ public class HASCO<S extends GraphSearchInput<N, A>, N, A, V extends Comparable<
 		this.logger.debug("Deriving search problem");
 		RefinementConfiguredSoftwareConfigurationProblem<V> refConfigSoftwareConfigurationProblem = new RefinementConfiguredSoftwareConfigurationProblem<>(
 				new SoftwareConfigurationProblem<V>(this.getInput().getComponents(), this.getInput().getRequiredInterface(), this.timeGrabbingEvaluationWrapper), this.getInput().getParamRefinementConfig());
-		HASCOReduction<V> hascoReduction = new HASCOReduction<>(() -> getBestSeenSolution());
+		HASCOReduction<V> hascoReduction = new HASCOReduction<>(() -> this.getBestSeenSolution());
 		this.planningProblem = hascoReduction.encodeProblem(refConfigSoftwareConfigurationProblem);
 		if (this.logger.isDebugEnabled()) {
 			String operations = this.planningProblem.getCorePlanningProblem().getDomain().getOperations().stream()
 					.map(o -> "\n\t\t" + o.getName() + "(" + o.getParams() + ")\n\t\t\tPre: " + o.getPrecondition() + "\n\t\t\tAdd List: " + o.getAddLists() + "\n\t\t\tDelete List: " + o.getDeleteLists()).collect(Collectors.joining());
 			String methods = this.planningProblem.getCorePlanningProblem().getDomain().getMethods().stream().map(m -> "\n\t\t" + m.getName() + "(" + m.getParameters() + ") for task " + m.getTask() + "\n\t\t\tPre: " + m.getPrecondition()
-			+ "\n\t\t\tPre Eval: " + m.getEvaluablePrecondition() + "\n\t\t\tNetwork: " + m.getNetwork().getLineBasedStringRepresentation()).collect(Collectors.joining());
+					+ "\n\t\t\tPre Eval: " + m.getEvaluablePrecondition() + "\n\t\t\tNetwork: " + m.getNetwork().getLineBasedStringRepresentation()).collect(Collectors.joining());
 			this.logger.debug("Derived the following HTN planning problem:\n\tOperations:{}\n\tMethods:{}", operations, methods);
 		}
 		this.searchProblem = new CostSensitivePlanningToSearchProblemTransformer<CEOCIPSTNPlanningProblem, V, N, A>(this.planningGraphGeneratorDeriver).encodeProblem(this.planningProblem);
@@ -136,7 +137,7 @@ public class HASCO<S extends GraphSearchInput<N, A>, N, A, V extends Comparable<
 			AlgorithmInitializedEvent event = this.activate();
 
 			/* analyze problem */
-			this.numUnparametrizedSolutions = Util.getNumberOfUnparametrizedCompositions(this.getInput().getComponents(), this.getInput().getRequiredInterface());
+			this.numUnparametrizedSolutions = ComponentUtil.getNumberOfUnparametrizedCompositions(this.getInput().getComponents(), this.getInput().getRequiredInterface());
 			this.logger.info("Search space contains {} unparametrized solutions.", this.numUnparametrizedSolutions);
 
 			/* setup search algorithm */
@@ -148,7 +149,7 @@ public class HASCO<S extends GraphSearchInput<N, A>, N, A, V extends Comparable<
 			} else {
 				this.logger.info("Not setting the logger name of the search. Logger name of HASCO is {}. Search loggingCustomizable: {}", this.loggerName, (this.search instanceof ILoggingCustomizable));
 			}
-			
+
 			/* register a listener on the search that will forward all events to HASCO's event bus */
 			this.search.registerListener(new Object() {
 
@@ -175,7 +176,7 @@ public class HASCO<S extends GraphSearchInput<N, A>, N, A, V extends Comparable<
 							score = HASCO.this.timeGrabbingEvaluationWrapper.evaluate(objectInstance);
 						}
 					} catch (ObjectEvaluationFailedException e) {
-						throw new AlgorithmException(e, "Could not evaluator component instance");
+						throw new AlgorithmException(e, "Could not evaluate component instance");
 					}
 					HASCO.this.logger.info("Received new solution with score {} from search, communicating this solution to the HASCO listeners. Number of returned unparametrized solutions is now {}/{}.", score,
 							HASCO.this.returnedUnparametrizedComponentInstances.size(), HASCO.this.numUnparametrizedSolutions);
