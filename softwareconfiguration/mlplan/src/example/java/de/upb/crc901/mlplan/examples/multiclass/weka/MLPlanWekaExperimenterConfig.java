7fbe3c8e1741e34d2ab4edb4a7cdf14e9afc4c00
mwever <wever@mail.uni-paderborn.de>
2019-04-18 15:31:14 +0200

    Fixed MLPlanWekaExperimenter for new experimenter package.

--- softwareconfiguration/mlplan/src/example/java/de/upb/crc901/mlplan/examples/multiclass/weka/MLPlanWekaExperimenterConfig.java
+++ softwareconfiguration/mlplan/src/example/java/de/upb/crc901/mlplan/examples/multiclass/weka/MLPlanWekaExperimenterConfig.java
@@ -2,10 +2,11 @@ package de.upb.crc901.mlplan.examples.multiclass.weka;
 
 import org.aeonbits.owner.Config.Sources;
 
+import jaicore.experiments.IDatabaseConfig;
 import jaicore.ml.experiments.IMultiClassClassificationExperimentConfig;
 
 @Sources({ "file:conf/mlplan-weka-eval.properties" })
-public interface MLPlanWekaExperimenterConfig extends IMultiClassClassificationExperimentConfig {
+public interface MLPlanWekaExperimenterConfig extends IMultiClassClassificationExperimentConfig, IDatabaseConfig {
 
 	public static final String DB_EVAL_TABLE = "db.evalTable";
 
