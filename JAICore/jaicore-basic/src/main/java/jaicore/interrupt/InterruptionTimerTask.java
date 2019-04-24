810d88d922351eb810c378fa8b178f313b702ef5
Felix Mohr <fmohr@mail.upb.de>
2019-04-24 15:49:42 +0200

    Added previous changes

---
+++ JAICore/jaicore-basic/src/main/java/jaicore/interrupt/InterruptionTimerTask.java
@@ -0,0 +1,66 @@
+package jaicore.interrupt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jaicore.concurrent.NamedTimerTask;
+
+public class InterruptionTimerTask extends NamedTimerTask {
+	private static final Logger logger = LoggerFactory.getLogger(InterruptionTimerTask.class);
+	private final Thread threadToBeInterrupted;
+	private final Object reason;
+	private final Runnable hookToExecutePriorToInterruption;
+
+	public InterruptionTimerTask(final String descriptor, final Thread threadToBeInterrupted, final Object reason, final Runnable hookToExecutePriorToInterruption) {
+		super(descriptor);
+		this.threadToBeInterrupted = threadToBeInterrupted;
+		this.hookToExecutePriorToInterruption = hookToExecutePriorToInterruption;
+		this.reason = reason;
+	}
+
+	public InterruptionTimerTask(final String descriptor, final Thread threadToBeInterrupted, final Runnable hookToExecutePriorToInterruption) {
+		super(descriptor);
+		this.threadToBeInterrupted = threadToBeInterrupted;
+		this.hookToExecutePriorToInterruption = hookToExecutePriorToInterruption;
+		this.reason = this;
+	}
+
+	public InterruptionTimerTask(final String descriptor, final Thread threadToBeInterrupted) {
+		this(descriptor, threadToBeInterrupted, null);
+	}
+
+	public InterruptionTimerTask(final String descriptor, final Runnable hookToExecutePriorToInterruption) {
+		this(descriptor, Thread.currentThread(), hookToExecutePriorToInterruption);
+	}
+
+	public InterruptionTimerTask(final String descriptor) {
+		this(descriptor, Thread.currentThread(), null);
+	}
+
+	public Thread getThreadToBeInterrupted() {
+		return this.threadToBeInterrupted;
+	}
+
+	public Runnable getHookToExecutePriorToInterruption() {
+		return this.hookToExecutePriorToInterruption;
+	}
+
+	@Override
+	public void run() {
+		long delay = System.currentTimeMillis() - this.scheduledExecutionTime();
+		logger.info("Executing interruption task {} with descriptor \"{}\". Interrupting thread {}. This interrupt has been triggered with a delay of {}ms", Math.abs(this.hashCode()), this.getDescriptor(), this.threadToBeInterrupted, delay);
+		if (delay > 50) {
+			logger.warn("Interrupt is executed with a delay of {}ms", delay);
+		}
+		if (this.hookToExecutePriorToInterruption != null) {
+			logger.debug("Running pre-interruption hook");
+			this.hookToExecutePriorToInterruption.run();
+		} else {
+			logger.debug("No pre-interruption hook has been defined.");
+		}
+		logger.debug("Interrupting the thread.");
+		Interrupter.get().interruptThread(this.threadToBeInterrupted, this.reason);
+	}
+
+
+}
---
+++ softwareconfiguration/mlplan/src/test/java/de/upb/crc901/automl/mlplan/test/MLPlanResultDeliveryTester.java
@@ -0,0 +1,32 @@
+package de.upb.crc901.automl.mlplan.test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import de.upb.crc901.automl.AutoMLAlgorithmResultProductionTester;
+import de.upb.crc901.mlplan.core.MLPlan;
+import de.upb.crc901.mlplan.core.MLPlanBuilder;
+import jaicore.basic.TimeOut;
+import jaicore.basic.algorithm.IAlgorithm;
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
+public class MLPlanResultDeliveryTester extends AutoMLAlgorithmResultProductionTester {
+
+	@Override
+	public IAlgorithm<Instances, Classifier> getAutoMLAlgorithm(final Instances data) {
+		try {
+			MLPlanBuilder builder = new MLPlanBuilder().withAutoWEKAConfiguration().withRandomCompletionBasedBestFirstSearch();
+			builder.withTimeoutForNodeEvaluation(new TimeOut(180, TimeUnit.SECONDS));
+			builder.withTimeoutForSingleSolutionEvaluation(new TimeOut(60, TimeUnit.SECONDS));
+			MLPlan mlplan = new MLPlan(builder, data);
+			mlplan.setRandomSeed(1);
+			mlplan.setPortionOfDataForPhase2(.0f);
+			mlplan.setNumCPUs(2);
+			return mlplan;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
