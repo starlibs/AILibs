77cc7490855c024a8ad2cc25a4428f76a5dc9e1d
helegraf <Graf.Helena@web.de>
2019-05-06 19:39:06 +0200

    Add MLPLan CLI (#117)
    * Add MLPLan CLI
    
    * fix mlplan configuration file setting in CLI
    
    * Temporary file.
    
    * Temporary file
    
    * Delete fruitfly.arff
    
    * Delete flags.arff
    
    * Delete car.arff
    
    * Added precision as loss function.

--- JAICore/jaicore-ml/src/main/java/jaicore/ml/core/evaluation/measure/singlelabel/RootMeanSquaredErrorLoss.java
+++ JAICore/jaicore-ml/src/main/java/jaicore/ml/core/evaluation/measure/singlelabel/RootMeanSquaredErrorLoss.java
@@ -2,8 +2,6 @@ package jaicore.ml.core.evaluation.measure.singlelabel;
 
 import java.util.List;
 
-import jaicore.basic.aggregate.IAggregateFunction;
-
 /**
  * The root mean squared loss function.
  * This loss function computes the sum of differences of expected/actual pairs,
@@ -18,11 +16,6 @@ public class RootMeanSquaredErrorLoss extends ASquaredErrorLoss {
 	 * Automatically generated version UID for serialization.
 	 */
 	private static final long serialVersionUID = -5957690628883765930L;
-
-	@Override
-	public Double calculateMeasure(final List<Double> actual, final List<Double> expected, final IAggregateFunction<Double> aggregateFunction) {
-		throw new UnsupportedOperationException("This loss can only be accessed via calculateAvgMeasure");
-	}
 
 	@Override
 	public Double calculateAvgMeasure(final List<Double> actual, final List<Double> expected) {
