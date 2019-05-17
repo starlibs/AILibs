6570c009bc9c0ee1a2e60ab7aa1d7d0e7598af6e
Felix Mohr <fmohr@mail.upb.de>
2019-05-17 06:54:35 +0200

    Added some logging functionality for node evaluation

--- JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/learningcurve/extrapolation/ipl/InversePowerLawExtrapolationMethod.java
+++ JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/learningcurve/extrapolation/ipl/InversePowerLawExtrapolationMethod.java
@@ -1,7 +1,12 @@
 package ai.libs.jaicore.ml.learningcurve.extrapolation.ipl;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ai.libs.jaicore.basic.ILoggingCustomizable;
 import ai.libs.jaicore.ml.interfaces.LearningCurve;
 import ai.libs.jaicore.ml.learningcurve.extrapolation.InvalidAnchorPointsException;
 import ai.libs.jaicore.ml.learningcurve.extrapolation.LearningCurveExtrapolationMethod;
@@ -15,7 +20,9 @@ import ai.libs.jaicore.ml.learningcurve.extrapolation.client.ExtrapolationServic
  * @author Lukas Brandt
  *
  */
-public class InversePowerLawExtrapolationMethod implements LearningCurveExtrapolationMethod {
+public class InversePowerLawExtrapolationMethod implements LearningCurveExtrapolationMethod, ILoggingCustomizable {
+	
+	private Logger logger = LoggerFactory.getLogger(InversePowerLawExtrapolationMethod.class);
 
 	// We assume the service to be running locally
 	private static final String ENDPOINT = "/jaicore/web/api/v1/ipl/modelparams";
@@ -37,13 +44,25 @@ public class InversePowerLawExtrapolationMethod implements LearningCurveExtrapol
 	@Override
 	public LearningCurve extrapolateLearningCurveFromAnchorPoints(int[] xValues, double[] yValues, int dataSetSize)
 			throws InvalidAnchorPointsException, InterruptedException, ExecutionException {
+		
 		// Request model parameters to create learning curve
+		logger.info("Querying service {} to extrapolate learning curve at anchor points {} with values {}", serviceUrl, Arrays.toString(xValues), Arrays.toString(yValues));
 		ExtrapolationServiceClient<InversePowerLawConfiguration> client = new ExtrapolationServiceClient<>(serviceUrl,
 				InversePowerLawConfiguration.class);
 		InversePowerLawConfiguration configuration = client.getConfigForAnchorPoints(xValues, yValues);
 		configuration.setA(Math.max(0.00000000001, Math.min(configuration.getA(), 0.99999999999)));
 		configuration.setC(Math.max(-0.99999999999, Math.min(configuration.getC(), -0.00000000001)));
 		return new InversePowerLawLearningCurve(configuration);
+	}
+
+	@Override
+	public String getLoggerName() {
+		return logger.getName();
+	}
+
+	@Override
+	public void setLoggerName(String name) {
+		this.logger = LoggerFactory.getLogger(name);
 	}
 
 }
