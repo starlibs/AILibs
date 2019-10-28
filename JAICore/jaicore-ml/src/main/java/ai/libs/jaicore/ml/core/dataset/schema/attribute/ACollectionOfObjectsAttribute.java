35b87c0e3ca280427e046dc0ce7d4cdc2b89b2e6
Alexander Tornede <alexander.tornede@upb.de>
2019-10-28 15:57:30 +0100

    Remove empty comment from ACollectionOfObjectsAttribute

--- JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/core/dataset/schema/attribute/ACollectionOfObjectsAttribute.java
+++ JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/core/dataset/schema/attribute/ACollectionOfObjectsAttribute.java
@@ -4,9 +4,6 @@ import java.util.Collection;
 
 public abstract class ACollectionOfObjectsAttribute<O> extends AGenericObjectAttribute<Collection<O>> {
 
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 2086475349391194480L;
 
 	protected ACollectionOfObjectsAttribute(final String name) {
