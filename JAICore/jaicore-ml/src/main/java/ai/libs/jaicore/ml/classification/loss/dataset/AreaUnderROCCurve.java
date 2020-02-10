29f59a02bb0885433e3f0fb41cfd1d4ca87b598e
mwever <wever@mail.uni-paderborn.de>
2020-02-10 12:15:21 +0100

    Implemented confusion matrix and weighted AUROC but still missing AUROC itself.

---
+++ JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/classification/loss/dataset/AreaUnderROCCurve.java
@@ -0,0 +1,18 @@
+package ai.libs.jaicore.ml.classification.loss.dataset;
+
+import java.util.List;
+
+public class AreaUnderROCCurve extends AHomogeneousPredictionPerformanceMeasure<Object> {
+
+	private final Object positiveClass;
+
+	public AreaUnderROCCurve(final Object positiveClass) {
+		this.positiveClass = positiveClass;
+	}
+
+	@Override
+	public double score(final List<?> expected, final List<?> actual) {
+		throw new UnsupportedOperationException("AUROC cannot be implemented as we do not have any class probabilities available");
+	}
+
+}
