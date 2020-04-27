881b8e85c2b31933197a00c7e3aa239478f1cb7b
kadirayk <kadiray.k@hotmail.com>
2020-04-27 12:45:33 +0200

    parameterize AsymmetricLoss

--- JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/regression/loss/dataset/AsymmetricLoss.java
+++ JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/regression/loss/dataset/AsymmetricLoss.java
@@ -7,6 +7,17 @@ import ai.libs.jaicore.basic.StatisticsUtil;
 
 public class AsymmetricLoss extends ARegressionMeasure {
 
+	private double dividerUnderestimation = 5;
+	private double dividerOverestimation = 20;
+
+	public AsymmetricLoss() {
+	}
+
+	public AsymmetricLoss(final double dividerUnderestimation, final double dividerOverestimation) {
+		this.dividerUnderestimation = dividerUnderestimation;
+		this.dividerOverestimation = dividerOverestimation;
+	}
+
 	@Override
 	public double loss(final List<? extends Double> expected, final List<? extends Double> actual) {
 		List<Double> accuracyList = new ArrayList<>();
@@ -14,9 +25,9 @@ public class AsymmetricLoss extends ARegressionMeasure {
 			Double percentageError = 100 * ((expected.get(i) - actual.get(i)) / expected.get(i));
 			Double accuracy;
 			if (percentageError <= 0) {
-				accuracy = Math.exp(-Math.log(0.5) * (percentageError / 5));
+				accuracy = Math.exp(-Math.log(0.5) * (percentageError / this.dividerUnderestimation));
 			} else {
-				accuracy = Math.exp(Math.log(0.5) * (percentageError / 20));
+				accuracy = Math.exp(Math.log(0.5) * (percentageError / this.dividerOverestimation));
 			}
 			accuracyList.add(accuracy);
 		}
