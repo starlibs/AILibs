148cc8c5603bc19a06a74d9cce6b48d2c0a87e42
Tanja Tornede <tanja.tornede@upb.de>
2020-06-02 15:52:23 +0200

    Parameterize tsfresh to make each feature selectable

--- JAICore/jaicore-ml/resources/sklearn/scikit_template_rul.twig.py
+++ JAICore/jaicore-ml/resources/sklearn/scikit_template_rul.twig.py
@@ -35,6 +35,15 @@ from tsfresh.feature_extraction.settings import ComprehensiveFCParameters
 
 OUTPUT_FILE = None
 
+class InvalidNumberReplacementTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        extracted_features = np.float32(X.to_numpy())
+        features = np.nan_to_num(extracted_features)
+        return features
+
 class LowComputationTimeFCParameters(dict):
     def __init__(self):
         initial_map = ComprehensiveFCParameters()
@@ -64,6 +73,136 @@ class MidComputationTimeFCParameters(dict):
         initial_map.pop("sample_entropy")
         initial_map.pop("change_quantiles")
         initial_map.pop("linear_trend_timewise")  # broken
+        super().__init__(initial_map)
+
+class Tsfresh(dict):
+    def __init__(self, has_duplicate_max, binned_entropy, last_location_of_maximum, abs_energy, c3, value_count, mean_second_derivative_central, first_location_of_minimum, standard_deviation, length, mean_abs_change, has_duplicate_min, mean_change, sum_values, percentage_of_reoccurring_datapoints_to_all_datapoints, range_count, absolute_sum_of_changes, energy_ratio_by_chunks, last_location_of_minimum, linear_trend, variance_larger_than_standard_deviation, spkt_welch_density, cid_ce, symmetry_looking, has_duplicate, skewness, count_above_mean, longest_strike_below_mean, mean, agg_autocorrelation, ratio_value_number_to_time_series_length, fft_aggregated, first_location_of_maximum, partial_autocorrelation, sum_of_reoccurring_data_points, count_below_mean, variance, longest_strike_above_mean, median, kurtosis, minimum, time_reversal_asymmetry_statistic, number_crossing_m, sum_of_reoccurring_values, maximum, approximate_entropy, number_cwt_peaks, augmented_dickey_fuller, quantile, agg_linear_trend, max_langevin_fixed_point, friedrich_coefficients, fft_coefficient, large_standard_deviation, autocorrelation, cwt_coefficients, percentage_of_reoccurring_values_to_all_values, ar_coefficient, ratio_beyond_r_sigma, number_peaks, sample_entropy, change_quantiles):
+        initial_map = ComprehensiveFCParameters()
+        initial_map.pop("linear_trend_timewise")  # broken
+        if not has_duplicate_max:
+            initial_map.pop("has_duplicate_max")
+        if not binned_entropy:
+            initial_map.pop("binned_entropy")
+        if not last_location_of_maximum:
+            initial_map.pop("last_location_of_maximum")
+        if not abs_energy:
+            initial_map.pop("abs_energy")
+        if not c3:
+            initial_map.pop("c3")
+        if not value_count:
+            initial_map.pop("value_count")
+        if not mean_second_derivative_central:
+            initial_map.pop("mean_second_derivative_central")
+        if not first_location_of_minimum:
+            initial_map.pop("first_location_of_minimum")
+        if not standard_deviation:
+            initial_map.pop("standard_deviation")
+        if not length:
+            initial_map.pop("length")
+        if not mean_abs_change:
+            initial_map.pop("mean_abs_change")
+        if not has_duplicate_min:
+            initial_map.pop("has_duplicate_min")
+        if not mean_change:
+            initial_map.pop("mean_change")
+        if not sum_values:
+            initial_map.pop("sum_values")
+        if not percentage_of_reoccurring_datapoints_to_all_datapoints:
+            initial_map.pop("percentage_of_reoccurring_datapoints_to_all_datapoints")
+        if not range_count:
+            initial_map.pop("range_count")
+        if not absolute_sum_of_changes:
+            initial_map.pop("absolute_sum_of_changes")
+        if not energy_ratio_by_chunks:
+            initial_map.pop("energy_ratio_by_chunks")
+        if not last_location_of_minimum:
+            initial_map.pop("last_location_of_minimum")
+        if not linear_trend:
+            initial_map.pop("linear_trend")
+        if not variance_larger_than_standard_deviation:
+            initial_map.pop("variance_larger_than_standard_deviation")
+        if not spkt_welch_density:
+            initial_map.pop("spkt_welch_density")
+        if not cid_ce:
+            initial_map.pop("cid_ce")
+        if not symmetry_looking:
+            initial_map.pop("symmetry_looking")
+        if not has_duplicate:
+            initial_map.pop("has_duplicate")
+        if not skewness:
+            initial_map.pop("skewness")
+        if not count_above_mean:
+            initial_map.pop("count_above_mean")
+        if not longest_strike_below_mean:
+            initial_map.pop("longest_strike_below_mean")
+        if not mean:
+            initial_map.pop("mean")
+        if not agg_autocorrelation:
+            initial_map.pop("agg_autocorrelation")
+        if not ratio_value_number_to_time_series_length:
+            initial_map.pop("ratio_value_number_to_time_series_length")
+        if not fft_aggregated:
+            initial_map.pop("fft_aggregated")
+        if not first_location_of_maximum:
+            initial_map.pop("first_location_of_maximum")
+        if not partial_autocorrelation:
+            initial_map.pop("partial_autocorrelation")
+        if not sum_of_reoccurring_data_points:
+            initial_map.pop("sum_of_reoccurring_data_points")
+        if not count_below_mean:
+            initial_map.pop("count_below_mean")
+        if not variance:
+            initial_map.pop("variance")
+        if not longest_strike_above_mean:
+            initial_map.pop("longest_strike_above_mean")
+        if not median:
+            initial_map.pop("median")
+        if not kurtosis:
+            initial_map.pop("kurtosis")
+        if not minimum:
+            initial_map.pop("minimum")
+        if not time_reversal_asymmetry_statistic:
+            initial_map.pop("time_reversal_asymmetry_statistic")
+        if not number_crossing_m:
+            initial_map.pop("number_crossing_m")
+        if not sum_of_reoccurring_values:
+            initial_map.pop("sum_of_reoccurring_values")
+        if not maximum:
+            initial_map.pop("maximum")
+        if not approximate_entropy:
+            initial_map.pop("approximate_entropy")
+        if not number_cwt_peaks:
+            initial_map.pop("number_cwt_peaks")
+        if not augmented_dickey_fuller:
+            initial_map.pop("augmented_dickey_fuller")
+        if not quantile:
+            initial_map.pop("quantile")
+        if not agg_linear_trend:
+            initial_map.pop("agg_linear_trend")
+        if not max_langevin_fixed_point:
+            initial_map.pop("max_langevin_fixed_point")
+        if not friedrich_coefficients:
+            initial_map.pop("friedrich_coefficients")
+        if not fft_coefficient:
+            initial_map.pop("fft_coefficient")
+        if not large_standard_deviation:
+            initial_map.pop("large_standard_deviation")
+        if not autocorrelation:
+            initial_map.pop("autocorrelation")
+        if not cwt_coefficients:
+            initial_map.pop("cwt_coefficients")
+        if not percentage_of_reoccurring_values_to_all_values:
+            initial_map.pop("percentage_of_reoccurring_values_to_all_values")
+        if not ar_coefficient:
+            initial_map.pop("ar_coefficient")
+        if not ratio_beyond_r_sigma:
+            initial_map.pop("ratio_beyond_r_sigma")
+        if not number_peaks:
+            initial_map.pop("number_peaks")
+        if not sample_entropy:
+            initial_map.pop("sample_entropy")
+        if not change_quantiles:
+            initial_map.pop("change_quantiles")
         super().__init__(initial_map)
 
 """
@@ -332,17 +471,6 @@ def serialize_prediction(prediction):
     print("write prediction to file ", OUTPUT_FILE)
     with open(OUTPUT_FILE, 'w') as file:
         file.write(prediction_json)
-
-
-class InvalidNumberReplacementTransformer(BaseEstimator, TransformerMixin):
-    def fit(self, X, y):
-        return self
-
-    def transform(self, X):
-        extracted_features = np.float32(X.to_numpy())
-        features = np.nan_to_num(extracted_features)
-        return features
-
 
 def run_train_mode(data):
     """
