a0ffa9363728907d50a041f3a0e91769cf7b488b
Felix <fmohr@mail.upb.de>
2020-06-30 18:27:30 +0200

    Reduced code redundancy in scheduling problem

--- JAICore/jaicore-basic/src/main/java/ai/libs/jaicore/problems/scheduling/computers/DefaultSchedulingComputer.java
+++ JAICore/jaicore-basic/src/main/java/ai/libs/jaicore/problems/scheduling/computers/DefaultSchedulingComputer.java
@@ -1,59 +1,21 @@
 package ai.libs.jaicore.problems.scheduling.computers;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import ai.libs.jaicore.basic.sets.Pair;
-import ai.libs.jaicore.problems.scheduling.IJobSchedulingInput;
-import ai.libs.jaicore.problems.scheduling.IScheduleComputer;
+import ai.libs.jaicore.problems.scheduling.ASchedulingComputer;
 import ai.libs.jaicore.problems.scheduling.Job;
 import ai.libs.jaicore.problems.scheduling.Machine;
 import ai.libs.jaicore.problems.scheduling.Operation;
 
-public class DefaultSchedulingComputer implements IScheduleComputer {
+public class DefaultSchedulingComputer extends ASchedulingComputer {
 
 	@Override
-	public void fillTimes(final IJobSchedulingInput problemInput, final List<Pair<Operation, Machine>> assignments, final Map<Job, Integer> arrivalTimes, final Map<Operation, Integer> startTimes, final Map<Operation, Integer> endTimes, final Map<Operation, Integer> setupStartTimes, final Map<Operation, Integer> setupEndTimes) {
+	public int getTimeWhenMachineBecomesAvailableForOperation(final Map<Job, Integer> arrivalTimes, final Map<Machine, Integer> machineReadiness, final Machine m) {
+		return machineReadiness.computeIfAbsent(m, Machine::getAvailableDate);
+	}
 
-		startTimes.clear();
-		endTimes.clear();
-		setupStartTimes.clear();
-		setupEndTimes.clear();
-
-		/* set all arrival times to the ones set in the job definitions */
-		problemInput.getJobs().forEach(j -> arrivalTimes.put(j, j.getReleaseDate()));
-
-		/* compute stats for the operations */
-		Map<Job, Integer> jobReadyness = new HashMap<>();
-		Map<Machine, Integer> machineReadyness = new HashMap<>();
-		Map<Machine, Integer> machineStates = new HashMap<>();
-		for (Pair<Operation, Machine> p : assignments) {
-
-			/* get times when job can arrive at the machine and time when machine can start to consider the job */
-			Machine m = p.getY();
-			Operation o = p.getX();
-			int timeWhenMachineBecomesAvailableForOperation = machineReadyness.computeIfAbsent(m, Machine::getAvailableDate);
-			int timeWhenJobArrivesAtMachine = jobReadyness.computeIfAbsent(o.getJob(), Job::getReleaseDate); // maybe add travel time here
-
-			/* compute required setup time */
-			int actualMachineState = machineStates.computeIfAbsent(m, Machine::getInitialState);
-			int requiredMachineState = o.getStatus();
-			int setupTime = (actualMachineState == requiredMachineState) ? 0 : m.getWorkcenter().getSetupMatrix()[actualMachineState][requiredMachineState];
-			int timeWhenMachineIsReadyToProcessOperation = timeWhenMachineBecomesAvailableForOperation + setupTime;
-
-			/* compute and memorize operation stats */
-			int startTime = Math.max(timeWhenMachineIsReadyToProcessOperation, timeWhenJobArrivesAtMachine);
-			int endTime = startTime + o.getProcessTime();
-			setupStartTimes.put(o, timeWhenMachineBecomesAvailableForOperation);
-			setupEndTimes.put(o, timeWhenMachineIsReadyToProcessOperation);
-			startTimes.put(o, startTime);
-			endTimes.put(o, endTime);
-
-			/* update machine state and readyness and job readyness */
-			machineReadyness.put(m, endTime);
-			machineStates.put(m, o.getStatus());
-			jobReadyness.put(o.getJob(), endTime);
-		}
+	@Override
+	public int timeWhenOperationArrivesAtMachine(final Map<Job, Integer> arrivalTimes, final Map<Machine, Integer> machineReadiness, final Map<Job, Integer> jobReadyness, final Operation o, final Machine m) {
+		return jobReadyness.computeIfAbsent(o.getJob(), Job::getReleaseDate); // maybe add travel time here
 	}
 }
--- JAICore/jaicore-basic/src/main/java/ai/libs/jaicore/problems/scheduling/computers/VariableReleaseDatesSchedulingComputer.java
+++ JAICore/jaicore-basic/src/main/java/ai/libs/jaicore/problems/scheduling/computers/VariableReleaseDatesSchedulingComputer.java
@@ -1,12 +1,10 @@
 package ai.libs.jaicore.problems.scheduling.computers;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import ai.libs.jaicore.basic.sets.Pair;
 import ai.libs.jaicore.problems.scheduling.IJobSchedulingInput;
-import ai.libs.jaicore.problems.scheduling.IScheduleComputer;
 import ai.libs.jaicore.problems.scheduling.Job;
 import ai.libs.jaicore.problems.scheduling.JobSchedulingProblemInput;
 import ai.libs.jaicore.problems.scheduling.Machine;
@@ -19,7 +17,9 @@ import ai.libs.jaicore.problems.scheduling.Operation;
  * @author Felix Mohr
  *
  */
-public class VariableReleaseDatesSchedulingComputer implements IScheduleComputer {
+public class VariableReleaseDatesSchedulingComputer extends DefaultSchedulingComputer {
+
+	private int latestArrivalTime;
 
 	@Override
 	public void fillTimes(final IJobSchedulingInput problemInput, final List<Pair<Operation, Machine>> assignments, final Map<Job, Integer> arrivalTimes, final Map<Operation, Integer> startTimes, final Map<Operation, Integer> endTimes, final Map<Operation, Integer> setupStartTimes,
@@ -29,51 +29,22 @@ public class VariableReleaseDatesSchedulingComputer implements IScheduleComputer
 			throw new IllegalArgumentException();
 		}
 		JobSchedulingProblemInput cProblemInput = (JobSchedulingProblemInput)problemInput;
-		int latestArrivalTime = cProblemInput.getLatestArrivalTime();
+		this.latestArrivalTime = cProblemInput.getLatestArrivalTime();
+		super.fillTimes(cProblemInput, assignments, arrivalTimes, startTimes, endTimes, setupStartTimes, setupEndTimes);
+	}
 
-		startTimes.clear();
-		endTimes.clear();
-		setupStartTimes.clear();
-		setupEndTimes.clear();
-
-		/* compute stats for the operations */
-		Map<Job, Integer> jobReadyness = new HashMap<>();
-		Map<Machine, Integer> machineReadyness = new HashMap<>();
-		Map<Machine, Integer> machineStates = new HashMap<>();
-		for (Pair<Operation, Machine> p : assignments) {
-
-			/* get times when job can arrive at the machine and time when machine can start to consider the job */
-			Machine m = p.getY();
-			Operation o = p.getX();
-			int timeWhenMachineBecomesAvailableForOperation = machineReadyness.computeIfAbsent(m, Machine::getAvailableDate);
-			Job job = o.getJob();
-			int timeWhenJobArrivesAtMachine;
-			if (!arrivalTimes.containsKey(job)) { // this is the first operation of assigned for this job (then use it as the arrival time for the job)
-				timeWhenJobArrivesAtMachine = Math.min(timeWhenMachineBecomesAvailableForOperation, latestArrivalTime);
-				arrivalTimes.put(job, timeWhenJobArrivesAtMachine);
-			}
-			else {
-				timeWhenJobArrivesAtMachine = Math.max(timeWhenMachineBecomesAvailableForOperation, jobReadyness.get(job));
-			}
-
-			/* compute required setup time */
-			int actualMachineState = machineStates.computeIfAbsent(m, Machine::getInitialState);
-			int requiredMachineState = o.getStatus();
-			int setupTime = (actualMachineState == requiredMachineState) ? 0 : m.getWorkcenter().getSetupMatrix()[actualMachineState][requiredMachineState];
-			int timeWhenMachineIsReadyToProcessOperation = timeWhenMachineBecomesAvailableForOperation + setupTime;
-
-			/* compute and memorize operation stats */
-			int startTime = Math.max(timeWhenMachineIsReadyToProcessOperation, timeWhenJobArrivesAtMachine);
-			int endTime = startTime + o.getProcessTime();
-			setupStartTimes.put(o, timeWhenMachineBecomesAvailableForOperation);
-			setupEndTimes.put(o, timeWhenMachineIsReadyToProcessOperation);
-			startTimes.put(o, startTime);
-			endTimes.put(o, endTime);
-
-			/* update machine state and readyness and job readyness */
-			machineReadyness.put(m, endTime);
-			machineStates.put(m, o.getStatus());
-			jobReadyness.put(o.getJob(), endTime);
+	@Override
+	public int timeWhenOperationArrivesAtMachine(final Map<Job, Integer> arrivalTimes, final Map<Machine, Integer> machineReadiness, final Map<Job, Integer> jobReadyness, final Operation o, final Machine m) {
+		Job job = o.getJob();
+		int timeWhenMachineBecomesAvailableForOperation = super.getTimeWhenMachineBecomesAvailableForOperation(arrivalTimes, machineReadiness, m);
+		int timeWhenJobArrivesAtMachine;
+		if (!arrivalTimes.containsKey(job)) { // this is the first operation of assigned for this job (then use it as the arrival time for the job)
+			timeWhenJobArrivesAtMachine = Math.min(timeWhenMachineBecomesAvailableForOperation, this.latestArrivalTime);
+			arrivalTimes.put(job, timeWhenJobArrivesAtMachine);
 		}
+		else {
+			timeWhenJobArrivesAtMachine = Math.max(timeWhenMachineBecomesAvailableForOperation, arrivalTimes.get(job));
+		}
+		return timeWhenJobArrivesAtMachine;
 	}
 }
