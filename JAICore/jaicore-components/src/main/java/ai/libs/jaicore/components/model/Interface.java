a8c54415354928626feb64eae9efad8310639192
Felix Mohr <fmohr@mail.upb.de>
2020-07-03 03:45:42 +0200

    Resolved the bug causing HASCO/ML-Plan to fail (#215)

--- JAICore/jaicore-components/src/main/java/ai/libs/jaicore/components/model/Interface.java
+++ JAICore/jaicore-components/src/main/java/ai/libs/jaicore/components/model/Interface.java
@@ -53,4 +53,58 @@ public class Interface implements Serializable {
 	public Integer getMax() {
 		return this.max;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+		result = prime * result + ((this.max == null) ? 0 : this.max.hashCode());
+		result = prime * result + ((this.min == null) ? 0 : this.min.hashCode());
+		result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		Interface other = (Interface) obj;
+		if (this.id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!this.id.equals(other.id)) {
+			return false;
+		}
+		if (this.max == null) {
+			if (other.max != null) {
+				return false;
+			}
+		} else if (!this.max.equals(other.max)) {
+			return false;
+		}
+		if (this.min == null) {
+			if (other.min != null) {
+				return false;
+			}
+		} else if (!this.min.equals(other.min)) {
+			return false;
+		}
+		if (this.name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!this.name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
 }
