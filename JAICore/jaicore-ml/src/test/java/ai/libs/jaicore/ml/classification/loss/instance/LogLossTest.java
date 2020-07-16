7104af4ac329fbd805d4419249439989d224b106
mwever <wever@mail.uni-paderborn.de>
2020-07-16 09:49:33 +0200

    Fixed compile error in LogLossTest.

--- JAICore/jaicore-ml/src/test/java/ai/libs/jaicore/ml/classification/loss/instance/LogLossTest.java
+++ JAICore/jaicore-ml/src/test/java/ai/libs/jaicore/ml/classification/loss/instance/LogLossTest.java
@@ -2,18 +2,21 @@ package ai.libs.jaicore.ml.classification.loss.instance;
 
 import static org.junit.Assert.assertEquals;
 
+import org.api4.java.ai.ml.classification.singlelabel.evaluation.ISingleLabelClassification;
 import org.junit.Test;
+
+import ai.libs.jaicore.ml.classification.singlelabel.SingleLabelClassification;
 
 public class LogLossTest {
 
-	private static final double[] SCORES = { 0.0, 0.4, 1.0 };
+	private static final ISingleLabelClassification PREDICTED = new SingleLabelClassification(new double[] { 0.0, 0.4, 1.0 });
 
 	@Test
 	public void testLogLoss() {
 		LogLoss ll = new LogLoss();
-		assertEquals("Log loss does not work as expected.", -Math.log(1E-15), ll.loss(0, SCORES), 1E-8);
-		assertEquals("Log loss does not work as expected.", -Math.log(0.4), ll.loss(1, SCORES), 1E-8);
-		assertEquals("Log loss does not work as expected.", -Math.log(1.0 - 1E-15), ll.loss(2, SCORES), 1E-8);
+		assertEquals("Log loss does not work as expected.", -Math.log(1E-15), ll.loss(0, PREDICTED), 1E-8);
+		assertEquals("Log loss does not work as expected.", -Math.log(0.4), ll.loss(1, PREDICTED), 1E-8);
+		assertEquals("Log loss does not work as expected.", -Math.log(1.0 - 1E-15), ll.loss(2, PREDICTED), 1E-8);
 	}
 
 }
