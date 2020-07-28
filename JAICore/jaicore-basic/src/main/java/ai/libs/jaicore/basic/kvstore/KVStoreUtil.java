9f56f839b215b583a7c2b5d2e1ec397bccd8e917
mwever <wever@mail.uni-paderborn.de>
2020-07-28 11:47:36 +0200

    Resolved code smells.

--- JAICore/jaicore-basic/src/main/java/ai/libs/jaicore/basic/kvstore/KVStoreUtil.java
+++ JAICore/jaicore-basic/src/main/java/ai/libs/jaicore/basic/kvstore/KVStoreUtil.java
@@ -47,29 +47,7 @@ public class KVStoreUtil {
 	 * @return A string representing the data of the {@link KVStoreCollection} in LaTeX formatting.
 	 */
 	public static String kvStoreCollectionToLaTeXTable(final KVStoreCollection kvStoreCollection, final String rowIndex, final String columnIndex, final String cellFormatting, final String missingEntry) {
-		StringBuilder sb = new StringBuilder();
-		Table<String> table = new Table<>();
-		for (IKVStore store : kvStoreCollection) {
-
-			String[] cellFormattingSplit = store.getAsString(cellFormatting).split("#");
-			List<String> cleanedCellFormatting = Arrays.stream(cellFormattingSplit).filter(x -> !x.equals("")).collect(Collectors.toList());
-
-			String rowValue = store.getAsString(rowIndex).replace("\\_", "\\\\_");
-			String columnValue = store.getAsString(columnIndex).replace("\\_", "\\\\_");
-
-			StringBuilder tableEntryBuilder = new StringBuilder();
-			for (String cellKey : cleanedCellFormatting) {
-				if (!store.containsKey(cellKey)) {
-					tableEntryBuilder.append(cellKey);
-				} else {
-					tableEntryBuilder.append(store.getAsString(cellKey));
-				}
-			}
-
-			table.add(columnValue, rowValue, tableEntryBuilder.toString());
-		}
-		sb.append(table.toLaTeX(missingEntry));
-		return sb.toString();
+		return kvStoreCollectionToTable(kvStoreCollection, rowIndex, columnIndex, cellFormatting, missingEntry).toLaTeX();
 	}
 
 	/**
