696d5f3b9fa5577b7fd5d9283c6adffcead9fdb6
mwever <wever@mail.uni-paderborn.de>
2020-07-28 11:34:54 +0200

    Resolved issues with MekaPipelineFactoryTest.

--- softwareconfiguration/mlplan/meka/src/test/java/ai/libs/mlplan/multilabel/MekaPipelineFactoryTest.java
+++ softwareconfiguration/mlplan/meka/src/test/java/ai/libs/mlplan/multilabel/MekaPipelineFactoryTest.java
@@ -1,19 +1,21 @@
 package ai.libs.mlplan.multilabel;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import org.api4.java.ai.ml.core.exception.TrainingException;
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.util.concurrent.AtomicDouble;
 
@@ -22,51 +24,48 @@ import ai.libs.jaicore.components.exceptions.ComponentInstantiationFailedExcepti
 import ai.libs.jaicore.components.model.ComponentInstance;
 import ai.libs.jaicore.components.model.ComponentUtil;
 import ai.libs.jaicore.components.serialization.ComponentLoader;
-import ai.libs.jaicore.ml.classification.multilabel.dataset.IMekaInstances;
-import ai.libs.jaicore.ml.classification.multilabel.dataset.MekaInstances;
-import ai.libs.jaicore.ml.classification.multilabel.learner.IMekaClassifier;
 import ai.libs.mlplan.multilabel.mekamlplan.EMLPlanMekaProblemType;
 import ai.libs.mlplan.multilabel.mekamlplan.MekaPipelineFactory;
-import meka.core.MLUtils;
-import weka.core.Instances;
 
 public class MekaPipelineFactoryTest {
 	private static final File SSC = FileUtil.getExistingFileWithHighestPriority(EMLPlanMekaProblemType.CLASSIFICATION_MULTILABEL.getSearchSpaceConfigFileFromResource(),
 			EMLPlanMekaProblemType.CLASSIFICATION_MULTILABEL.getSearchSpaceConfigFromFileSystem());
+	private static final Logger LOGGER = LoggerFactory.getLogger(MekaPipelineFactoryTest.class);
 
 	private static ComponentLoader cl;
 	private static MekaPipelineFactory mpf;
-	private static IMekaInstances dTrain;
 
 	@BeforeClass
 	public static void setup() throws Exception {
 		cl = new ComponentLoader(SSC);
 		mpf = new MekaPipelineFactory();
-		Instances data = new Instances(new FileReader(new File("testrsc/flags.arff")));
-		MLUtils.prepareData(data);
-		dTrain = new MekaInstances(data);
 	}
 
-	@Before
-	public void init() {
-	}
-
-	@Ignore
 	@Test
 	public void testValidDefaultConfigInstantiation() throws ComponentInstantiationFailedException, TrainingException, InterruptedException {
 		Collection<ComponentInstance> algorithmSelections = ComponentUtil.getAllAlgorithmSelectionInstances("MLClassifier", cl.getComponents());
 		List<ComponentInstance> list = new ArrayList<>(algorithmSelections);
 
-		double currentPercentage = 0.0;
+		AtomicInteger count = new AtomicInteger(0);
+		AtomicDouble currentPercentage = new AtomicDouble(0.0);
 		double step = 0.05;
-		for (int i = 0; i < list.size(); i++) {
-			IMekaClassifier c = mpf.getComponentInstantiation(list.get(i));
-
-			if ((double) i / list.size() >= currentPercentage) {
-				System.out.println("Current state: " + (currentPercentage * 100) + "%");
-				currentPercentage += step;
+		List<String> listOfFails = Collections.synchronizedList(new ArrayList<>());
+		IntStream.range(0, list.size()).parallel().forEach(i -> {
+			try {
+				mpf.getComponentInstantiation(list.get(i));
+			} catch (ComponentInstantiationFailedException e) {
+				String name = ComponentUtil.getComponentInstanceAsComponentNames(list.get(i));
+				LOGGER.debug("Exception occurred instantiating {}", name, e);
+				listOfFails.add(name);
 			}
-		}
+
+			if ((double) count.incrementAndGet() / list.size() >= currentPercentage.get()) {
+				System.out.println("Current state: " + (currentPercentage.get() * 100) + "%");
+				currentPercentage.addAndGet(step);
+			}
+		});
+
+		assertTrue("List of fails is not empty: " + listOfFails, listOfFails.isEmpty());
 	}
 
 	@Test
@@ -77,14 +76,16 @@ public class MekaPipelineFactoryTest {
 		AtomicInteger count = new AtomicInteger(0);
 		AtomicDouble currentPercentage = new AtomicDouble(0.0);
 		double step = 0.05;
+		List<String> listOfFails = Collections.synchronizedList(new ArrayList<>());
 		IntStream.range(0, list.size()).parallel().forEach(i -> {
-			System.out.println(ComponentUtil.getComponentInstanceAsComponentNames(list.get(i)));
 			int currentI = i;
 			IntStream.range(0, 5).forEach(s -> {
 				try {
 					mpf.getComponentInstantiation(ComponentUtil.getRandomParametrization(list.get(currentI), new Random(s)));
 				} catch (ComponentInstantiationFailedException e) {
-					e.printStackTrace();
+					String name = ComponentUtil.getComponentInstanceAsComponentNames(list.get(i));
+					LOGGER.debug("Exception occurred instantiating {}", name, e);
+					listOfFails.add(name);
 				}
 			});
 
@@ -93,6 +94,8 @@ public class MekaPipelineFactoryTest {
 				currentPercentage.addAndGet(step);
 			}
 		});
+
+		assertTrue("List of fails is not empty: " + listOfFails, listOfFails.isEmpty());
 	}
 
 }
