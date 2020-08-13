48360985017df74f0769502ef0eda305f5f195f3
mwever <wever@mail.uni-paderborn.de>
2020-08-13 16:58:33 +0200

    Added documentation for multi-fidelity object evaluator interface.

--- JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/core/evaluation/evaluator/IMultiFidelityObjectEvaluator.java
+++ JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/core/evaluation/evaluator/IMultiFidelityObjectEvaluator.java
@@ -3,6 +3,14 @@ package ai.libs.jaicore.ml.core.evaluation.evaluator;
 import org.api4.java.common.attributedobjects.IObjectEvaluator;
 import org.api4.java.common.attributedobjects.ObjectEvaluationFailedException;
 
+/**
+ * A multi-fidelity object evaluator allows for specifying a certain amount of an evaluation resource.
+ *
+ * @author mwever
+ *
+ * @param <T> The type of object to be evaluated.
+ * @param <V> The comparable evaluation value.
+ */
 public interface IMultiFidelityObjectEvaluator<T, V extends Comparable<V>> extends IObjectEvaluator<T, V> {
 
 	public double getMaxBudget();
