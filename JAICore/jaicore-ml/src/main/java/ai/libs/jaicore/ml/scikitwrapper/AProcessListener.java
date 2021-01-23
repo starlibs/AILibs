667d56ae1d4d124ed3b2bdc5b9bd07e2a3937eee
Alexander Tornede <alexander.tornede@upb.de>
2021-01-23 21:52:07 +0100

    Fix bug in AProcessListener

--- JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/scikitwrapper/AProcessListener.java
+++ JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/scikitwrapper/AProcessListener.java
@@ -37,7 +37,8 @@ public abstract class AProcessListener implements IProcessListener, ILoggingCust
 	@Override
 	public void listenTo(final Process process) throws IOException, InterruptedException {
 		this.logger.info("Starting to listen to process {}", process);
-		try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(process.getInputStream())); BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
+		try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
 			// While process is alive the output- and error stream is output.
 			while (process.isAlive()) {
 				if (Thread.interrupted()) { // reset flag since we will throw an exception now
@@ -57,7 +58,7 @@ public abstract class AProcessListener implements IProcessListener, ILoggingCust
 					}
 					this.handleInput(line);
 				}
-				while (this.checkReady(inputReader) && (line = errorReader.readLine()) != null) {
+				while (this.checkReady(errorReader) && (line = errorReader.readLine()) != null) {
 					if (line.contains("import imp") || line.contains("imp module")) {
 						continue;
 					}
