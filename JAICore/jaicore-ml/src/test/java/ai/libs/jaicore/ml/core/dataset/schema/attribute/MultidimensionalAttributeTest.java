9594e57e189f3fc61d1f3fd738fe189cf6bdec0b
Lukas Fehring <fehring2@mail.upb.de>
2021-03-09 15:58:47 +0100

    added fergotten Test

--- JAICore/jaicore-ml/src/test/java/ai/libs/jaicore/ml/core/dataset/schema/attribute/MultidimensionalAttributeTest.java
+++ JAICore/jaicore-ml/src/test/java/ai/libs/jaicore/ml/core/dataset/schema/attribute/MultidimensionalAttributeTest.java
@@ -1,17 +1,40 @@
 package ai.libs.jaicore.ml.core.dataset.schema.attribute;
 
-import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.api4.java.ai.ml.core.dataset.schema.attribute.IAttribute;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
-public class MultidimensionalAttributeTest extends AAttributeTest possibly
-{
+public class MultidimensionalAttributeTest extends AAttributeTest {
 
 	private final static String TEST_ATTR_NAME = "testattribute";
-	private final static String TEST_SERIALISE_STRING = "[[1.0 2.0] [3.0 4.0] [5.0 6.0]]";
+	private final static double[][] TEST_CONTAINED_VAL = { { 1.0, 2.0 }, { 3.0, 4.0 }, { 5.0, 6.0 } };
 	private final static int Test_ATTR_XSIZE = 3;
 	private final static int Test_ATTR_YSIZE = 2;
+
 	static MultidimensionalAttribute testAttr;
+
+	@BeforeAll
+	public static void SetupApi() {
+		testAttr = new MultidimensionalAttribute(TEST_ATTR_NAME, Test_ATTR_XSIZE, Test_ATTR_YSIZE);
+	}
+
+	@Override
+	public String getExpectedAttributeName() {
+		return TEST_ATTR_NAME;
+	}
+
+	@Override
+	public Collection<?> getValuesThatMustBeContained() {
+		Collection<double[][]> testList = new ArrayList<double[][]>();
+		testList.add(TEST_CONTAINED_VAL);
+		return testList;
+	}
+
+	@Override
+	public IAttribute getTestedAttribute() {
+		return testAttr;
+	}
 
 }
